21e5291e2827b7d3fd2998b2d215babec403b432
Scott Kirkland <srkirkland@gmail.com>
2011-07-18 10:34:13 -0700

    Added global startup code, automapper and IoC

--- Purchasing.Web/Global.asax.cs
+++ Purchasing.Web/Global.asax.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
+using Castle.Windsor;
+using Microsoft.Practices.ServiceLocation;
+using MvcMiniProfiler;
+using Purchasing.Web.Controllers;
+using UCDArch.Data.NHibernate;
+using UCDArch.Web.IoC;
+using UCDArch.Web.ModelBinder;
 
 namespace Purchasing.Web
 {
@@ -12,29 +15,60 @@ namespace Purchasing.Web
 
     public class MvcApplication : System.Web.HttpApplication
     {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
-
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
             );
 
         }
 
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+#if DEBUG
+            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
+#endif
 
-            RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+
+            ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
+
+            AutomapperConfig.Configure();
+
+            NHibernateSessionConfiguration.Mappings.UseFluentMappings(typeof(Purchasing.Core.Class1).Assembly);
+
+            IWindsorContainer container = InitializeServiceLocator();
+        }
+
+        private static IWindsorContainer InitializeServiceLocator()
+        {
+            IWindsorContainer container = new WindsorContainer();
+
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
+
+            container.RegisterControllers(typeof(HomeController).Assembly);
+            ComponentRegistrar.AddComponentsTo(container);
+
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+
+            return container;
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal)
+            {
+                MiniProfiler.Start();
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
         }
     }
 }
