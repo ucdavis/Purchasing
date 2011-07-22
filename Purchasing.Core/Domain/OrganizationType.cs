d69e11d1bb15ab67322374a729fc6f07a328b53a
Scott Kirkland <srkirkland@gmail.com>
2011-07-22 13:36:37 -0700

    started mapping classes, including user/role/departments

---
+++ Purchasing.Core/Domain/OrganizationType.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
+
+namespace Purchasing.Core.Domain
+{
+    public class OrganizationType : DomainObjectWithTypedId<string>
+    {
+        public virtual string Name { get; set; }
+        public virtual bool IsActive { get; set; }
+    }
+
+    public class OrganizationTypeMap : ClassMap<OrganizationType>
+    {
+        public OrganizationTypeMap()
+        {
+            ReadOnly();
+
+            Table("vOrganizationTypes");
+
+            Map(x => x.Name);
+            Map(x => x.IsActive);
+        }
+    }
+}
---
+++ Purchasing.Core/Domain/Role.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
+
+namespace Purchasing.Core.Domain
+{
+    public class Role : DomainObjectWithTypedId<string>
+    {
+        public virtual string Name { get; set; }
+    }
+
+    public class RoleMap : ClassMap<Role>
+    {
+        public RoleMap()
+        {
+            Map(x => x.Name);
+        }
+    }
+}
