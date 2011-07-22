be349cd344e84aeed450b594f19ef2c1f9bc0a25
Scott Kirkland <srkirkland@gmail.com>
2011-07-22 14:57:25 -0700

    finished up the non-permission based mappings

---
+++ Purchasing.Core/Domain/LineItem.cs
@@ -0,0 +1,33 @@
+﻿using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
+
+namespace Purchasing.Core.Domain
+{
+    public class LineItem : DomainObject
+    {
+        public virtual int Quantity { get; set; }
+        public virtual string CatalogNumber { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Unit { get; set; }
+        public virtual decimal UnitPrice { get; set; }
+        public virtual string Url { get; set; }
+        public virtual string Notes { get; set; }
+        public virtual Order Order { get; set; }
+    }
+
+    public class LineItemMap : ClassMap<LineItem>
+    {
+        public LineItemMap()
+        {
+            Map(x => x.Quantity);
+            Map(x => x.CatalogNumber);
+            Map(x => x.Description);
+            Map(x => x.Unit);
+            Map(x => x.UnitPrice);
+            Map(x => x.Url);
+            Map(x => x.Notes);
+
+            References(x => x.Order);
+        }
+    }
+}
---
+++ Purchasing.Core/Domain/OrderTracking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
+
+namespace Purchasing.Core.Domain
+{
+    public class OrderTracking : DomainObject
+    {
+        public virtual string Description { get; set; } //TODO: Enum?
+        public virtual DateTime DateCreated { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual User User { get; set; }
+    }
+
+    public class OrderTrackingMap : ClassMap<OrderTracking>
+    {
+        public OrderTrackingMap()
+        {
+            Table("OrderTracking");
+
+            Map(x => x.Description);
+            Map(x => x.DateCreated);
+
+            References(x => x.Order);
+            References(x => x.User);
+        }
+    }
+}
