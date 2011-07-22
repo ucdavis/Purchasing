1b5d69a6209fd4b4416084456318c5bb980e0fc3
Scott Kirkland <srkirkland@gmail.com>
2011-07-22 11:27:28 -0700

    Created a role provider for purchasing, using the dapper mapper for SQL

---
+++ Purchasing.Web/Services/DbService.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Common;
+using System.Web.Configuration;
+using MvcMiniProfiler;
+using System.Data.SqlClient;
+
+namespace Purchasing.Web.Services
+{
+    public interface IDbService
+    {
+        DbConnection GetConnection(string connectionString = null);
+    }
+
+    public class DbService : IDbService
+    {
+        public DbConnection GetConnection(string connectionString = null)
+        {
+            //If connection string is null, use the default sql ce connection
+            if (connectionString == null)
+            {
+                connectionString = WebConfigurationManager.ConnectionStrings["MainDb"].ConnectionString;
+            }
+
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            return MvcMiniProfiler.Data.ProfiledDbConnection.Get(connection, MiniProfiler.Current);
+        }
+    }
+}
