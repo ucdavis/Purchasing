70a1af2f4cd866c6e6a3768bf8fe83295bfe6be7
Scott Kirkland <srkirkland@gmail.com>
2011-07-25 10:40:22 -0700

    added auditing tables to the db scheema

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Audits.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Audits] (
+    [ID]          UNIQUEIDENTIFIER NOT NULL,
+    [ObjectName]  VARCHAR (50)     NOT NULL,
+    [ObjectId]    VARCHAR (50)     NULL,
+    [AuditAction] CHAR (1)         NOT NULL,
+    [Username]    NVARCHAR (256)   NOT NULL,
+    [AuditDate]   DATETIME         NOT NULL
+);
+
