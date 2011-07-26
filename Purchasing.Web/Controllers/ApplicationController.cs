5b61e4748c363b623c04d64a73cd19cc1e351c9e
Scott Kirkland <srkirkland@gmail.com>
2011-07-26 11:20:39 -0700

    Added an attribute which will profile the controller whenever an action is executed

--- Purchasing.Web/Controllers/ApplicationController.cs
+++ Purchasing.Web/Controllers/ApplicationController.cs
@@ -1,8 +1,10 @@
-﻿using UCDArch.Web.Controller;
+﻿using Purchasing.Web.Helpers;
+using UCDArch.Web.Controller;
 using UCDArch.Web.Attributes;
 
 namespace Purchasing.Web.Controllers
 {
     [Version(MajorVersion = 1)]
+    [Profile]
     public abstract class ApplicationController : SuperController { }
 }
