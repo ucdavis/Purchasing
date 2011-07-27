05b2b1b67cd30eb0298f17ac86b9703265ebadf1
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-07-27 08:35:04 -0700

    Setting up tests

---
+++ Purchasing.Tests/Core/RepositoryLoad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Purchasing.Core.Domain;
+using UCDArch.Core.PersistanceSupport;
+
+namespace Purchasing.Tests.Core
+{
+    public static class RepositoryLoad
+    {
+        //public static void LoadAccount(IRepository repository, int entriesToAdd)
+        //{
+        //    for (int i = 0; i < entriesToAdd; i++)
+        //    {
+        //        var validEntity = CreateValidEntities.Account(i + 1);
+        //        repository.OfType<Account>().EnsurePersistent(validEntity);
+        //    }
+        //}
+    }
+}
