1f0508a9d52d476c244e6b4368e4ef451253cdf1
Scott Kirkland <srkirkland@gmail.com>
2011-07-28 15:09:38 -0700

    updated the account table scheema to reflect the org name change

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_vAccounts_vDepartments.fkey.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_vAccounts_vDepartments.fkey.sql
@@ -1,5 +1,7 @@
 ﻿ALTER TABLE [dbo].[vAccounts]
-    ADD CONSTRAINT [FK_vAccounts_vDepartments] FOREIGN KEY ([DepartmentId]) REFERENCES [dbo].[vOrganizations] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+    ADD CONSTRAINT [FK_vAccounts_vDepartments] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[vOrganizations] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
 
 
 
--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/vAccounts.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/vAccounts.table.sql
@@ -2,10 +2,12 @@
     [Id]             VARCHAR (10) NOT NULL,
     [Name]           VARCHAR (50) NOT NULL,
     [IsActive]       BIT          NOT NULL,
-    [AccountManager] VARCHAR (15) NOT NULL,
-    [PI]             VARCHAR (15) NULL,
-    [DepartmentId]   CHAR (4)     NOT NULL
+    [AccountManager] VARCHAR (30) NOT NULL,
+    [PI]             VARCHAR (30) NULL,
+    [OrganizationId] CHAR (4)     NOT NULL
 );
+
+
 
 
 
