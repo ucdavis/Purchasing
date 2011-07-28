16bd6761bd5f13e15015031d264e6230f3621398
Scott Kirkland <srkirkland@gmail.com>
2011-07-28 16:24:49 -0700

    Changed the name of lots of keys to match org instead of dept

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_vAccounts_vDepartments.fkey.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_vAccounts_vDepartments.fkey.sql
@@ -1,5 +1,5 @@
-﻿ALTER TABLE [dbo].[vAccounts]
-    ADD CONSTRAINT [FK_vAccounts_vDepartments] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[vOrganizations] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+﻿/*ALTER TABLE [dbo].[vAccounts]
+    ADD CONSTRAINT [FK_vAccounts_vDepartments] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[vOrganizations] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;*/
 
 
 
---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_vAccounts_vOrganizations.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[vAccounts]
+    ADD CONSTRAINT [FK_vAccounts_vOrganizations] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[vOrganizations] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
