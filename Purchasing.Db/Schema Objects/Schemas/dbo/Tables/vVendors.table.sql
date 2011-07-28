a1ebf19655fe535094a92e38a8b180c2b1755f37
Scott Kirkland <srkirkland@gmail.com>
2011-07-28 16:01:27 -0700

    Added vendors views

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/vVendors.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[vVendors] (
+    [Id]               CHAR (10)    NOT NULL,
+    [Name]             VARCHAR (40) NOT NULL,
+    [OwnershipCode]    VARCHAR (2)  NULL,
+    [BusinessTypeCode] VARCHAR (2)  NULL
+);
+
