d7e9d5fff250f1bc7e83c0f5f9450e73b963c205
Alan Lai <anlai@ucdavis.edu>
2011-08-01 11:17:14 -0700

    added support for workgroup addresses Associate #2.

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_WorkgroupAddresses_States.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[WorkgroupAddresses]
+    ADD CONSTRAINT [FK_WorkgroupAddresses_States] FOREIGN KEY ([StateId]) REFERENCES [dbo].[States] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_WorkgroupAddresses_Workgroups.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[WorkgroupAddresses]
+    ADD CONSTRAINT [FK_WorkgroupAddresses_Workgroups] FOREIGN KEY ([WorkgroupId]) REFERENCES [dbo].[Workgroups] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
