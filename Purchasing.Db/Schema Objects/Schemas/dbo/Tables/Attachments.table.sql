fcd67f4af1fc31981c911a2e63eab81756c1938d
Alan Lai <anlai@ucdavis.edu>
2011-08-01 12:45:07 -0700

    added table for attachments Associate #2.

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Attachments.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Attachments] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Filename]    VARCHAR (100)   NOT NULL,
+    [ContentType] VARCHAR (200)   NOT NULL,
+    [Contents]    VARBINARY (MAX) NOT NULL,
+    [OrderId]     INT             NOT NULL,
+    [DateCreated] DATETIME        NOT NULL,
+    [CreatedBy]   VARCHAR (10)    NOT NULL
+);
+
