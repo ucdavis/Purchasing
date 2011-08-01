5206919839efaf77278a04ffebc3bd130f3c63dd
Alan Lai <anlai@ucdavis.edu>
2011-08-01 13:09:41 -0700

    added support for workgroup vendor table Associate #2

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_WorkgroupVendors_Workgroups.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[WorkgroupVendors]
+    ADD CONSTRAINT [FK_WorkgroupVendors_Workgroups] FOREIGN KEY ([WorkgroupId]) REFERENCES [dbo].[Workgroups] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
