215ba26d91cd73cf246d523989ec54f82c168785
Alan Lai <anlai@ucdavis.edu>
2011-08-01 13:03:27 -0700

    added table for auto approvals Associate #2.

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Constraints/DF_AutoApprovals_LessThan.defconst.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[AutoApprovals]
+    ADD CONSTRAINT [DF_AutoApprovals_LessThan] DEFAULT ((0)) FOR [LessThan];
+
---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_AutoApprovals_Users1.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[AutoApprovals]
+    ADD CONSTRAINT [FK_AutoApprovals_Users1] FOREIGN KEY ([TargetUserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
