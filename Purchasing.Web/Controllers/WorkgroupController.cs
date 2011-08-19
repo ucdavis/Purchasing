648b2e364e2892befef6796c936574bd7e927f62
unknown <Administrator@.(none)>
2011-08-19 10:22:00 -0700

    Created Workgroup controller with Index action. Basic CRUD still missing.

---
+++ Purchasing.Web/Controllers/WorkgroupController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+using Purchasing.Core.Domain;
+
+namespace Purchasing.Web.Controllers
+{
+    /// <summary>
+    /// Controller for the Workgroup class
+    /// </summary>
+    public class WorkgroupController : ApplicationController
+    {
+	    private readonly IRepository<Workgroup> _workgroupRepository;
+
+        public WorkgroupController(IRepository<Workgroup> workgroupRepository)
+        {
+            _workgroupRepository = workgroupRepository;
+        }
+    
+        //
+        // GET: /Workgroup/
+        public ActionResult Index()
+        {
+            var workgroupList = _workgroupRepository.Queryable;
+
+            return View(workgroupList.ToList());
+        }
+
+    }
+
+	/// <summary>
+    /// ViewModel for the Workgroup class
+    /// </summary>
+    public class WorkgroupViewModel
+	{
+		public Workgroup Workgroup { get; set; }
+ 
+		public static WorkgroupViewModel Create(IRepository repository)
+		{
+			Check.Require(repository != null, "Repository must be supplied");
+			
+			var viewModel = new WorkgroupViewModel {Workgroup = new Workgroup()};
+ 
+			return viewModel;
+		}
+	}
+}
