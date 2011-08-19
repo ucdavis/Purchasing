56b70cfb8413071399447fd57fe2624594b8de0a
James Cubbage <jscubbage@ucdavis.edu>
2011-08-19 14:20:27 -0700

    AccountController index

---
+++ Purchasing.Web/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+
+namespace Purchasing.Web.Controllers
+{
+    /// <summary>
+    /// Controller for the Error class
+    /// </summary>
+    public class ErrorController : ApplicationController
+    {
+	    
+    
+        //
+        // GET: /Error/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
