153e458bf7fe48faa5b70f68de13b904e8dc6fde
Alan Lai <anlai@ucdavis.edu>
2011-08-19 07:34:11 -0700

    updated schema for workgroup to have multiple departments and domain model for workgroup

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_Workgroups_vDepartments.fkey.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_Workgroups_vDepartments.fkey.sql
@@ -1,5 +1,5 @@
-﻿ALTER TABLE [dbo].[Workgroups]
-    ADD CONSTRAINT [FK_Workgroups_vDepartments] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[vOrganizations] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+﻿/*ALTER TABLE [dbo].[Workgroups]
+    ADD CONSTRAINT [FK_Workgroups_vDepartments] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[vOrganizations] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;*/
 
 
 
--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Workgroups.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Workgroups.table.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[Workgroups] (
-    [Id]             INT          IDENTITY (1, 1) NOT NULL,
-    [Name]           VARCHAR (50) NOT NULL,
-    [OrganizationId] CHAR (4)     NOT NULL,
-    [IsActive]       BIT          NOT NULL
+    [Id]       INT          IDENTITY (1, 1) NOT NULL,
+    [Name]     VARCHAR (50) NOT NULL,
+    [IsActive] BIT          NOT NULL
 );
+
+
 
 
 
