682feda675ec32e759584e6296cedf77c6a595c7
Alan Lai <anlai@ucdavis.edu>
2011-08-19 11:25:58 -0700

    added some samples with the other data types

--- Purchasing.Web/Controllers/MultiSelectController.cs
+++ Purchasing.Web/Controllers/MultiSelectController.cs
@@ -18,21 +18,60 @@ namespace Purchasing.Web.Controllers
         // GET: /MultiSelect/
         public ActionResult Index()
         {
-            string[] accts = {"3-6851000", "3-APSAC37", "3-APSAR24"};
+            var viewModel = MultiSelectViewModel.Create(Repository);
 
-            var accounts = Repository.OfType<Account>().Queryable.Where(a => accts.Contains(a.Id));
-
-            return View(accounts);
+            return View(viewModel);
         }
 
-        public JsonNetResult Search(string searchTerm)
+        public JsonNetResult SearchAccounts(string searchTerm)
         {
-            var results = Repository.OfType<Account>().Queryable.Where(a => a.Name.Contains(searchTerm));
+            var results = Repository.OfType<Account>().Queryable.Where(a => a.Name.Contains(searchTerm) || a.Id.Contains(searchTerm));
 
             return new JsonNetResult(results.Select(a => new {Id = a.Id, Label = a.Name}));
-
         }
 
+        public JsonNetResult SearchOrganizations(string searchTerm)
+        {
+            var results = Repository.OfType<Organization>().Queryable.Where(a => a.Name.Contains(searchTerm) || a.Id.Contains(searchTerm));
+
+            return new JsonNetResult(results.Select(a => new { Id = a.Id, Label = a.Name }));
+        }
+
+        public JsonNetResult SearchVendors(string searchTerm)
+        {
+            var results = Repository.OfType<Vendor>().Queryable.Where(a => a.Name.Contains(searchTerm) || a.Id.Contains(searchTerm));
+
+            return new JsonNetResult(results.Select(a => new { Id = a.Id, Label = a.Name }));
+        }
+
+    }
+
+    public class MultiSelectViewModel
+    {
+        public IEnumerable<Account> Accounts { get; set; }
+        public IEnumerable<Organization> Organizations { get; set; }
+        public IEnumerable<Vendor> Vendors { get; set; }
+
+        public static MultiSelectViewModel Create(IRepository repository)
+        {
+            string[] accts = { "3-6851000", "3-APSAC37", "3-APSAR24" };
+            var accounts = repository.OfType<Account>().Queryable.Where(a => accts.Contains(a.Id));
+
+            string[] orgs = {"AARC", "ACCR", "AAES"};
+            var oganizations = repository.OfType<Organization>().Queryable.Where(a => orgs.Contains(a.Id));
+
+            string[] vends = { "0000006853", "0000006859", "0000008583" };
+            var vendors = repository.OfType<Vendor>().Queryable.Where(a => vends.Contains(a.Id));
+
+            var viewModel = new MultiSelectViewModel()
+                                {
+                                    Accounts = accounts.ToList(),
+                                    Organizations = oganizations.ToList(),
+                                    Vendors = vendors.ToList()
+                                };
+
+            return viewModel;
+        }
     }
 
 }
