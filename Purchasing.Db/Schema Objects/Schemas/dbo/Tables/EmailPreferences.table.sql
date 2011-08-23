f95b3dbc409a855056db211926f8d5e3786c83aa
Scott Kirkland <srkirkland@gmail.com>
2011-08-23 13:00:20 -0700

    updated the email preferences table to include all of the desired fields

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/EmailPreferences.table.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[EmailPreferences] (
+    [Id]                               VARCHAR (10) NOT NULL,
+    [RequesterOrderSubmission]         BIT          NOT NULL,
+    [RequesterApproverApproved]        BIT          NOT NULL,
+    [RequesterApproverChanged]         BIT          NOT NULL,
+    [RequesterAccountManagerApproved]  BIT          NOT NULL,
+    [RequesterAccountManagerChanged]   BIT          NOT NULL,
+    [RequesterPurchaserAction]         BIT          NOT NULL,
+    [RequesterPurchaserChanged]        BIT          NOT NULL,
+    [RequesterKualiProcessed]          BIT          NOT NULL,
+    [RequesterKualiApproved]           BIT          NOT NULL,
+    [ApproverAccountManagerApproved]   BIT          NOT NULL,
+    [ApproverAccountManagerDenied]     BIT          NOT NULL,
+    [ApproverKualiApproved]            BIT          NOT NULL,
+    [ApproverPurchaserProcessed]       BIT          NOT NULL,
+    [ApproverOrderCompleted]           BIT          NOT NULL,
+    [AccountManagerKualiApproved]      BIT          NOT NULL,
+    [AccountManagerOrderCompleted]     BIT          NOT NULL,
+    [AccountManagerPurchaserProcessed] BIT          NOT NULL,
+    [PurchaserKualiApproved]           BIT          NOT NULL,
+    [PurchaserOrderCompleted]          BIT          NOT NULL,
+    [NotificationType]                 VARCHAR (50) NOT NULL
+);
+
