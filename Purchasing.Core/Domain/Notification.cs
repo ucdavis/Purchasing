116a170e997a4adc5b3b69f24591f4473fbe8d0e
Scott Kirkland <srkirkland@gmail.com>
2011-08-23 14:06:27 -0700

    create notification class for placing user notifications in the queue

---
+++ Purchasing.Core/Domain/Notification.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
+
+namespace Purchasing.Core.Domain
+{
+    public class Notification : DomainObjectWithTypedId<Guid>
+    {
+        public virtual User User { get; set; }
+        public virtual DateTime Created { get; set; }
+        public virtual DateTime? Sent { get; set; }
+        public virtual string Status { get; set; }
+        public virtual bool Pending { get; set; }
+        public virtual bool PerEvent { get; set; }
+        public virtual bool Daily { get; set; }
+        public virtual bool Weekly { get; set; }
+    }
+
+    public class NotificationMap : ClassMap<Notification>
+    {
+        public NotificationMap()
+        {
+            Id(x => x.Id).GeneratedBy.GuidComb();
+
+            Map(x => x.Created);
+            Map(x => x.Sent).ReadOnly();
+            Map(x => x.Status).ReadOnly();
+            Map(x => x.Pending);
+            Map(x => x.PerEvent);
+            Map(x => x.Daily);
+            Map(x => x.Weekly);
+
+            References(x => x.User);
+        }
+    }
+}
