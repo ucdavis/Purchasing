ef1b203815212d4aff59a0090a7cbef13a455ac9
Scott Kirkland <srkirkland@gmail.com>
2011-08-23 14:11:26 -0700

    update the user table to ensure email is present and to add an away until date field, which is nullable in case of present

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Users.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Users.table.sql
@@ -2,9 +2,12 @@
     [Id]        VARCHAR (10) NOT NULL,
     [FirstName] VARCHAR (50) NOT NULL,
     [LastName]  VARCHAR (50) NOT NULL,
-    [Email]     VARCHAR (50) NULL,
+    [Email]     VARCHAR (50) NOT NULL,
+    [AwayUntil] DATETIME     NULL,
     [IsActive]  BIT          NOT NULL
 );
+
+
 
 
 
