ef9b61db9725ed10a682b7af111d419b5cc6a76a
Scott Kirkland <srkirkland@gmail.com>
2011-08-23 12:04:16 -0700

    Added tons of classes and mappings to email preferences

--- Purchasing.Core/Domain/EmailPreferences.cs
+++ Purchasing.Core/Domain/EmailPreferences.cs
@@ -9,8 +9,40 @@ namespace Purchasing.Core.Domain
         public EmailPreferences(string id) { Id = id; }
 
         public virtual bool RequesterOrderSubmission { get; set; }
+
         public virtual bool RequesterApproverApproved { get; set; }
         public virtual bool RequesterApproverChanged { get; set; }
+
+        public virtual bool RequesterAccountManagerApproved { get; set; }
+        public virtual bool RequesterAccountManagerChanged { get; set; }
+
+        public virtual bool RequesterPurchaserAction { get; set; }
+        public virtual bool RequesterPurchaserChanged { get; set; }
+
+        public virtual bool RequesterKualiProcessed { get; set; }
+        public virtual bool RequesterKualiApproved { get; set; }
+
+        public virtual bool ApproverAccountManagerApproved { get; set; }
+        public virtual bool ApproverAccountManagerDenied { get; set; }
+        public virtual bool ApproverPurchaserProcessed { get; set; }
+        public virtual bool ApproverKualiApproved { get; set; }
+        public virtual bool ApproverOrderCompleted { get; set; }
+
+        public virtual bool AccountManagerPurchaserProcessed { get; set; }
+        public virtual bool AccountManagerKualiApproved { get; set; }
+        public virtual bool AccountManagerOrderCompleted { get; set; }
+
+        public virtual bool PurchaserKualiApproved { get; set; }
+        public virtual bool PurchaserOrderCompleted { get; set; }
+
+        public virtual string NotificationType { get; set; }
+
+        public enum NotificationTypes
+        {
+            PerEvent,
+            Daily,
+            Weekly
+        }
     }
 
     public class EmailPreferencesMap : ClassMap<EmailPreferences>
@@ -22,6 +54,25 @@ namespace Purchasing.Core.Domain
             Map(x => x.RequesterOrderSubmission);
             Map(x => x.RequesterApproverApproved);
             Map(x => x.RequesterApproverChanged);
+            Map(x => x.RequesterAccountManagerApproved);
+            Map(x => x.RequesterAccountManagerChanged);
+            Map(x => x.RequesterPurchaserAction);
+            Map(x => x.RequesterPurchaserChanged);
+            Map(x => x.RequesterKualiProcessed); 
+            Map(x => x.RequesterKualiApproved);
+
+            Map(x => x.ApproverAccountManagerApproved);
+            Map(x => x.ApproverAccountManagerDenied);
+            Map(x => x.ApproverKualiApproved);
+            Map(x => x.ApproverPurchaserProcessed);
+            Map(x => x.ApproverOrderCompleted);
+
+            Map(x => x.AccountManagerKualiApproved);
+            Map(x => x.AccountManagerOrderCompleted);
+            Map(x => x.AccountManagerPurchaserProcessed);
+
+            Map(x => x.PurchaserKualiApproved);
+            Map(x => x.PurchaserOrderCompleted);
         }
     }
 }
