d00f61dc5e6d0e63933534fb6cf83be897e078e2
Scott Kirkland <srkirkland@gmail.com>
2011-08-24 10:30:24 -0700

    fixed the user class to not even map one-to-one to email preference since it was causing issues.  We'll just query for it when needed

--- Purchasing.Core/Domain/User.cs
+++ Purchasing.Core/Domain/User.cs
@@ -50,7 +50,7 @@ namespace Purchasing.Core.Domain
         
         public virtual bool IsActive { get; set; }
 
-        public virtual EmailPreferences EmailPreferences { get; set; }
+        //public virtual EmailPreferences EmailPreferences { get; set; }
 
         public virtual IList<Organization> Organizations { get; set; }
         public virtual IList<Role> Roles { get; set; }
@@ -68,7 +68,7 @@ namespace Purchasing.Core.Domain
             Map(x => x.AwayUntil);
             Map(x => x.IsActive);
 
-            HasOne(x => x.EmailPreferences).Constrained().LazyLoad().Cascade.SaveUpdate();
+            //HasOne(x => x.EmailPreferences).Constrained().LazyLoad().Cascade.SaveUpdate();
 
             HasManyToMany(x => x.Organizations).Table("UsersXOrganizations").ParentKeyColumn("UserID").ChildKeyColumn("OrganizationID");
             HasManyToMany(x => x.Roles).Table("Permissions").ChildKeyColumn("RoleID").ParentKeyColumn("UserID");
