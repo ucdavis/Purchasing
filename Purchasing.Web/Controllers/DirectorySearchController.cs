5c84f5c6fddbd88d54ea9c3b0c0872e32146474b
Scott Kirkland <srkirkland@gmail.com>
2011-08-24 09:36:56 -0700

    added a directory search controller which can ajax search for a user

---
+++ Purchasing.Web/Controllers/DirectorySearchController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using Purchasing.Web.Services;
+using UCDArch.Web.ActionResults;
+
+namespace Purchasing.Web.Controllers
+{
+    /// <summary>
+    /// Controller for the DirectorySearch class
+    /// </summary>
+    public class DirectorySearchController : ApplicationController
+    {
+        private readonly IDirectorySearchService _directorySearchService;
+
+
+        public DirectorySearchController(IDirectorySearchService directorySearchService)
+        {
+            _directorySearchService = directorySearchService;
+        }
+
+        /// <summary>
+        /// Returns LDAP person or null, given either email or login id for the user 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonNetResult FindPerson(string loginOrEmail)
+        {
+            var user = _directorySearchService.FindUser(loginOrEmail);
+
+            return new JsonNetResult(user);
+        }
+    }
+}
