6c94618ca7c7b357e2092178525e832949a45183
Scott Kirkland <srkirkland@gmail.com>
2011-08-25 14:00:50 -0700

    added commodity code view tables and added 2 groups and 40 commodities to the startup script

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/vCommodityGroups.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[vCommodityGroups] (
+    [Id]           VARCHAR (4)  NOT NULL,
+    [Name]         VARCHAR (40) NOT NULL,
+    [SubGroupCode] VARCHAR (2)  NOT NULL,
+    [SubGroupName] VARCHAR (40) NOT NULL
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Commodity group code', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'vCommodityGroups', @level2type = N'COLUMN', @level2name = N'Id';
+
