6f22701b03fa210aec173b95931855393d6632f8
Alan Lai <anlai@ucdavis.edu>
2011-08-25 14:35:20 -0700

    added email queue table

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/EmailQueue.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[EmailQueue] (
+    [Id]               UNIQUEIDENTIFIER NOT NULL,
+    [UserId]           VARCHAR (10)     NOT NULL,
+    [Text]             VARCHAR (MAX)    NOT NULL,
+    [OrderId]          INT              NOT NULL,
+    [DateTimeCreated]  DATETIME         NOT NULL,
+    [Pending]          BIT              NOT NULL,
+    [DateTimeSent]     DATETIME         NULL,
+    [Status]           VARCHAR (MAX)    NULL,
+    [Email]            VARCHAR (100)    NULL,
+    [NotificationType] VARCHAR (50)     NOT NULL
+);
+
