005b138d151f573da4c1797851f18c0fea1b478f
Alan Lai <anlai@ucdavis.edu>
2011-08-25 14:45:56 -0700

    updated fields to datetime2

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/EmailQueue.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/EmailQueue.table.sql
@@ -3,11 +3,13 @@
     [UserId]           VARCHAR (10)     NOT NULL,
     [Text]             VARCHAR (MAX)    NOT NULL,
     [OrderId]          INT              NOT NULL,
-    [DateTimeCreated]  DATETIME         NOT NULL,
+    [DateTimeCreated]  DATETIME2 (7)    NOT NULL,
     [Pending]          BIT              NOT NULL,
-    [DateTimeSent]     DATETIME         NULL,
+    [DateTimeSent]     DATETIME2 (7)    NULL,
     [Status]           VARCHAR (MAX)    NULL,
     [Email]            VARCHAR (100)    NULL,
     [NotificationType] VARCHAR (50)     NOT NULL
 );
+
+
 
