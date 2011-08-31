a5938bb51d36e30f7484dc9060f473836a6497e8
Alan Lai <anlai@ucdavis.edu>
2011-08-31 15:50:15 -0700

    updated db to support email rpeferences and what not when sending

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/EmailPreferences.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/EmailPreferences.table.sql
@@ -19,6 +19,9 @@
     [AccountManagerPurchaserProcessed] BIT          NOT NULL,
     [PurchaserKualiApproved]           BIT          NOT NULL,
     [PurchaserOrderCompleted]          BIT          NOT NULL,
-    [NotificationType]                 VARCHAR (50) NOT NULL
+    [NotificationType]                 VARCHAR (50) NOT NULL,
+    [UserId]                           VARCHAR (10) NOT NULL
 );
+
+
 
