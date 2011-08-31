1ee1c9e4b47f524e65129ad174fbde3d82cdad07
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-08-31 11:38:20 -0700

    Fixed missing stuff

---
+++ Purchasing.Tests/RepositoryTests/AccountRepositoryTests.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Purchasing.Core.Domain;
+using Purchasing.Tests.Core;
+//using Purchasing.Tests.Core.Extensions;
+//using Purchasing.Tests.Core.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentNHibernate.Testing;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Data.NHibernate;
+using UCDArch.Testing.Extensions;
+
+namespace Purchasing.Tests.RepositoryTests
+{
+    /// <summary>
+    /// Entity Name:		Account
+    /// LookupFieldName:	Name
+    /// </summary>
+    [TestClass]
+    public class AccountRepositoryTests : AbstractRepositoryTests<Account, string, AccountMap>
+    {
+        /// <summary>
+        /// Gets or sets the Account repository.
+        /// </summary>
+        /// <value>The Account repository.</value>
+        public IRepository<Account> AccountRepository { get; set; }
+		
+        #region Init and Overrides
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccountRepositoryTests"/> class.
+        /// </summary>
+        public AccountRepositoryTests()
+        {
+            AccountRepository = new Repository<Account>();
+        }
+
+        /// <summary>
+        /// Gets the valid entity of type T
+        /// </summary>
+        /// <param name="counter">The counter.</param>
+        /// <returns>A valid entity of type T</returns>
+        protected override Account GetValid(int? counter)
+        {
+            return CreateValidEntities.Account(counter);
+        }
+
+        /// <summary>
+        /// A Query which will return a single record
+        /// </summary>
+        /// <param name="numberAtEnd"></param>
+        /// <returns></returns>
+        protected override IQueryable<Account> GetQuery(int numberAtEnd)
+        {
+            return AccountRepository.Queryable.Where(a => a.Name.EndsWith(numberAtEnd.ToString()));
+        }
+
+        /// <summary>
+        /// A way to compare the entities that were read.
+        /// For example, this would have the assert.AreEqual("Comment" + counter, entity.Comment);
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="counter"></param>
+        protected override void FoundEntityComparison(Account entity, int counter)
+        {
+            Assert.AreEqual("Name" + counter, entity.Name);
+        }
+
+        /// <summary>
+        /// Updates , compares, restores.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <param name="action">The action.</param>
+        protected override void UpdateUtility(Account entity, ARTAction action)
+        {
+            const string updateValue = "Updated";
+            switch (action)
+            {
+                case ARTAction.Compare:
+                    Assert.AreEqual(updateValue, entity.Name);
+                    break;
+                case ARTAction.Restore:
+                    entity.Name = RestoreValue;
+                    break;
+                case ARTAction.Update:
+                    RestoreValue = entity.Name;
+                    entity.Name = updateValue;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Loads the data.
+        /// </summary>
+        protected override void LoadData()
+        {
+            AccountRepository.DbContext.BeginTransaction();
+            LoadRecords(5);
+            AccountRepository.DbContext.CommitTransaction();
+        }
+
+        #endregion Init and Overrides	
+        
+        
+        
+        
+        
+        
+        #region Reflection of Database.
+
+        /// <summary>
+        /// Tests all fields in the database have been tested.
+        /// If this fails and no other tests, it means that a field has been added which has not been tested above.
+        /// </summary>
+        [TestMethod]
+        public void TestAllFieldsInTheDatabaseHaveBeenTested()
+        {
+            #region Arrange
+            var expectedFields = new List<NameAndType>();
+
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>
+            {
+                "[Newtonsoft.Json.JsonPropertyAttribute()]", 
+                "[System.Xml.Serialization.XmlIgnoreAttribute()]"
+            }));
+
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(Account));
+
+        }
+
+        #endregion Reflection of Database.	
+		
+		
+    }
+}
