0061dbb9ef24f59829d73f40f9d6d1d84cfc11ff
Alan Lai <anlai@ucdavis.edu>
2011-08-31 10:54:58 -0700

    updated order status codes

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Constraints/DF_OrderStatusCodes_IsComplete_2.defconst.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[OrderStatusCodes]
+    ADD CONSTRAINT [DF_OrderStatusCodes_IsComplete] DEFAULT ((0)) FOR [IsComplete];
+
---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_Approvals_OrderStatusCodes.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[Approvals]
+    ADD CONSTRAINT [FK_Approvals_OrderStatusCodes] FOREIGN KEY ([OrderStatusCodeId]) REFERENCES [dbo].[OrderStatusCodes] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
