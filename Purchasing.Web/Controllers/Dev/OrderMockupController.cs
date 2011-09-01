eb80d3b5a6c4dc37beb5a3d9466d823c7fdb3f8d
Alan Lai <anlai@ucdavis.edu>
2011-09-01 07:55:12 -0700

    moved development controllers into folder to seperate from final ones

---
+++ Purchasing.Web/Controllers/Dev/OrderMockupController.cs
@@ -0,0 +1,52 @@
+﻿
+
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using Purchasing.Core.Domain;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+using UCDArch.Web.Attributes;
+
+namespace Purchasing.Web.Controllers
+{
+    /// <summary>
+    /// Controller for the OrderMockup class
+    /// </summary>
+    public class OrderMockupController : ApplicationController
+    {
+        private readonly IRepository<Order> _orderRepository;
+
+        public OrderMockupController(IRepository<Order> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        //
+        // GET: /OrderMockup/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public new ActionResult Request()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [BypassAntiForgeryToken]
+        public ActionResult AddVendor()
+        {
+            return Json(new {id = new Random().Next(100)});
+        }
+        
+        [HttpPost]
+        [BypassAntiForgeryToken]
+        public ActionResult AddAddress()
+        {
+            return Json(new { id = new Random().Next(100) });
+        }
+    }
+}
