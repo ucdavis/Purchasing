1dce1e25f3e118373318d8d2edf80869cca66c1c
Scott Kirkland <srkirkland@gmail.com>
2011-09-01 15:10:50 -0700

    removed redundant userid on email preferences table

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_EmailPreferences_Users.fkey.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_EmailPreferences_Users.fkey.sql
@@ -1,5 +1,5 @@
-﻿ALTER TABLE [dbo].[EmailPreferences]
-    ADD CONSTRAINT [FK_EmailPreferences_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+﻿/*ALTER TABLE [dbo].[EmailPreferences]
+    ADD CONSTRAINT [FK_EmailPreferences_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;*/
 
 
 
