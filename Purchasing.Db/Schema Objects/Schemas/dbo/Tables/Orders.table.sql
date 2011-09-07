4aae22496265b6581b996ebe76f50ebdb904ed65
Scott Kirkland <srkirkland@gmail.com>
2011-09-07 10:30:30 -0700

    updated the order table to use a proper name for last status id and also added an FK from orders to approvals

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Orders.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Orders.table.sql
@@ -1,19 +1,21 @@
 ﻿CREATE TABLE [dbo].[Orders] (
-    [Id]                    INT           IDENTITY (1, 1) NOT NULL,
-    [OrderTypeId]           CHAR (3)      NOT NULL,
-    [VendorId]              INT           NOT NULL,
-    [AddressId]             INT           NOT NULL,
-    [ShippingTypeId]        CHAR (2)      NULL,
-    [DateNeeded]            DATETIME2 (7) NULL,
-    [AllowBackorder]        BIT           NOT NULL,
-    [EstimatedTax]          FLOAT         NULL,
-    [WorkgroupId]           INT           NOT NULL,
-    [OrganizationId]        CHAR (4)      NOT NULL,
-    [PONumber]              VARCHAR (50)  NULL,
-    [LastCompletedApproval] INT           NULL,
-    [ShippingAmount]        MONEY         NULL,
-    [OrderStatusCodeId]     CHAR (2)      NOT NULL
+    [Id]                      INT           IDENTITY (1, 1) NOT NULL,
+    [OrderTypeId]             CHAR (3)      NOT NULL,
+    [VendorId]                INT           NOT NULL,
+    [AddressId]               INT           NOT NULL,
+    [ShippingTypeId]          CHAR (2)      NULL,
+    [DateNeeded]              DATETIME2 (7) NULL,
+    [AllowBackorder]          BIT           NOT NULL,
+    [EstimatedTax]            FLOAT         NULL,
+    [WorkgroupId]             INT           NOT NULL,
+    [OrganizationId]          CHAR (4)      NOT NULL,
+    [PONumber]                VARCHAR (50)  NULL,
+    [LastCompletedApprovalId] INT           NULL,
+    [ShippingAmount]          MONEY         NULL,
+    [OrderStatusCodeId]       CHAR (2)      NOT NULL
 );
+
+
 
 
 
