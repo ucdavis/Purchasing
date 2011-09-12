9e408dfe6f305e553848c48349ac4608b26bfa1c
Scott Kirkland <srkirkland@gmail.com>
2011-09-12 15:19:21 -0700

    created a new repository factory which contains some other repos, and maybe eventually will contain them all.  reduces the need to declare tons of repos, including correct id types, when you use lots of repos in one controller

---
+++ Purchasing.Core/IRepositoryFactory.cs
@@ -0,0 +1,31 @@
+﻿using Purchasing.Core.Domain;
+using UCDArch.Core.PersistanceSupport;
+
+namespace Purchasing.Core
+{
+    public interface IRepositoryFactory
+    {
+        IRepository<AutoApproval> AutoApprovalRepository { get; set; }
+        IRepository<ConditionalApproval> ConditionalApprovalRepository { get; set; }
+        IRepository<Order> OrderRepository { get; set; }
+        IRepository<Workgroup> WorkgroupRepository { get; set; }
+        IRepository<WorkgroupAccount> WorkgroupAccountRepository { get; set; }
+        IRepositoryWithTypedId<Account, string> AccountRepository { get; set; }
+        IRepositoryWithTypedId<User, string> UserRepository { get; set; }
+    }
+
+    public class RepositoryFactory : IRepositoryFactory
+    {
+        #region IRepositoryFactory Members
+
+        public IRepository<AutoApproval> AutoApprovalRepository { get; set; }
+        public IRepository<ConditionalApproval> ConditionalApprovalRepository { get; set; }
+        public IRepository<Order> OrderRepository { get; set; }
+        public IRepository<Workgroup> WorkgroupRepository { get; set; }
+        public IRepository<WorkgroupAccount> WorkgroupAccountRepository { get; set; }
+        public IRepositoryWithTypedId<Account, string> AccountRepository { get; set; }
+        public IRepositoryWithTypedId<User, string> UserRepository { get; set; }
+
+        #endregion
+    }
+}
--- Purchasing.Web/ComponentRegistrar.asax.cs
+++ Purchasing.Web/ComponentRegistrar.asax.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.Windsor;
+using Purchasing.Core;
 using UCDArch.Core.CommonValidator;
 using UCDArch.Core.DataAnnotationsValidator.CommonValidatorAdapter;
 using UCDArch.Core.PersistanceSupport;
@@ -15,6 +16,7 @@ namespace Purchasing.Web
         {
             AddGenericRepositoriesTo(container);
 
+            container.Register(Component.For<IRepositoryFactory>().ImplementedBy<RepositoryFactory>().Named("repositoryFactory"));
             container.Register(Component.For<IOrderService>().ImplementedBy<OrderService>().Named("orderService"));
             container.Register(Component.For<IDirectorySearchService>().ImplementedBy<DirectorySearchService>().Named("directorySearch"));
             container.Register(Component.For<IInterceptor>().ImplementedBy<AuditInterceptor>().Named("audit"));
