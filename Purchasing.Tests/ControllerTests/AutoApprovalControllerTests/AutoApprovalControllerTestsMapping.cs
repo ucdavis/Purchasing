c1dcb6b1f344454d5c65b4f1c9dbcf03de2de2ad
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-09-22 13:26:07 -0700

    Added AutoApproval Controller tests structure.

---
+++ Purchasing.Tests/ControllerTests/AutoApprovalControllerTests/AutoApprovalControllerTestsMapping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Purchasing.Web;
+using Purchasing.Core.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using Purchasing.Web.Controllers.Dev;
+using Rhino.Mocks;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Testing;
+using UCDArch.Web.Attributes;
+
+namespace Purchasing.Tests.ControllerTests.AutoApprovalControllerTests
+{
+    public partial class AutoApprovalControllerTests 
+    {
+        #region Mapping Tests
+        //[TestMethod]
+        //public void TestExampleMapping()
+        //{
+        //    "~/AutoApproval/YourMethod/".ShouldMapTo<AutoApprovalController>(a => a.YourMethod(null));
+        //}
+        #endregion Mapping Tests
+    }
+}
---
+++ Purchasing.Tests/ControllerTests/AutoApprovalControllerTests/AutoApprovalControllerTestsPart01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Purchasing.Web;
+using Purchasing.Core.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using Purchasing.Web.Controllers.Dev;
+using Rhino.Mocks;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Testing;
+using UCDArch.Web.Attributes;
+
+namespace Purchasing.Tests.ControllerTests.AutoApprovalControllerTests
+{
+    public partial class AutoApprovalControllerTests
+    {
+        #region Method Tests
+
+        [TestMethod]
+        public void TestWriteMethodTests()
+        {
+            #region Arrange
+            Assert.Inconclusive("Need to write these tests");
+            #endregion Arrange
+
+            #region Act
+
+            #endregion Act
+
+            #region Assert
+
+            #endregion Assert
+        }
+        #endregion Method Tests
+    }
+}
