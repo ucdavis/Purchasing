4bf6f46a2332a74edffc1345d394ba975bff447a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-09-29 11:50:26 -0700

    Group users together, show all roles. Code may need optimizing

--- Purchasing.Web/Controllers/Dev/JandJWorkgroupManagementController.cs
+++ Purchasing.Web/Controllers/Dev/JandJWorkgroupManagementController.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web.Mvc;
 using AutoMapper;
+using Castle.Components.DictionaryAdapter;
 using Purchasing.Web.Services;
 using Purchasing.Web.Utility;
 using UCDArch.Core.PersistanceSupport;
@@ -433,10 +434,11 @@ namespace Purchasing.Web.Controllers
     public class WorgroupPeopleListModel
     {
         public Workgroup Workgroup { get; set; }
-        public IEnumerable<WorkgroupPermission> WorkgroupPermissions { get; set; }
+        //public IEnumerable<WorkgroupPermission> WorkgroupPermissions { get; set; }
         public Role Role { get; set; }
         public List<IdAndName> Users { get; set; }
         public List<Role> Roles { get; set; }
+        public List<UserRoles> UserRoles { get; set; } 
 
         /// <summary>
         /// Create ViewModel
@@ -446,7 +448,7 @@ namespace Purchasing.Web.Controllers
         /// <param name="workgroup"></param>
         /// <param name="rolefilter">Role Id</param>
         /// <returns></returns>
-        public  static WorgroupPeopleListModel Create(IRepository repository, IRepositoryWithTypedId<Role, string> roleRepository ,Workgroup workgroup, string rolefilter)
+        public static WorgroupPeopleListModel Create(IRepository repository, IRepositoryWithTypedId<Role, string> roleRepository ,Workgroup workgroup, string rolefilter)
         {
             Check.Require(repository != null);
             Check.Require(roleRepository != null);
@@ -454,18 +456,56 @@ namespace Purchasing.Web.Controllers
 
             var viewModel = new WorgroupPeopleListModel()
                                 {
-                                    Workgroup = workgroup
+                                    Workgroup = workgroup,
+                                    UserRoles = new List<UserRoles>()
                                 };
-            viewModel.WorkgroupPermissions =
-                repository.OfType<WorkgroupPermission>().Queryable.Where(a => a.Workgroup == workgroup && a.User.IsActive);
-            if (!string.IsNullOrWhiteSpace(rolefilter))
+            var allWorkgroupPermissions =
+                repository.OfType<WorkgroupPermission>().Queryable.Where(a => a.Workgroup == workgroup && a.User.IsActive && a.Role.Level >= 1 && a.Role.Level <= 4);
+
+
+            var workgroupPermissions = !string.IsNullOrWhiteSpace(rolefilter) ? allWorkgroupPermissions.Where(a => a.Role.Id == rolefilter) : allWorkgroupPermissions;
+            foreach(var workgroupPermission in allWorkgroupPermissions)
             {
-                viewModel.WorkgroupPermissions = viewModel.WorkgroupPermissions.Where(a => a.Role.Id == rolefilter);
+                if(workgroupPermissions.Where(a => a.User.Id == workgroupPermission.User.Id).Any())
+                {
+                    if (viewModel.UserRoles.Where(a => a.User.Id == workgroupPermission.User.Id).Any())
+                    {
+                        viewModel.UserRoles.Where(a => a.User.Id == workgroupPermission.User.Id).Single().Roles.Add(workgroupPermission.Role);
+                    }
+                    else
+                    {
+                        viewModel.UserRoles.Add(new UserRoles(workgroupPermission));
+                    }
+                }
             }
+
             viewModel.Roles = roleRepository.Queryable.Where(a => a.Level >= 1 && a.Level <= 4).ToList();
             return viewModel;
         }
 
+    }
+
+    public class UserRoles
+    {
+        public User User { get; set; }
+        public IList<Role> Roles { get; set; }
+        public int FirstWorkgroupPermissionId { get; set; }
+
+        public UserRoles(WorkgroupPermission workgroupPermission)
+        {
+            User = workgroupPermission.User;
+            FirstWorkgroupPermissionId = workgroupPermission.Id;
+            Roles = new List<Role>();
+            Roles.Add(workgroupPermission.Role);
+        }
+
+        public string RolesList
+        {
+            get
+            {
+                return string.Join(" ", Roles.Select(a => a.Name).ToArray());
+            } 
+        }
     }
 
     public class WorgroupPeopleCreateModel
@@ -487,11 +527,6 @@ namespace Purchasing.Web.Controllers
                 Workgroup = workgroup
             };
             
-            //viewModel.Roles = new List<Role>();
-            //viewModel.Roles.Add(roleRepository.GetNullableById(Role.Codes.AccountManager));
-            //viewModel.Roles.Add(roleRepository.GetNullableById(Role.Codes.Purchaser));
-            //viewModel.Roles.Add(roleRepository.GetNullableById(Role.Codes.Approver));
-            //viewModel.Roles.Add(roleRepository.GetNullableById(Role.Codes.Requester));
             viewModel.Roles = roleRepository.Queryable.Where(a => a.Level >= 1 && a.Level <= 4).ToList();
             return viewModel;
         }
