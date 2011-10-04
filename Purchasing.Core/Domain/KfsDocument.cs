134433c2c29feb99c1464003b5358fea95866301
Alan Lai <anlai@ucdavis.edu>
2011-10-04 10:26:36 -0700

    added support for kfs documents to an order.

---
+++ Purchasing.Core/Domain/KfsDocument.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
+
+namespace Purchasing.Core.Domain
+{
+    public class KfsDocument : DomainObject
+    {
+        public string DocNumber { get; set; }
+
+        public Order Order { get; set; }
+    }
+
+    public class KfsDocumentMap : ClassMap<KfsDocument>
+    {
+        public KfsDocumentMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.DocNumber);
+
+            References(x => x.Order);
+        }
+    }
+}
