8a051dfc2f5064cea45b73c51d9cf4317dd02c1e
James Cubbage <jscubbage@ucdavis.edu>
2011-10-05 15:11:40 -0700

    Finishing touchs of workgroup addess: added Stats to Domain, added drop down selector for State to partial view, updated partial list both state code and name.

---
+++ Purchasing.Core/Domain/State.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
+
+namespace Purchasing.Core.Domain
+{
+    public class State : DomainObjectWithTypedId<string>
+    {
+        [Required]
+        [StringLength(50)]
+        public virtual string Name { get; set; }
+
+    }
+
+    public class StateMap : ClassMap<State>
+    {
+        public StateMap()
+        {
+            ReadOnly();
+
+            Id(a => a.Id).GeneratedBy.Assigned();
+
+            Map(a => a.Name);
+        }
+    }
+}
