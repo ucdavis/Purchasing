c2b644e3443f375ed7da8caf87fab37e6b0e764e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-10-10 13:55:27 -0700

    Changed Display name of Workgroup (PrimaryOrganization and IsActive). Tests.

--- Purchasing.Tests/ControllerTests/WorkgroupControllerTests/WorkgroupControllerTestsReflection.cs
+++ Purchasing.Tests/ControllerTests/WorkgroupControllerTests/WorkgroupControllerTestsReflection.cs
@@ -152,7 +152,7 @@ namespace Purchasing.Tests.ControllerTests.WorkgroupControllerTests
 
             #region Assert
             Assert.Inconclusive("Tests are still being written. When done, remove this line.");
-            Assert.AreEqual(10, result.Count(), "It looks like a method was added or removed from the controller.");
+            Assert.AreEqual(11, result.Count(), "It looks like a method was added or removed from the controller.");
             #endregion Assert
         }
 
@@ -361,6 +361,26 @@ namespace Purchasing.Tests.ControllerTests.WorkgroupControllerTests
 
             #region Act
             var allAttributes = controllerMethod.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(0, allAttributes.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Actions #5 (11)
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodEditGetContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = ControllerClass;
+            var controllerMethod = controllerClass.GetMethods().Where(a => a.Name == "Edit");
+            #endregion Arrange
+
+            #region Act
+            var allAttributes = controllerMethod.ElementAt(0).GetCustomAttributes(true);
             #endregion Act
 
             #region Assert
