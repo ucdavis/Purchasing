369f2838f87114441f34a380e89ea2fad65e1024
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-10-13 08:12:20 -0700

    Fixed up OrderAccess Service.

--- Purchasing.Web/Services/OrderAccess.cs
+++ Purchasing.Web/Services/OrderAccess.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Purchasing.Core.Domain;
+using UCDArch.Core.DomainModel;
 using UCDArch.Core.PersistanceSupport;
 
 namespace Purchasing.Web.Services
@@ -16,56 +17,39 @@ namespace Purchasing.Web.Services
     public class OrderAccess : IOrderAccess
     {
         private readonly IUserIdentity _userIdentity;
-        private readonly IRepository<Order> _orderRepository;
+        //private readonly IRepository<Order> _orderRepository;
         private readonly IRepository<Approval> _approvalRespository;
 
-        public OrderAccess(IUserIdentity userIdentity, IRepository<Order> orderRepository, IRepository<Approval> approvalRespository)
+        public OrderAccess(IUserIdentity userIdentity, IRepository<Approval> approvalRespository) //IRepository<Order> orderRepository,
         {
             _userIdentity = userIdentity;
-            _orderRepository = orderRepository;
+            //_orderRepository = orderRepository;
             _approvalRespository = approvalRespository;
         }
 
         public IList<Order> GetViewableOrders(OrderStatusCode statusCode)
         {
             var user = _userIdentity.Current;
-            var orderIds =
-                _approvalRespository.Queryable.Where(
-                    a => a.User != null && a.User.Id == user && !a.Approved && a.StatusCode == statusCode).Select(a => a.Order.Id).Distinct().ToList();
-            var orders = _orderRepository.Queryable.Where(a => orderIds.Contains(a.Id) && a.StatusCode == statusCode).ToList();
-            return orders;
+            //var orderIds =
+            //    _approvalRespository.Queryable.Where(
+            //        a => a.User != null && a.User.Id == user && !a.Approved && a.StatusCode == statusCode).Select(a => a.Order.Id).Distinct().ToList();
+            //var orders = _orderRepository.Queryable.Where(a => orderIds.Contains(a.Id) && a.StatusCode == statusCode).ToList();
 
+            var orders = _approvalRespository.Queryable.Where(a => a.User != null && a.User.Id == user && !a.Approved && a.StatusCode == statusCode)
+                .Select(a => a.Order).Where(b => b.StatusCode == statusCode).ToList();
+            return orders;
         }
 
         public IList<Order> GetViewableOrders(IList<OrderStatusCode> orderStatusCodesList )
         {
             var orders = new List<Order>();
+            var comparer = new BaseObjectEqualityComparer<Order>();
             foreach (var orderStatusCode in orderStatusCodesList)
             {
-                orders = orders.Union(GetViewableOrders(orderStatusCode), new OrderComparer()).ToList();
+                orders = orders.Union(GetViewableOrders(orderStatusCode), comparer).ToList();
             }
             return orders;
         }
 
-        private class OrderComparer : IEqualityComparer<Order>
-        {
-            public bool Equals(Order x, Order y)
-            {
-                if (x==null || y==null)
-                {
-                    return false;
-                }
-                if (x.Id == y.Id)
-                {
-                    return true;
-                }
-                return false;
-            }
-
-            public int GetHashCode(Order obj)
-            {
-                throw new NotImplementedException();
-            }
-        }
     }
 }
