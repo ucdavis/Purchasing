a17feeb41927f1f1b7a5829e737b2ac5a927f8d4
Scott Kirkland <srkirkland@gmail.com>
2011-10-14 10:22:10 -0700

    added the attachment/class/mapping and updated DB to use a guid

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Attachments.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Attachments.table.sql
@@ -1,12 +1,14 @@
 ﻿CREATE TABLE [dbo].[Attachments] (
-    [Id]          INT             IDENTITY (1, 1) NOT NULL,
-    [Filename]    VARCHAR (100)   NOT NULL,
-    [ContentType] VARCHAR (200)   NOT NULL,
-    [Contents]    VARBINARY (MAX) NOT NULL,
-    [OrderId]     INT             NOT NULL,
-    [DateCreated] DATETIME        NOT NULL,
-    [UserId]      VARCHAR (10)    NOT NULL
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [Filename]    VARCHAR (100)    NOT NULL,
+    [ContentType] VARCHAR (200)    NOT NULL,
+    [Contents]    VARBINARY (MAX)  NOT NULL,
+    [OrderId]     INT              NOT NULL,
+    [DateCreated] DATETIME         NOT NULL,
+    [UserId]      VARCHAR (10)     NOT NULL
 );
+
+
 
 
 
---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Constraints/DF_Attachments_Id.defconst.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[Attachments]
+    ADD CONSTRAINT [DF_Attachments_Id] DEFAULT (newid()) FOR [Id];
+
