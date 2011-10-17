97d608f52b11a7d443ec4f567a06e0cb47ab2ee8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-10-17 14:04:13 -0700

    Code cleanup. Tests

--- Purchasing.Tests/ControllerTests/WorkgroupControllerTests/WorkgroupControllerTestsWorkgroupAccountsPart02.cs
+++ Purchasing.Tests/ControllerTests/WorkgroupControllerTests/WorkgroupControllerTestsWorkgroupAccountsPart02.cs
@@ -129,5 +129,128 @@ namespace Purchasing.Tests.ControllerTests.WorkgroupControllerTests
             #endregion Assert		
         }
         #endregion EditAccount Get Tests
+
+        #region EditAccount Post Tests
+        [TestMethod]
+        public void TestEditAccountPostRedirectsIfAccountNotFound()
+        {
+            #region Arrange
+            new FakeWorkgroupAccounts(3, WorkgroupAccountRepository);
+            #endregion Arrange
+
+            #region Act
+            Controller.EditAccount(4, new WorkgroupAccount())
+                .AssertActionRedirect()
+                .ToAction<WorkgroupController>(a => a.Index());
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual("Account could not be found", Controller.ErrorMessage);
+            WorkgroupAccountRepository.AssertWasNotCalled(a => a.EnsurePersistent(Arg<WorkgroupAccount>.Is.Anything));
+            #endregion Assert
+        }
+
+
+
+        [TestMethod]
+        public void TestEditAccountPostRedirectsWhenValid()
+        {
+            #region Arrange
+            var accounts = new List<WorkgroupAccount>();
+            for(var i = 0; i < 3; i++)
+            {
+                accounts.Add(CreateValidEntities.WorkgroupAccount(i + 1));
+            }
+            accounts[2].Workgroup = CreateValidEntities.Workgroup(9);
+            accounts[2].Workgroup.SetIdTo(9);
+            accounts[2].Account.SetIdTo("blah");
+            accounts[2].AccountManager.SetIdTo("myAccMan");
+            accounts[2].Approver.SetIdTo("myApp");
+            accounts[2].Purchaser.SetIdTo("myPur");
+            new FakeWorkgroupAccounts(0, WorkgroupAccountRepository, accounts);
+
+            var accountToEdit = CreateValidEntities.WorkgroupAccount(8);
+            accountToEdit.SetIdTo(8);
+            accountToEdit.Workgroup = CreateValidEntities.Workgroup(7);
+            accountToEdit.Workgroup.SetIdTo(7);
+            accountToEdit.Account.SetIdTo("Eblah");
+            accountToEdit.AccountManager.SetIdTo("EmyAccMan");
+            accountToEdit.Approver.SetIdTo("EmyApp");
+            accountToEdit.Purchaser.SetIdTo("EmyPur");
+            #endregion Arrange
+
+            #region Act
+            var result = Controller.EditAccount(3, accountToEdit)
+                .AssertActionRedirect()
+                .ToAction<WorkgroupController>(a => a.Accounts(9));
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(9, result.RouteValues["id"]);
+            Assert.AreEqual("Workgroup account has been updated.", Controller.Message);
+
+            WorkgroupAccountRepository.AssertWasCalled(a => a.EnsurePersistent(Arg<WorkgroupAccount>.Is.Anything));
+            var args = (WorkgroupAccount) WorkgroupAccountRepository.GetArgumentsForCallsMadeOn(a => a.EnsurePersistent(Arg<WorkgroupAccount>.Is.Anything))[0][0]; 
+            Assert.IsNotNull(args);
+            Assert.AreEqual(9, args.Workgroup.Id); //Did not change
+            Assert.AreEqual(3, args.Id); //Did not change
+            Assert.AreEqual("Eblah", args.Account.Id);
+            Assert.AreEqual("EmyAccMan", args.AccountManager.Id);
+            Assert.AreEqual("EmyApp", args.Approver.Id);
+            Assert.AreEqual("EmyPur", args.Purchaser.Id);
+            #endregion Assert		
+        }
+
+
+        [TestMethod]
+        public void TestEditAccountPostWhenNotValidReturnsView()
+        {
+            #region Arrange
+            SetupDataForAccounts1();
+            var accounts = new List<WorkgroupAccount>();
+            for(var i = 0; i < 3; i++)
+            {
+                accounts.Add(CreateValidEntities.WorkgroupAccount(i + 1));
+            }
+            accounts[2].Workgroup = WorkgroupRepository.GetNullableById(3);
+            accounts[2].Account.SetIdTo("blah");
+            accounts[2].AccountManager.SetIdTo("myAccMan");
+            accounts[2].Approver.SetIdTo("myApp");
+            accounts[2].Purchaser.SetIdTo("myPur");
+            new FakeWorkgroupAccounts(0, WorkgroupAccountRepository, accounts);
+
+            var accountToEdit = CreateValidEntities.WorkgroupAccount(8);
+            accountToEdit.SetIdTo(8);
+            accountToEdit.Workgroup = CreateValidEntities.Workgroup(7);
+            accountToEdit.Workgroup.SetIdTo(7);
+            //accountToEdit.Account.SetIdTo("Eblah");
+            accountToEdit.Account = null;
+            //accountToEdit.AccountManager.SetIdTo("EmyAccMan");
+            accountToEdit.AccountManager = null;
+            accountToEdit.Approver.SetIdTo("EmyApp");
+            accountToEdit.Purchaser.SetIdTo("EmyPur");
+            #endregion Arrange
+
+            #region Act
+            var result = Controller.EditAccount(3, accountToEdit)
+                .AssertViewRendered()
+                .WithViewData<WorkgroupAccountModel>();
+            #endregion Act
+
+            #region Assert
+            Controller.ModelState.AssertErrorsAre("The Account field is required.");
+            Assert.IsNotNull(result);
+            Assert.AreEqual(10, result.Accounts.Count());
+            Assert.AreEqual(12, result.WorkGroupPermissions.Count());
+            Assert.AreEqual(1, result.Approvers.Count());
+            Assert.AreEqual(1, result.AccountManagers.Count());
+            Assert.AreEqual(1, result.Purchasers.Count());
+
+            Assert.IsNull(result.WorkgroupAccount.Account);
+            Assert.IsNull(result.WorkgroupAccount.AccountManager);
+            #endregion Assert		
+        }
+        #endregion EditAccount Post Tests
     }
 }
