077fe93d844bf6240c2021e32ce0827e97b66751
James Cubbage <jscubbage@ucdavis.edu>
2011-10-19 14:37:17 -0700

    Cont. order view page

--- Purchasing.Web/Controllers/Dev/JamesTestController.cs
+++ Purchasing.Web/Controllers/Dev/JamesTestController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using Purchasing.Core.Domain;
 using Purchasing.Web.Services;
 using UCDArch.Core.PersistanceSupport;
 using UCDArch.Core.Utils;
 
-namespace Purchasing.Web.Controllers.Dev
+namespace Purchasing.Web.Controllers
 {
     /// <summary>
     /// Controller for the JamesTest class
@@ -19,14 +21,48 @@ namespace Purchasing.Web.Controllers.Dev
             _orderAccessService = orderAccessService;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string[] statusFilter, DateTime? startDate, DateTime? endDate, bool showall = false)
         {
-            var orders = _orderAccessService.GetListofOrders();
+            if(statusFilter==null)
+            {
+                statusFilter = new string[0];
+            }
+            var filters = statusFilter.ToList();
 
-            return View(orders);
+            var orders = _orderAccessService.GetListofOrders();
+            var viewModel = FilteredOrderListModel.Create(Repository, orders);
+            viewModel.CheckedOrderStatusCodes = filters;
+            viewModel.StartDate = startDate;
+            viewModel.EndDate = endDate;
+            viewModel.Showall = showall;
+
+            return View(viewModel);
         }
 
     }
 
+    public class FilteredOrderListModel
+    {
+        public List<OrderStatusCode> OrderStatusCodes { get; set; }
+        public IList<Order> Orders { get; set; }
+        public List<string> CheckedOrderStatusCodes { get; set; }
+        public bool Showall { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public static FilteredOrderListModel Create(IRepository repository, IList<Order> orders)
+        {
+            Check.Require(repository != null, "Repository must be supplied");
+
+            var viewModel = new FilteredOrderListModel
+            {
+                Orders = orders
+            };
+            viewModel.OrderStatusCodes = repository.OfType<OrderStatusCode>().Queryable.ToList();
+            viewModel.CheckedOrderStatusCodes = new List<string>();
+
+            return viewModel;
+        }
+    }
 	
 }
