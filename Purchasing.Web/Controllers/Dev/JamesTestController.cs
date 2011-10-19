6b53855827ff38dc00a379dcd4dd2b2d12a9a154
James Cubbage <jscubbage@ucdavis.edu>
2011-10-19 11:08:05 -0700

    Setting up view orders page using Alans new service.

---
+++ Purchasing.Web/Controllers/Dev/JamesTestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Purchasing.Web.Services;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+
+namespace Purchasing.Web.Controllers.Dev
+{
+    /// <summary>
+    /// Controller for the JamesTest class
+    /// </summary>
+    public class JamesTestController : ApplicationController
+    {
+	    private readonly IOrderAccessService _orderAccessService;
+
+        public JamesTestController(IOrderAccessService orderAccessService)
+        {
+            _orderAccessService = orderAccessService;
+        }
+
+        public ActionResult Index()
+        {
+            var orders = _orderAccessService.GetListofOrders();
+
+            return View(orders);
+        }
+
+    }
+
+	
+}
