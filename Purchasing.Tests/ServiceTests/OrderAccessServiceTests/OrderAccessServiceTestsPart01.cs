e9ae20c807169012c58958de9fb50774567387af
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-10-21 14:41:18 -0700

    WIP OrderAccessServiceTests

--- Purchasing.Tests/ServiceTests/OrderAccessServiceTests/OrderAccessServiceTestsPart01.cs
+++ Purchasing.Tests/ServiceTests/OrderAccessServiceTests/OrderAccessServiceTestsPart01.cs
@@ -8,6 +8,7 @@ using Rhino.Mocks;
 
 namespace Purchasing.Tests.ServiceTests.OrderAccessServiceTests
 {
+
     public partial class OrderAccessServiceTests
     {
         [TestMethod]
@@ -92,6 +93,9 @@ namespace Purchasing.Tests.ServiceTests.OrderAccessServiceTests
             #endregion Assert
         }
 
+        /// <summary>
+        /// Same as the one above but new way of setting data
+        /// </summary>
         [TestMethod]
         public void TestUnCompletedOrdersForApprover2()
         {
@@ -117,6 +121,207 @@ namespace Purchasing.Tests.ServiceTests.OrderAccessServiceTests
             Assert.AreEqual("bender", results[0].CreatedBy.Id);
             Assert.AreEqual("moe", results[5].CreatedBy.Id);
             #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestUnCompletedOrdersForApprover3()
+        {
+            #region Arrange
+            UserIdentity.Expect(a => a.Current).Return("hsimpson").Repeat.Any();
+            SetupUsers1();
+            SetupWorkgroupPermissions1();
+            var orders = new List<Order>();
+            var approvals = new List<Approval>();
+            SetupOrders(orders, approvals, 4, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.AccountManager), 1);
+            SetupOrders(orders, approvals, 1, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.AccountManager), 2);
+            SetupOrders(orders, approvals, 2, "moe", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.AccountManager), 1, true);
+
+            #endregion Arrange
+
+            #region Act
+            var results = OrderAccessService.GetListofOrders();
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(0, results.Count);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestUnCompletedOrdersForApprover4()
+        {
+            #region Arrange
+            UserIdentity.Expect(a => a.Current).Return("hsimpson").Repeat.Any();
+            SetupUsers1();
+            SetupWorkgroupPermissions1();
+            var orders = new List<Order>();
+            var approvals = new List<Approval>();
+            SetupOrders(orders, approvals, 4, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 1);
+            SetupOrders(orders, approvals, 1, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 2);
+            SetupOrders(orders, approvals, 2, "moe", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 1, true);
+
+            #endregion Arrange
+
+            #region Act
+            var results = OrderAccessService.GetListofOrders();
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(0, results.Count);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestUnCompletedOrdersForApprover5()
+        {
+            #region Arrange
+            UserIdentity.Expect(a => a.Current).Return("hsimpson").Repeat.Any();
+            SetupUsers1();
+            SetupWorkgroupPermissions1();
+            var orders = new List<Order>();
+            var approvals = new List<Approval>();
+            SetupOrders(orders, approvals, 4, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 1);
+            SetupOrders(orders, approvals, 1, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 2);
+            SetupOrders(orders, approvals, 2, "moe", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Approver), 1, true);
+
+            #endregion Arrange
+
+            #region Act
+            var results = OrderAccessService.GetListofOrders();
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(2, results.Count);
+            foreach (var result in results)
+            {
+                Assert.AreEqual("moe", result.CreatedBy.Id);
+            }
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestUnCompletedOrdersForApprover6()
+        {
+            #region Arrange
+            UserIdentity.Expect(a => a.Current).Return("flanders").Repeat.Any();
+            SetupUsers1();
+            SetupWorkgroupPermissions1();
+            var orders = new List<Order>();
+            var approvals = new List<Approval>();
+            SetupOrders(orders, approvals, 4, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 1);
+            SetupOrders(orders, approvals, 1, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.AccountManager), 2);
+            SetupOrders(orders, approvals, 2, "moe", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 1, true);
+
+            #endregion Arrange
+
+            #region Act
+            var results = OrderAccessService.GetListofOrders();
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(0, results.Count);
+            #endregion Assert
+        }
+
+
+        [TestMethod]
+        public void TestUnCompletedOrdersForApprover7()
+        {
+            #region Arrange
+            UserIdentity.Expect(a => a.Current).Return("flanders").Repeat.Any();
+            SetupUsers1();
+            SetupWorkgroupPermissions1();
+            var orders = new List<Order>();
+            var approvals = new List<Approval>();
+            SetupOrders(orders, approvals, 4, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.AccountManager), 1);
+            SetupOrders(orders, approvals, 1, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.AccountManager), 2);
+            SetupOrders(orders, approvals, 2, "moe", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 1, true);
+
+            #endregion Arrange
+
+            #region Act
+            var results = OrderAccessService.GetListofOrders();
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(4, results.Count);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestUnCompletedOrdersForApprover8()
+        {
+            #region Arrange
+            UserIdentity.Expect(a => a.Current).Return("brannigan").Repeat.Any(); //brannigan is an AccountManager, but not for these orders and wg
+            SetupUsers1();
+            SetupWorkgroupPermissions1();
+            var orders = new List<Order>();
+            var approvals = new List<Approval>();
+            SetupOrders(orders, approvals, 4, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 1);
+            SetupOrders(orders, approvals, 1, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.AccountManager), 2);
+            SetupOrders(orders, approvals, 2, "moe", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 1, true);
+
+            #endregion Arrange
+
+            #region Act
+            var results = OrderAccessService.GetListofOrders();
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(0, results.Count);
+            #endregion Assert
+        }
+
+
+        [TestMethod]
+        public void TestUnCompletedOrdersForApprover9()
+        {
+            #region Arrange
+            UserIdentity.Expect(a => a.Current).Return("brannigan").Repeat.Any(); //brannigan is an AccountManager, but not for these orders but would have orders if flanders was away/null?
+            SetupUsers1();
+            SetupWorkgroupPermissions1();
+            var orders = new List<Order>();
+            var approvals = new List<Approval>();
+            SetupOrders(orders, approvals, 4, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.AccountManager), 1);
+            SetupOrders(orders, approvals, 1, "bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.AccountManager), 2);
+            SetupOrders(orders, approvals, 2, "moe", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser), 1, true);
+
+            #endregion Arrange
+
+            #region Act
+            var results = OrderAccessService.GetListofOrders();
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(0, results.Count);
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Test when user is null in approval
+        /// Test when user is away
+        /// tests for other parameters
+        /// </summary>
+        [TestMethod]
+        public void TestDescription()
+        {
+            #region Arrange
+            Assert.Inconclusive("More tests");
+            
+            #endregion Arrange
+
+            #region Act
+            #endregion Act
+
+            #region Assert
+            #endregion Assert		
         }
     }
 }
