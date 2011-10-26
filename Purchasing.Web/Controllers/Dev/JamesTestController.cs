870a9e6c421638e7dbeea8b4318479bbb12723ee
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-10-26 15:29:13 -0700

    Test method to return current user's orders which have not been acted on in a certain number of days.

--- Purchasing.Web/Controllers/Dev/JamesTestController.cs
+++ Purchasing.Web/Controllers/Dev/JamesTestController.cs
@@ -42,6 +42,38 @@ namespace Purchasing.Web.Controllers
             viewModel.ShowOwned = showOwned;
 
             return View(viewModel);
+
+            //var x = Repository.OfType<OrderTracking>().Queryable.Where(a => a.User.Id == CurrentUser.Identity.Name).GroupBy(a => a.Order).
+             
+
+
+        }
+
+        /// <summary>
+        /// This returns a list of orders created by the current user than have not been acted on X number of days.
+        /// It Does this by querying the orderTracking to find the last date acted on and filter those that have been acted on
+        /// The it checks all those orders to see which ones have a status that is complete and filter out those. 
+        /// </summary>
+        /// <param name="id">The number of days when an order was lasted acted on</param>
+        /// <returns></returns>
+        public ActionResult Test(int id)
+        {
+            //TODO: Might be better to have a trigger in the database update the order to have the last acted upon date
+            var dt = DateTime.Now.AddDays(-id).Date;
+
+            var orderIds = (Repository.OfType<OrderTracking>().Queryable
+                .Where(a => a.Order.CreatedBy.Id == CurrentUser.Identity.Name)
+                .GroupBy(s => s.Order.Id).Select(g => new { id = g.Key, LastDateActedOn = g.Max(s => s.DateCreated) }).ToList().Where(xy => xy.LastDateActedOn <= dt)).Select(cx => cx.id).ToList();
+
+
+
+            var results = Repository.OfType<Order>().Queryable.Where(a => orderIds.Contains(a.Id) && !a.OrderTrackings.Where(b => b.StatusCode.IsComplete).Any()).Distinct().ToList();
+            //var results = Repository.OfType<Order>().Queryable.Where(a => a.CreatedBy.Id == CurrentUser.Identity.Name && !a.OrderTrackings.Where(b => !b.StatusCode.IsComplete && b.DateCreated >= dt).Any()).Distinct().ToList();
+
+            var viewModel = FilteredOrderListModel.Create(Repository, results);
+
+
+            return View(viewModel);
         }
 
     }
