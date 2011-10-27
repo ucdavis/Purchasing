7cd6231da97a1d8d3818d6376a3a54c650aa9c54
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-10-27 13:31:35 -0700

    tests

--- Purchasing.Tests/Core/CreateValidEntities.cs
+++ Purchasing.Tests/Core/CreateValidEntities.cs
@@ -260,5 +260,16 @@ namespace Purchasing.Tests.Core
 
             return rtValue;
         }
+
+        public static OrderTracking OrderTracking(int? counter)
+        {
+            var rtValue = new OrderTracking();
+            rtValue.DateCreated = DateTime.Now;
+            rtValue.Description = "Description" + counter.Extra();
+            rtValue.Order = new Order();
+            rtValue.StatusCode = new OrderStatusCode();
+
+            return rtValue;
+        }
     }
 }
