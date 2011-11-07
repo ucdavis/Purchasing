7449889690fcf669096ad2181f56d396c2d214cf
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-11-07 15:26:28 -0800

    Setting up file structure for Notification Service Tests

---
+++ Purchasing.Tests/ServiceTests/NotificationServiceTests/NotificationServiceTestsOrderApproved.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Purchasing.Core.Domain;
+using Purchasing.Web.Services;
+using Rhino.Mocks;
+using UCDArch.Core.PersistanceSupport;
+
+namespace Purchasing.Tests.ServiceTests.NotificationServiceTests
+{
+    public partial class NotificationServiceTests
+    {
+        #region OrderApproved Tests
+
+        [TestMethod]
+        public void TestOrderApproved1()
+        {
+            #region Arrange
+            UserIdentity.Expect(a => a.Current).Return("burns");
+            SetupUsers();
+            var order = SetupData1("bender", OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser));
+            var approval = new Approval();
+            approval.StatusCode = OrderStatusCodeRepository.GetNullableById(OrderStatusCode.Codes.Purchaser);
+            #endregion Arrange
+
+            #region Act
+            NotificationService.OrderApproved(order, approval);
+            #endregion Act
+
+            #region Assert
+            #endregion Assert		
+        }
+        #endregion OrderApproved Tests
+    }
+}
