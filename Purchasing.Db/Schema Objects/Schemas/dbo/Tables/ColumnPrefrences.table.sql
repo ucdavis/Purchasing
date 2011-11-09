08eb23e8b8ce806d7a1ee33c1ea086b5ab71aa90
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-11-09 10:39:17 -0800

    Added ColumnPrefrences table

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/ColumnPrefrences.table.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[ColumnPrefrences] (
+    [Id]                      VARCHAR (10) NOT NULL,
+    [ShowRequestNumber]       BIT          NOT NULL,
+    [ShowPurchaseOrderNumber] BIT          NOT NULL,
+    [ShowWorkgroup]           BIT          NOT NULL,
+    [ShowOrganization]        BIT          NOT NULL,
+    [ShowVendor]              BIT          NOT NULL,
+    [ShowShipTo]              BIT          NOT NULL,
+    [ShowAllowBackorder]      BIT          NOT NULL,
+    [ShowRestrictedOrder]     BIT          NOT NULL,
+    [ShowHasSplits]           BIT          NOT NULL,
+    [ShowHasAttachments]      BIT          NOT NULL,
+    [ShowNumberOfLines]       BIT          NOT NULL,
+    [ShowTotalAmount]         BIT          NOT NULL,
+    [ShowCreatedBy]           BIT          NOT NULL,
+    [ShowCreatedDate]         BIT          NOT NULL,
+    [ShowStatus]              BIT          NOT NULL,
+    [ShowNeededDate]          BIT          NOT NULL,
+    [ShowShippingType]        BIT          NOT NULL,
+    [ShowDaysNotActedOn]      BIT          NOT NULL,
+    [ShowLastActedOnBy]       BIT          NOT NULL,
+    [ShowPeoplePendingAction] BIT          NOT NULL
+);
+
