f9af18fcdac305184dad2f8c233fcf1f0c9e1bed
Alan Lai <anlai@ucdavis.edu>
2011-11-14 14:44:06 -0800

    added administrative flag to workgroup.

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Workgroups.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Workgroups.table.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[Workgroups] (
-    [Id]                    INT          IDENTITY (1, 1) NOT NULL,
-    [Name]                  VARCHAR (50) NOT NULL,
-    [PrimaryOrganizationId] CHAR (4)     NOT NULL,
-    [IsActive]              BIT          NOT NULL
+	[Id]                    INT          IDENTITY (1, 1) NOT NULL,
+	[Name]                  VARCHAR (50) NOT NULL,
+	[PrimaryOrganizationId] CHAR (4)     NOT NULL,
+	[IsActive]              BIT          NOT NULL,
+	[Administrative]		BIT			 DEFAULT 0 NOT NULL
 );
 
 
