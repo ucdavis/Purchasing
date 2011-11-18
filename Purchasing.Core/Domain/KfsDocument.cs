d785df96a5e2b840ac7e2f5b33f429530d092010
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-11-18 12:41:22 -0800

    Added Attributes to KfsDocument to match the DB settings. Added KfsDocument Repository settings.

--- Purchasing.Core/Domain/KfsDocument.cs
+++ Purchasing.Core/Domain/KfsDocument.cs
@@ -1,12 +1,16 @@
-﻿using FluentNHibernate.Mapping;
+﻿using System.ComponentModel.DataAnnotations;
+using FluentNHibernate.Mapping;
 using UCDArch.Core.DomainModel;
 
 namespace Purchasing.Core.Domain
 {
     public class KfsDocument : DomainObject
     {
+        [Required]
+        [StringLength(50)]
         public virtual string DocNumber { get; set; }
 
+        [Required]
         public virtual Order Order { get; set; }
     }
 
