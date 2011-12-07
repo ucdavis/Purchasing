1e180cfbff1733b75cc710eed3a11c5c9fc4fcdb
Christopher Thielen <cmthielen@ucdavis.edu>
2011-12-07 15:02:05 -0800

    Progress on dynamic un-serializing

--- Purchasing.Web/Scripts/OrderMockupCS.js
+++ Purchasing.Web/Scripts/OrderMockupCS.js
@@ -17,17 +17,31 @@ function restoreOrderForm() {
   
   $("form[name=orderform]").unserializeForm(data, {
     'callback'  : function(el, val) {
-      // TODO: Build the form dynamically here
-      $(el).val(val); // just set the element for now
+      // Do we need to build a dynamic form?
+      var match = /items\[([0-9]+)\].quantity/i.exec(el)
+      if(match != null) {
+        // Set a line-item. Form only has three by default. Add one if we see items[3] or greater.
+        // Note that one line-item is a group of a few elements that will trigger the callback.
+        // Only respond to items[?].quantity (arbitrary. we could respond to any single one in the group)
+        if(parseInt(match[1]) > 2) {
+          // found a line item that requires we expand the DOM
+          
+          // FIXME: This isn't correct. If we see a items[54], we need to ensure there are 54, not simply add
+          // one that will create items[3]. 
+          $("#add-line-item").trigger('click');
+          
+          
+        }
+      }
+      
+      $(el).val(val); // presuming any dynamic bits were created, set the element
     }
   });
 }
 
 // Clear out a saved order form
 function clearStoredOrderForm() {
-  localStorage.removeItem("justification");
-  localStorage.removeItem("vendor");
-  localStorage.removeItem("shipping");
+  localStorage.removeItem("orderform");
 }
 
-// TODO: UI/UX to prompt when leaving/loading page
+// TODO: Save automatically every five seconds
