6b971adee027fcc5c373a2f35d240119894c6c6a
Alan Lai <anlai@ucdavis.edu>
2011-12-29 10:12:36 -0800

    changed custom fields "order" column to "rank"

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/CustomFields.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/CustomFields.table.sql
@@ -2,7 +2,7 @@
     [Id]             INT           IDENTITY (1, 1) NOT NULL,
     [Name]           VARCHAR (MAX) NOT NULL,
     [OrganizationId] CHAR (4)      NOT NULL,
-    [Order]          INT           NOT NULL,
+    [Rank]	         INT           NOT NULL,
     [IsActive]       BIT           NOT NULL,
     [IsRequired]     BIT           NOT NULL
 );
