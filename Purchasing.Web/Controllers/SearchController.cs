ad38f35196e857ea52824f9fb1563c717aea53d7
Alan Lai <anlai@ucdavis.edu>
2012-01-05 11:28:25 -0800

    created a search controller, it does nothing right now.

---
+++ Purchasing.Web/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+
+namespace Purchasing.Web.Controllers
+{
+    /// <summary>
+    /// Controller for the Search class
+    /// </summary>
+    [Authorize]
+    public class SearchController : ApplicationController
+    {
+        public SearchController()
+        {
+        }
+    
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Index(string id)
+        {
+            return View();
+        }
+
+    }
+	
+}
