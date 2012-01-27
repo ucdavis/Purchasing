ce87368f5c3bfef7b89a901b218c06166bcb7ced
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-01-27 09:39:08 -0800

    Created initial structure for Wizard Controller Tests

---
+++ Purchasing.Tests/ControllerTests/WizardControllerTests.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.Windsor;
+using Purchasing.Web;
+using Purchasing.Web.Controllers;
+//using Purchasing.Controllers.Filters;
+using Purchasing.Core.Domain;
+//using Purchasing.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using Purchasing.Web.Helpers;
+using Purchasing.Web.Services;
+using Rhino.Mocks;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Testing;
+using UCDArch.Web.Attributes;
+
+
+namespace Purchasing.Tests.ControllerTests
+{
+    [TestClass]
+    public class WizardControllerTests : ControllerTestBase<WizardController>
+    {
+        private readonly Type _controllerClass = typeof(WizardController);
+        protected IRepositoryWithTypedId<User, string> UserRepository;
+        protected IRepositoryWithTypedId<Role, string> RoleRepository;
+        protected ISecurityService SecurityService;
+        protected IDirectorySearchService SearchService;
+        protected readonly Type ControllerClass = typeof(WorkgroupController);
+        protected IRepository<Workgroup> WorkgroupRepository;
+        protected IRepository<WorkgroupPermission> WorkgroupPermissionRepository;
+        protected IRepository<WorkgroupVendor> WorkgroupVendorRepository;
+        protected IRepositoryWithTypedId<Vendor, string> VendorRepository;
+        protected IRepositoryWithTypedId<VendorAddress, Guid> VendorAddressRepository;
+        protected IRepositoryWithTypedId<State, string> StateRepository;
+        protected IRepositoryWithTypedId<EmailPreferences, string> EmailPreferencesRepository;
+        protected IRepository<Organization> OrganizationRepository;
+        protected IRepository<WorkgroupAccount> WorkgroupAccountRepository;
+        protected IWorkgroupAddressService WorkgroupAddressService;
+        protected IWorkgroupService WorkgroupService;
+
+        #region Init
+        /// <summary>
+        /// Setups the controller.
+        /// </summary>
+        protected override void SetupController()
+        {
+            WorkgroupRepository = FakeRepository<Workgroup>();
+            UserRepository = MockRepository.GenerateStub<IRepositoryWithTypedId<User, string>>();
+            RoleRepository = MockRepository.GenerateStub<IRepositoryWithTypedId<Role, string>>();
+            SecurityService = MockRepository.GenerateStub<ISecurityService>();
+            SearchService = MockRepository.GenerateStub<IDirectorySearchService>();
+            WorkgroupPermissionRepository = MockRepository.GenerateStub<IRepository<WorkgroupPermission>>();
+            WorkgroupVendorRepository = MockRepository.GenerateStub<IRepository<WorkgroupVendor>>();
+            VendorRepository = MockRepository.GenerateStub<IRepositoryWithTypedId<Vendor, string>>();
+            VendorAddressRepository = MockRepository.GenerateStub<IRepositoryWithTypedId<VendorAddress, Guid>>();
+            StateRepository = MockRepository.GenerateStub<IRepositoryWithTypedId<State, string>>();
+            WorkgroupAccountRepository = MockRepository.GenerateStub<IRepository<WorkgroupAccount>>();
+            WorkgroupAddressService = MockRepository.GenerateStub<IWorkgroupAddressService>();
+            WorkgroupService = MockRepository.GenerateStub<IWorkgroupService>();
+
+            Controller = new TestControllerBuilder().CreateController<WizardController>(WorkgroupRepository,
+                UserRepository,
+                RoleRepository,
+                WorkgroupPermissionRepository,
+                SecurityService,
+                SearchService,
+                WorkgroupVendorRepository,
+                VendorRepository,
+                VendorAddressRepository,
+                StateRepository,
+                WorkgroupAccountRepository,
+                WorkgroupAddressService,
+                WorkgroupService);
+        }
+
+        protected override void RegisterRoutes()
+        {
+            new RouteConfigurator().RegisterRoutes(); //Try this one if below doesn't work
+            //RouteRegistrar.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected override void RegisterAdditionalServices(IWindsorContainer container)
+        {
+            AutomapperConfig.Configure();
+            base.RegisterAdditionalServices(container);
+        }
+
+        public WizardControllerTests()
+        {
+            //    ExampleRepository = FakeRepository<Example>();
+            //    Controller.Repository.Expect(a => a.OfType<Example>()).Return(ExampleRepository).Repeat.Any();
+
+            Controller.Repository.Expect(a => a.OfType<Workgroup>()).Return(WorkgroupRepository).Repeat.Any();	
+        }
+        #endregion Init
+
+        #region Mapping Tests
+        //[TestMethod]
+        //public void TestExampleMapping()
+        //{
+        //    "~/Wizard/YourMethod/".ShouldMapTo<WizardController>(a => a.YourMethod(null));
+        //}
+        #endregion Mapping Tests
+
+        #region Method Tests
+
+        [TestMethod]
+        public void TestWriteMethodTests()
+        {
+            #region Arrange
+            Assert.Inconclusive("Need to write these tests");          
+            #endregion Arrange
+
+            #region Act
+
+            #endregion Act
+
+            #region Assert
+
+            #endregion Assert		
+        }      
+        #endregion Method Tests
+
+        #region Reflection Tests
+
+        #region Controller Class Tests
+        /// <summary>
+        /// Tests the controller inherits from application controller.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerInheritsFromApplicationController()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            Assert.IsNotNull(controllerClass.BaseType);
+            var result = controllerClass.BaseType.Name;
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual("ApplicationController", result);
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has only three attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasOnlyThreeAttributes()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(3, result.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has transaction attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasTransactionAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseTransactionsByDefaultAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseTransactionsByDefaultAttribute not found.");
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has anti forgery token attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasAntiForgeryTokenAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseAntiForgeryTokenOnPostByDefault>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseAntiForgeryTokenOnPostByDefault not found.");
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestControllerHasVersionAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<VersionAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "VersionAttribute not found.");
+            #endregion Assert
+        }
+
+        #endregion Controller Class Tests
+
+        #region Controller Method Tests
+
+        [TestMethod]
+        public void TestControllerContainsExpectedNumberOfPublicMethods()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetMethods().Where(a => a.DeclaringType == controllerClass);
+            #endregion Act
+
+            #region Assert
+            Assert.Inconclusive("Tests are still being written. When done, remove this line.");
+            Assert.AreEqual(0, result.Count(), "It looks like a method was added or removed from the controller.");
+            #endregion Assert
+        }
+
+        //Examples
+
+        //[TestMethod]
+        //public void TestControllerMethodLogOnContainsExpectedAttributes()
+        //{
+        //    #region Arrange
+        //    var controllerClass = _controllerClass;
+        //    var controllerMethod = controllerClass.GetMethod("LogOn");
+        //    #endregion Arrange
+
+        //    #region Act
+        //    //var expectedAttribute = controllerMethod.GetCustomAttributes(true).OfType<UserOnlyAttribute>();
+        //    var allAttributes = controllerMethod.GetCustomAttributes(true);
+        //    #endregion Act
+
+        //    #region Assert
+        //    //Assert.AreEqual(1, expectedAttribute.Count(), "UserOnlyAttribute not found");
+        //    Assert.AreEqual(0, allAttributes.Count());
+        //    #endregion Assert
+        //}
+
+        //[TestMethod]
+        //public void TestControllerMethodLogOutContainsExpectedAttributes()
+        //{
+        //    #region Arrange
+        //    var controllerClass = _controllerClass;
+        //    var controllerMethod = controllerClass.GetMethod("LogOut");
+        //    #endregion Arrange
+
+        //    #region Act
+        //    //var expectedAttribute = controllerMethod.GetCustomAttributes(true).OfType<UserOnlyAttribute>();
+        //    var allAttributes = controllerMethod.GetCustomAttributes(true);
+        //    #endregion Act
+
+        //    #region Assert
+        //    //Assert.AreEqual(1, expectedAttribute.Count(), "UserOnlyAttribute not found");
+        //    Assert.AreEqual(0, allAttributes.Count());
+        //    #endregion Assert
+        //}
+
+
+        //[TestMethod]
+        //public void TestControllerMethodCreateContainsExpectedAttributes1()
+        //{
+        //    #region Arrange
+        //    var controllerClass = _controllerClass;
+        //    var controllerMethod = controllerClass.GetMethods().Where(a => a.Name == "Create");
+        //    #endregion Arrange
+
+        //    #region Act
+        //    var expectedAttribute = controllerMethod.ElementAt(0).GetCustomAttributes(true).OfType<UserOnlyAttribute>();
+        //    var allAttributes = controllerMethod.ElementAt(0).GetCustomAttributes(true);
+        //    #endregion Act
+
+        //    #region Assert
+        //    Assert.AreEqual(1, expectedAttribute.Count(), "UserOnlyAttribute not found");
+        //    Assert.AreEqual(1, allAttributes.Count());
+        //    #endregion Assert
+        //}
+
+        //[TestMethod]
+        //public void TestControllerMethodCreateContainsExpectedAttributes2()
+        //{
+        //    #region Arrange
+        //    var controllerClass = _controllerClass;
+        //    var controllerMethod = controllerClass.GetMethods().Where(a => a.Name == "Create");
+        //    #endregion Arrange
+
+        //    #region Act
+        //    var expectedAttribute = controllerMethod.ElementAt(1).GetCustomAttributes(true).OfType<HttpPostAttribute>();
+        //    var allAttributes = controllerMethod.ElementAt(1).GetCustomAttributes(true);
+        //    #endregion Act
+
+        //    #region Assert
+        //    Assert.AreEqual(1, expectedAttribute.Count(), "HttpPostAttribute not found");
+        //    Assert.AreEqual(2, allAttributes.Count(), "More than expected custom attributes found.");
+        //    #endregion Assert
+        //}
+
+        //[TestMethod]
+        //public void TestControllerMethodCreateContainsExpectedAttributes3()
+        //{
+        //    #region Arrange
+        //    var controllerClass = _controllerClass;
+        //    var controllerMethod = controllerClass.GetMethods().Where(a => a.Name == "Create");
+        //    #endregion Arrange
+
+        //    #region Act
+        //    var expectedAttribute = controllerMethod.ElementAt(1).GetCustomAttributes(true).OfType<UserOnlyAttribute>();
+        //    var allAttributes = controllerMethod.ElementAt(1).GetCustomAttributes(true);
+        //    #endregion Act
+
+        //    #region Assert
+        //    Assert.AreEqual(1, expectedAttribute.Count(), "UserOnlyAttribute not found");
+        //    Assert.AreEqual(2, allAttributes.Count(), "More than expected custom attributes found.");
+        //    #endregion Assert
+        //}
+
+
+        #endregion Controller Method Tests
+
+        #endregion Reflection Tests
+    }
+}
