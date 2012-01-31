e6f6a3886655c4704394169ad144c924d8c1418f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-01-30 16:01:13 -0800

    WIP Wizard Controller tests

--- Purchasing.Tests/ControllerTests/WizardControllerTests/WizardControllerTestsPart03.cs
+++ Purchasing.Tests/ControllerTests/WizardControllerTests/WizardControllerTestsPart03.cs
@@ -26,5 +26,86 @@ namespace Purchasing.Tests.ControllerTests.WizardControllerTests
 {
     public partial class WizardControllerTests
     {
+        #region AddPeople Get Tests
+
+        [TestMethod]
+        public void TestAddPeopleRedirectsToCreateWorkgroupWhenWorkgroupIdIsZero()
+        {
+            #region Arrange
+            
+            #endregion Arrange
+
+            #region Act
+            #endregion Act
+
+            #region Assert
+            Controller.AddPeople(0, null)
+                .AssertActionRedirect()
+                .ToAction<WizardController>(a => a.CreateWorkgroup());
+            #endregion Assert		
+        }
+
+        [TestMethod]
+        public void TestAddPeopleGetRedirectsIfNoAccess()
+        {
+            #region Arrange
+            new FakeWorkgroups(3, WorkgroupRepository);
+            string message = "Fake Message";
+            SecurityService.Expect(a => a.HasWorkgroupOrOrganizationAccess(Arg<Workgroup>.Is.Anything, Arg<Organization>.Is.Anything, out Arg<string>.Out(message).Dummy)).Return(false);
+            #endregion Arrange
+
+            #region Act
+            Controller.AddPeople(3, Role.Codes.Requester)
+                .AssertActionRedirect()
+                .ToAction<ErrorController>(a => a.NotAuthorized());
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual("Fake Message", Controller.Message);
+            SecurityService.AssertWasCalled(a => a.HasWorkgroupOrOrganizationAccess(Arg<Workgroup>.Is.Anything, Arg<Organization>.Is.Anything, out Arg<string>.Out(message).Dummy));
+            var args = SecurityService.GetArgumentsForCallsMadeOn(a => a.HasWorkgroupOrOrganizationAccess(Arg<Workgroup>.Is.Anything, Arg<Organization>.Is.Anything, out Arg<string>.Out(message).Dummy))[0];
+            Assert.IsNotNull(args);
+            Assert.AreEqual("Name3", ((Workgroup)args[0]).Name);
+            Assert.IsNull(args[1]);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (PreconditionException))]
+        public void TestAddPeopleChecksDatabaseRole()
+        {
+            var thisFar = false;
+            try
+            {
+                #region Arrange
+                var roles = new List<Role>();
+                for (int i = 0; i < 4; i++)
+                {
+                    roles.Add(CreateValidEntities.Role(i+1));
+                    roles[i].Level = i + 1;
+                    roles[i].SetIdTo((i + 1).ToString());
+                }
+                new FakeWorkgroups(3, WorkgroupRepository);
+                string message = string.Empty;
+                SecurityService.Expect(a => a.HasWorkgroupOrOrganizationAccess(Arg<Workgroup>.Is.Anything, Arg<Organization>.Is.Anything, out Arg<string>.Out(message).Dummy)).Return(true);
+                new FakeRoles(0, RoleRepository, roles, true);
+
+                thisFar = true;
+                #endregion Arrange
+
+                #region Act
+                Controller.AddPeople(3, "2");
+                #endregion Act
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(thisFar);
+                Assert.IsNotNull(ex);
+                Assert.AreEqual("Precondition failed.", ex.Message);
+                throw;
+            }
+        }
+
+        #endregion AddPeople Get Tests
     }
 }
