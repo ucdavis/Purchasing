1d898872a807faf0f29e12ce2cb3a8fb81199808
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-02-03 14:37:44 -0800

    added commentHistory view

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Views/vCommentHistory.view.sql
@@ -0,0 +1,177 @@
+﻿CREATE VIEW dbo.vCommentHistory
+AS
+SELECT DISTINCT 
+                      dbo.OrderComments.OrderId, dbo.OrderComments.Text AS comment, dbo.OrderComments.UserId AS createdby, dbo.OrderComments.DateCreated, 
+                      access.UserId AS access, dbo.OrderComments.Id
+FROM         dbo.OrderTracking INNER JOIN
+                      dbo.Orders ON dbo.Orders.Id = dbo.OrderTracking.OrderId INNER JOIN
+                      dbo.OrderComments ON dbo.OrderComments.OrderId = dbo.Orders.Id INNER JOIN
+                          (SELECT DISTINCT OrderId, UserId
+                            FROM          (SELECT     OrderId, UserId
+                                                    FROM          dbo.OrderTracking AS OrderTracking_1
+                                                    UNION
+                                                    SELECT     OrderId, UserId
+                                                    FROM         dbo.Approvals
+                                                    WHERE     (Completed = 1) AND (UserId IS NOT NULL)
+                                                    UNION
+                                                    SELECT     OrderId, SecondaryUserId
+                                                    FROM         dbo.Approvals AS Approvals_1
+                                                    WHERE     (Completed = 1) AND (SecondaryUserId IS NOT NULL)) AS blank) AS access ON access.OrderId = dbo.Orders.Id
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[31] 4[21] 2[30] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "OrderTracking"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 125
+               Right = 221
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Orders"
+            Begin Extent = 
+               Top = 6
+               Left = 259
+               Bottom = 125
+               Right = 472
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "OrderComments"
+            Begin Extent = 
+               Top = 6
+               Left = 510
+               Bottom = 125
+               Right = 670
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "access"
+            Begin Extent = 
+               Top = 6
+               Left = 708
+               Bottom = 95
+               Right = 868
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 9
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      E', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vCommentHistory';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane2', @value = N'nd
+   End
+End', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vCommentHistory';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 2, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vCommentHistory';
+
