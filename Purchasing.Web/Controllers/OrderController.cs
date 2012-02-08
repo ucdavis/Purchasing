29094dbbd8e4de4cb449c91e543c1d4e949b0285
Scott Kirkland <srkirkland@gmail.com>
2012-02-08 10:56:26 -0800

    Order edit can handle account specified or not, for Approvers

--- Purchasing.Web/Controllers/OrderController.cs
+++ Purchasing.Web/Controllers/OrderController.cs
@@ -552,9 +552,8 @@ namespace Purchasing.Web.Controllers
             }
             else
             {
-                splitType = splits.Count() == 1
-                                      ? OrderViewModel.SplitTypes.None
-                                      : OrderViewModel.SplitTypes.Order;
+                //splits count = 0 if no account specified, 1 if only one account was specified with no splits
+                splitType = splits.Count <= 1 ? OrderViewModel.SplitTypes.None : OrderViewModel.SplitTypes.Order;
             }
 
             return new JsonNetResult(new { id, lineItems, splits, splitType = splitType.ToString() });
