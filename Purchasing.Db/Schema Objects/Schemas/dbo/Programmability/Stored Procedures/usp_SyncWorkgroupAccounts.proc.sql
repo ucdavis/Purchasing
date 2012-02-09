ad9c287643bfe9a40ce64becc09dd46b7d952493
Alan Lai <anlai@ucdavis.edu>
2012-02-09 09:35:58 -0800

    added beginning of sproc for sync.

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/usp_SyncWorkgroupAccounts.proc.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[usp_SyncWorkgroupAccounts]
+AS
+
+	declare @cursor cursor, @id int
+
+	set @cursor = cursor for
+		select workgroups.id, workgroups.primaryorganizationid from workgroups where syncaccounts = 1
+	
+	open @cursor
+
+	fetch next from @cursor into @id
+
+	while(@@FETCH_STATUS = 0)
+	begin
+
+		-- delete accounts that are no longer part of the org
+
+		-- insert any new accounts that are part of the org
+
+		fetch next from @cursor into @id
+
+	end
+
+	close @cursor
+	deallocate @cursor
+
+RETURN 0
