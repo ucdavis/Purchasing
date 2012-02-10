89edef5ed86287cdb56b0b5085c922c68f724429
Scott Kirkland <srkirkland@gmail.com>
2012-02-10 09:21:21 -0800

    Added individual css to cassette, and updated copy/edit pages

--- Purchasing.Web/CassetteConfiguration.cs
+++ Purchasing.Web/CassetteConfiguration.cs
@@ -18,12 +18,13 @@ namespace Purchasing.Web
             // This default configuration treats each file as a separate 'bundle'.
             // In production the content will be minified, but the files are not combined.
             // So you probably want to tweak these defaults!
-            //bundles.AddPerIndividualFile<StylesheetBundle>("Css");
+            bundles.AddPerIndividualFile<StylesheetBundle>("Css");
             //bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
 
             // To combine files, try something like this instead:
             //   bundles.Add<StylesheetBundle>("Content");
             // In production mode, all of ~/Content will be combined into a single bundle.
+            //TODO: combine CSS into a bundle
 
             bundles.AddPerSubDirectory<ScriptBundle>("Scripts/external"); //just the CDN hosted stuff
             bundles.AddPerSubDirectory<ScriptBundle>("Scripts/public");
