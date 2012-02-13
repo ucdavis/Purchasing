090c076eda10f14dbbdcd35c7643007e2fbc42db
Scott Kirkland <srkirkland@gmail.com>
2012-02-13 10:09:01 -0800

    Removed the obsolete ajax calls from the order controller

--- Purchasing.Web/Controllers/OrderController.cs
+++ Purchasing.Web/Controllers/OrderController.cs
@@ -477,65 +477,6 @@ namespace Purchasing.Web.Controllers
             return Json(results, JsonRequestBehavior.AllowGet);
         }
 
-        [Obsolete]
-        public JsonNetResult GetLineItems(int id)
-        {
-            throw new NotSupportedException("You should use GetLineItemsAndSplits instead");
-            var lineItems = _repositoryFactory.LineItemRepository
-                .Queryable
-                .Where(x => x.Order.Id == id)
-                .Select(
-                    x =>
-                    new OrderViewModel.LineItem
-                    {
-                        CatalogNumber = x.CatalogNumber,
-                        CommodityCode = x.Commodity.Id,
-                        Description = x.Description,
-                        Id = x.Id,
-                        Notes = x.Notes,
-                        Price = x.UnitPrice.ToString(),
-                        Quantity = x.Quantity.ToString(),
-                        Units = x.Unit,
-                        Url = x.Url
-                    });
-
-            return new JsonNetResult(new { id, lineItems });
-        }
-
-        [Obsolete]
-        public JsonNetResult GetSplits(int id)
-        {
-            throw new NotSupportedException("You should use GetLineItemsAndSplits instead");
-            var splits = _repositoryFactory.SplitRepository
-                .Queryable
-                .Where(x => x.Order.Id == id)
-                .Select(
-                    x =>
-                    new OrderViewModel.Split
-                    {
-                        Account = x.Account,
-                        Amount = x.Amount.ToString(),
-                        LineItemId = x.LineItem == null ? 0 : x.LineItem.Id,
-                        Project = x.Project,
-                        SubAccount = x.SubAccount
-                    });
-
-            OrderViewModel.SplitTypes splitType;
-
-            if (splits.Any(x => x.LineItemId != 0))
-            {
-                splitType = OrderViewModel.SplitTypes.Line;
-            }
-            else
-            {
-                splitType = splits.Count() == 1
-                                      ? OrderViewModel.SplitTypes.None
-                                      : OrderViewModel.SplitTypes.Order;
-            }
-
-            return new JsonNetResult(new { id, splits, splitType = splitType.ToString() });
-        }
-
         [AuthorizeEditOrder]
         public JsonNetResult GetLineItemsAndSplits(int id)
         {
