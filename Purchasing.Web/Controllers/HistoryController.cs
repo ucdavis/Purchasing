6923f55c331833ba36e4fe38c9c248d8a9a3b359
Scott Kirkland <srkirkland@gmail.com>
2012-02-14 14:34:43 -0800

    Added a history controller which will serve order history queries

---
+++ Purchasing.Web/Controllers/HistoryController.cs
@@ -0,0 +1,16 @@
+﻿using Purchasing.Core;
+
+namespace Purchasing.Web.Controllers
+{
+    public class HistoryController : ApplicationController
+    {
+        private readonly IRepositoryFactory _repositoryFactory;
+
+        public HistoryController(IRepositoryFactory repositoryFactory)
+        {
+            _repositoryFactory = repositoryFactory;
+        }
+
+
+    }
+}
