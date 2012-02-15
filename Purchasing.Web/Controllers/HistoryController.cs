35da29fb0882263be4133addebc2166a72940a01
Scott Kirkland <srkirkland@gmail.com>
2012-02-14 16:07:34 -0800

    Moved finishedRecently and comments into a partial. The goal is to eventually load on demand.

--- Purchasing.Web/Controllers/HistoryController.cs
+++ Purchasing.Web/Controllers/HistoryController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Purchasing.Core;
 using System.Web.Mvc;
 using Purchasing.Core.Domain;
+using Purchasing.Core.Queries;
 using Purchasing.Web.Helpers;
 
 namespace Purchasing.Web.Controllers
@@ -26,6 +27,29 @@ namespace Purchasing.Web.Controllers
             var lastOrder = lastOrderEvent == null ? null : lastOrderEvent.Order;
 
             return PartialView(lastOrder);
+        }
+
+        [ChildActionOnly]
+        public ActionResult RecentComments()
+        {
+            var recentComments = Repository.OfType<CommentHistory>()
+                .Queryable.Where(a => a.AccessUserId == CurrentUser.Identity.Name)
+                .OrderByDescending(o => o.DateCreated)
+                .Take(5).ToList();
+
+            return PartialView(recentComments);
+        }
+
+        [ChildActionOnly]
+        public ActionResult RecentlyFinished()
+        {
+            //viewModel.FinishedThisWeekCount =
+              //  Repository.OfType<CompletedOrdersThisWeek>().Queryable.Count(c => c.OrderTrackingUser == CurrentUser.Identity.Name);
+            //TODO: create a repository factory for just the queries
+            
+            var finishedThisMonth = Repository.OfType<CompletedOrdersThisMonth>().Queryable.Count(a => a.OrderTrackingUser == CurrentUser.Identity.Name);
+
+            return PartialView(finishedThisMonth);
         }
     }
 }
