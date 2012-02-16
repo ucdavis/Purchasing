c8692970bd5bd6f040f8322375f663b6d87efee8
Alan Lai <anlai@ucdavis.edu>
2012-02-16 09:37:13 -0800

    updated ordertracking.

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Views/vRecentOrders.view.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vRecentOrders]
+	AS 
+
+select row_number() over (order by orders.id) id
+	, orders.id orderid, requestnumber, orders.datecreated
+	, dateneeded, creator.firstname + ' ' + creator.lastname
+from orders
+	inner join users creator on creator.id = orders.createdby
+	inner join ordertracking tracking on tracking.orderid = orders.id
+where
+	tracking.datecreated in ( select max(tracking.datecreated)
+							  from ordertracking itracking
+							  where tracking.orderid = itracking.orderid )
