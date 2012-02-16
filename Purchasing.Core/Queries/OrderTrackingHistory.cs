ae7cf588683eec2d0c6ca808d9d4efd8ecf4be75
Alan Lai <anlai@ucdavis.edu>
2012-02-16 08:48:18 -0800

    created the order tracking history

---
+++ Purchasing.Core/Queries/OrderTrackingHistory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
+
+namespace Purchasing.Core.Queries
+{
+    public class OrderTrackingHistory : DomainObject
+    {
+        public virtual DateTime TrackingDate { get; set; }
+        public virtual int OrderId { get; set; }
+        public virtual string RequestNumber { get; set; }
+        public virtual string EditBy { get; set; }
+        public virtual string Summary { get; set; }
+
+        /// <summary>
+        /// Gets the summary "shortened" with display logic
+        /// </summary>
+        /// <returns></returns>
+        public virtual string DisplaySummary()
+        {
+            return Summary.Length < 100 ? Summary : string.Format("{0}...", Summary.Substring(0, 100));
+        }
+    }
+
+    public class OrderTrackingHistoryMap : ClassMap<OrderTrackingHistory>
+    {
+        public OrderTrackingHistoryMap()
+        {
+            Id(x => x.Id);
+
+            Table("vOrderTracking");
+            ReadOnly();
+
+            Map(x => x.TrackingDate);
+            Map(x => x.OrderId);
+            Map(x => x.RequestNumber);
+            Map(x => x.EditBy);
+            Map(x => x.Summary);
+        }
+    }
+}
