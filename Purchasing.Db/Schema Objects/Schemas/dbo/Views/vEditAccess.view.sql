9f6a903b911241d83e036286bc4d13e306602516
Alan Lai <anlai@ucdavis.edu>
2012-02-17 13:39:23 -0800

    access query views and classes have been written.

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Views/vEditAccess.view.sql
@@ -0,0 +1,82 @@
+﻿/*
+
+	Description:
+
+	Returns a list of users who have edit access to the associated order
+
+	Ways to have permissions:
+	
+		*ALL APPROVALS EXCEPT CA
+		*Specified as primary user and not away
+		*Primary user is away, go to workgroup permissions
+		*No user defined, go to workgroup permissions
+		
+		*CA APPROVALS
+		*Specified as primary user
+		*Specified as secondary user
+		
+
+*/
+
+CREATE VIEW [dbo].[vEditAccess]
+
+	AS 
+
+select ROW_NUMBER() over ( order by userid ) id, access.orderid, access.UserId accessuserid, access.IsAway
+from
+(
+-- primary user specified, and not away
+select orders.id orderid, approvals.userid, users.isaway
+from orders
+	-- order's current status
+	inner join orderstatuscodes os on os.id = orders.orderstatuscodeid
+	-- approvals at the same level as the order status and not completed
+	inner join approvals on approvals.orderid = orders.id and approvals.orderstatuscodeid = os.id and approvals.completed = 0
+	inner join users on users.id = approvals.userid
+where approvals.userid is not null
+  and os.IsComplete = 0
+union
+-- secondary user specified, away status doesn't matter
+select orders.id orderid, approvals.secondaryuserid, users.isaway
+from orders
+	-- order's current status
+	inner join orderstatuscodes os on os.id = orders.orderstatuscodeid
+	-- approvals at the same level as the order status and not completed
+	inner join approvals on approvals.orderid = orders.id and approvals.orderstatuscodeid = os.id and approvals.completed = 0
+	inner join users on users.id = approvals.secondaryuserid
+where secondaryuserid is not null
+  and os.IsComplete = 0
+union
+-- workgroup permissions
+select orders.id orderid, workgrouppermissions.userid, users.isaway
+from orders
+	-- order's current status
+	inner join orderstatuscodes os on os.id = orders.orderstatuscodeid
+	-- approvals at the same level as the order status and not completed
+	inner join approvals on approvals.orderid = orders.id and approvals.orderstatuscodeid = os.id and approvals.completed = 0
+	-- join with the workgroup
+	inner join workgroups on orders.workgroupid = workgroups.id
+	-- workgroup permissions
+	inner join workgrouppermissions on workgroups.id = workgrouppermissions.workgroupid and orders.orderstatuscodeid = workgrouppermissions.roleid
+	inner join users on users.id = approvals.userid
+where approvals.userid is null and approvals.secondaryuserid is null
+  and os.IsComplete = 0
+union
+-- capture the away approvals that are not conditioanl approvals
+select orders.id orderid, workgrouppermissions.userid, users.isaway
+from orders
+	-- order's current status
+	inner join orderstatuscodes os on os.id = orders.orderstatuscodeid
+	-- approvals at the same level as the order status and not completed
+	inner join approvals on approvals.orderid = orders.id and approvals.orderstatuscodeid = os.id and approvals.completed = 0
+	-- join with the workgroup
+	inner join workgroups on orders.workgroupid = workgroups.id
+	-- workgroup permissions
+	inner join workgrouppermissions on workgroups.id = workgrouppermissions.workgroupid and orders.orderstatuscodeid = workgrouppermissions.roleid
+	-- join for the approval user to make sure they are away
+	inner join users on users.id = approvals.userid
+where approvals.userid is null and approvals.secondaryuserid is null
+  and users.isaway = 1
+  and approvals.orderstatuscodeid <> 'CA'
+  and os.IsComplete = 0
+) access
---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Views/vReadAccess.view.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vReadAccess]
+
+	AS 
+
+select ROW_NUMBER() over (order by orderid) id, access.orderid, access.UserId accessuserid, access.IsAway
+from
+(
+select distinct orderid, userid, users.IsAway
+from ordertracking
+	inner join Users on users.Id = ordertracking.userid
+) access
