b3792f5a0bb9d13df3d3432c645b53909ca6ebc7
Alan Lai <anlai@ucdavis.edu>
2012-02-22 15:21:59 -0800

    added a web service project to interact with the kfs api.

---
+++ Purchasing.WS/IFinancialSystemService.cs
@@ -0,0 +1,14 @@
+﻿using Purchasing.Core.Domain;
+
+namespace Purchasing.WS
+{
+    public interface IFinancialSystemService
+    {
+        /// <summary>
+        /// Submits the order to the campus financial system
+        /// </summary>
+        /// <param name="order">Order to be submitted</param>
+        /// <param name="userId">Kerb id of user to save the document to</param>
+        void SubmitOrder(Order order, string userId);
+    }
+}
