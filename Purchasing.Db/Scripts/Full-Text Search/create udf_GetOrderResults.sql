848a5b5dd12f5f1ba64234427d1a6010d2c5e589
Ken Taylor <kentaylor@ucdavis.edu>
2012-02-23 14:39:33 -0800

    Revised udf_GetCommentResilts and udf_GetOrderResults as per Scott Kirkland
    * Comments: CreatedBy should be 'FirstName + ' ' + LastName'.  You'll have to join to the users table for that
    
    * Order: The search should match justification, requestNumber, deliverTo and DeliverToEmail.  Also, CreatedBy should return first+last just like comments.

--- Purchasing.Db/Scripts/Full-Text Search/create udf_GetOrderResults.sql
+++ Purchasing.Db/Scripts/Full-Text Search/create udf_GetOrderResults.sql
@@ -15,17 +15,22 @@ GO
 -- USE [PrePurchasing]
 -- GO
 -- 
--- DECLARE @ContainsSearchCondition varchar(255) = 'reading AND fun' 
+-- DECLARE @ContainsSearchCondition varchar(255) = '(reading AND fun) OR Smith OR Lai OR ACRU'
 -- DECLARE @UserId varchar(255) = 'anlai' --'jsylvest'
 -- 
 -- SELECT * from udf_GetOrderResults(@UserId, @ContainsSearchCondition)
 -- 
 -- results:
--- Id	DateCreated				DeliverTo	DeliverToEmail		Justification					CreatedBy	RequestNumber
--- 4	2012-02-23 09:35:22.000	Mr. Smith	msmith@ucdavis.edu	Because reading is fun-damental	postit		ACRU-DGAJOAS
+-- Id	DateCreated				DeliverTo		DeliverToEmail		Justification					CreatedBy		RequestNumber
+-- 2	2012-02-22 20:53:35.000	Alan Lai		anlai@ucdavis.edu	Justification					Alan Lai		ACRU-EFTT2H9
+-- 3	2012-02-23 07:58:25.000	Frank Grimes	NULL				Justification					Frank Grimes	ACRU-FJEZEK6
+-- 4	2012-02-23 09:35:22.000	Mr. Smith		msmith@ucdavis.edu	Because reading is fun-damental	Scott Kirkland	ACRU-DGAJOAS
+-- 5	2012-02-23 10:33:41.000	Mr. Smith		msmith@ucdavis.edu	Because reading is fun-damental	Scott Kirkland	ACRU-FJ6GZIL
+-- 6	2012-02-23 10:39:19.000	Mr. Smith		msmith@ucdavis.edu	Because reading is fun-damental	Scott Kirkland	ACRU-DCZDRMJ
+-- 7	2012-02-23 11:21:04.000	Mr. Smith		msmith@ucdavis.edu	Because reading is fun-damental	Scott Kirkland	ACRU-C1L5RCV
 --
 -- =============================================
-ALTER FUNCTION udf_GetOrderResults 
+CREATE FUNCTION udf_GetOrderResults
 (	
     -- Add the parameters for the function here
     @UserId varchar(10), 
@@ -35,15 +40,16 @@ RETURNS TABLE
 AS
 RETURN 
 (
-    SELECT TOP 100 PERCENT O.[Id]
+  SELECT TOP 100 PERCENT O.[Id]
       ,[DateCreated]
       ,[DeliverTo]
       ,[DeliverToEmail]
       ,[Justification]
-      ,[CreatedBy]
+      ,[FirstName] + ' ' + [LastName] AS [CreatedBy]
       ,[RequestNumber]
   FROM [PrePurchasing].[dbo].[Orders] O
-  INNER JOIN [PrePurchasing].[dbo].[vAccess] A ON O.Id = A.OrderId 
-  WHERE CONTAINS(justification, @ContainsSearchCondition) AND A.AccessUserId = @UserId AND A.isadmin = 0 
+  INNER JOIN [PrePurchasing].[dbo].[Users] U ON O.[CreatedBy] = U.[Id]
+  INNER JOIN [PrePurchasing].[dbo].[vAccess] A ON O.[Id] = A.[OrderId] 
+  WHERE CONTAINS(([Justification], [RequestNumber], [DeliverTo], [DeliverToEmail]), @ContainsSearchCondition) AND A.[AccessUserId] = @UserId AND A.[isadmin] = 0 
 )
 GO
