1c6f3a24e3f5172ea04ae393b71ff13c9d26735a
Alan Lai <anlai@ucdavis.edu>
2012-02-22 22:17:21 -0800

    fixed issue with db project and updated according to ken's.

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Keys/FK_WorkgroupsXOrganizations_vOrganizations_2.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[WorkgroupsXOrganizations]
+    ADD CONSTRAINT [FK_WorkgroupsXOrganizations_vOrganizations] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[vOrganizations] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
