c6af793070dd4d6cbe143d10bc208bc63fb7938f
Scott Kirkland <srkirkland@gmail.com>
2012-02-24 10:03:26 -0800

    Moved common startup code into startup.js and added global Config js namespace.

---
+++ Purchasing.Web/Scripts/public/common/startup.js
@@ -0,0 +1,23 @@
+var uvOptions = {};
+
+if (typeof console == "undefined") { //Define the console as a backup for browsers without console
+    window.console = {
+        log: function () { }
+    };
+}
+
+$(function() { //run the page load startup code
+    $('input[type=checkbox]').tzCheckbox({ labels: ['Yes', 'No'] });
+    $(".dt-table, .datatable").dataTable({ "bJQueryUI": true, "sPaginationType": "full_numbers", "iDisplayLength": window.Configuration.dataTablesPageSize });
+    $(".button, .text_btn, button").button();
+    $('input[type="datetime"]').datepicker();
+    $('input.datepicker').datepicker();
+
+    initUservoice();
+});
+
+function initUservoice() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/39iNhpJPwlSkFDpX5Ajxw.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+}
