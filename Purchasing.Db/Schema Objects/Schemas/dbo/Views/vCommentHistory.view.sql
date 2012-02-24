49ecdc4c1361aa904d4b86586ff4ec7b7dbca212
Alan Lai <anlai@ucdavis.edu>
2012-02-24 09:19:52 -0800

    Imported some more of ken's changes.
    also tweaked the comment history so that if you have access to to an order, you can see the comment history.

--- Purchasing.Db/Schema Objects/Schemas/dbo/Views/vCommentHistory.view.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Views/vCommentHistory.view.sql
@@ -1,5 +1,4 @@
 ﻿CREATE VIEW dbo.vCommentHistory
-
 AS
 
 SELECT DISTINCT NEWID() AS id, dbo.OrderComments.OrderId, dbo.Orders.RequestNumber, dbo.Users.FirstName + ' ' + dbo.Users.LastName AS CreatedBy, 
@@ -10,21 +9,8 @@ FROM dbo.OrderComments
 	INNER JOIN (
 		SELECT DISTINCT OrderId, UserId
 		FROM (
-			SELECT OrderId, UserId
-			FROM dbo.OrderTracking
-			UNION
-			SELECT dbo.Approvals.OrderId, dbo.Approvals.UserId
-			FROM dbo.Approvals 
-				INNER JOIN dbo.OrderStatusCodes AS osc ON dbo.Approvals.OrderStatusCodeId = osc.Id 
-				INNER JOIN dbo.Orders AS Orders_2 ON dbo.Approvals.OrderId = Orders_2.Id 
-				INNER JOIN dbo.OrderStatusCodes AS osc2 ON Orders_2.OrderStatusCodeId = osc2.Id AND osc.[Level] = osc2.[Level]
-			WHERE dbo.Approvals.UserId IS NOT NULL
-			UNION
-			SELECT Approvals_1.OrderId, Approvals_1.SecondaryUserId
-			FROM dbo.Approvals AS Approvals_1 
-				INNER JOIN dbo.OrderStatusCodes AS osc ON Approvals_1.OrderStatusCodeId = osc.Id 
-				INNER JOIN dbo.Orders AS Orders_1 ON Approvals_1.OrderId = Orders_1.Id 
-				INNER JOIN dbo.OrderStatusCodes AS osc2 ON Orders_1.OrderStatusCodeId = osc2.Id AND osc.[Level] = osc2.[Level]
-			WHERE (Approvals_1.SecondaryUserId IS NOT NULL)
+			select orderid, accessuserid userid
+			from vaccess
+			where isadmin = 0
 		) AS blank
 	) AS access ON access.OrderId = dbo.OrderComments.OrderId
