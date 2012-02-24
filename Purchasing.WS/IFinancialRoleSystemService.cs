0a09fc940f5505881f79e155968006077194deb1
Alan Lai <anlai@ucdavis.edu>
2012-02-24 12:05:46 -0800

    most of the webservice calls are making proper calls, still do not have it returning much.
    and waiting for hellp on the submit order.

---
+++ Purchasing.WS/IFinancialRoleSystemService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Purchasing.WS
+{
+    public interface IFinancialRoleSystemService
+    {
+        List<AccountInfo> GetAccountInfos(List<AccountInfo> accounts);
+
+        AccountInfo GetAccountInfo(AccountInfo account);
+
+        bool IsFiscalOfficer(AccountInfo account, string userId);
+    }
+}
