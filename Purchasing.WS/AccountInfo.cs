2045aca88f2183ebe091c544244fc3685cac9437
Alan Lai <anlai@ucdavis.edu>
2012-02-24 15:51:42 -0800

    submissions possible.  just waiting on documentation.

--- Purchasing.WS/AccountInfo.cs
+++ Purchasing.WS/AccountInfo.cs
@@ -1,7 +1,25 @@
+using Purchasing.WS.FinancialRoleService;
+
 namespace Purchasing.WS
 {
     public class AccountInfo
     {
+        public AccountInfo(string chart, string number)
+        {
+            Chart = chart;
+            Number = number;
+        }
+
+        public AccountInfo(simpleAccountInfo simpleAccountInfo)
+        {
+            Chart = simpleAccountInfo.chartOfAccountsCode;
+            Number = simpleAccountInfo.accountNumber;
+
+            Name = simpleAccountInfo.accountName;
+            FiscalOfficerPrincipalId = simpleAccountInfo.fiscalOfficerPrincipalId;
+            FiscalOfficerPrincipalUserId = simpleAccountInfo.fiscalOfficerPrincipalName;
+        }
+
         public string Chart { get; set; }
         public string Number { get; set; }
 
