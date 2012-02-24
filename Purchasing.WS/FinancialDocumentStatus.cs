05e898adb2c615265dd60e4e5108121a9bb62178
Alan Lai <anlai@ucdavis.edu>
2012-02-24 15:51:42 -0800

    submissions possible.  just waiting on documentation.

---
+++ Purchasing.WS/FinancialDocumentStatus.cs
@@ -0,0 +1,40 @@
+using Purchasing.WS.PurchaseDocumentService;
+
+namespace Purchasing.WS
+{
+    public class FinancialDocumentStatus
+    {
+        public FinancialDocumentStatus(purchaseRequisitionStatusInfo prStatus)
+        {
+            DocumentNumber = prStatus.documentNumber;
+            FullyPaid = prStatus.fullyPaid;
+            Received = prStatus.received;
+            RouteLevel = prStatus.routeLevel;
+            RouteStatusCode = prStatus.routeStatusCode;
+
+            PoDocumentNumber = prStatus.purchaseOrderDocumentNumber;
+            PoNumber = prStatus.purchaseOrderNumber;
+            PoStatusCode = prStatus.purchaseOrderStatusCode;
+            PoStatusName = prStatus.purchaseOrderStatusName;
+            PoTypeCode = prStatus.purchaseOrderTypeCode;
+
+            PrStatusCode = prStatus.purchaseRequsitionStatusCode;
+            PRStatusName = prStatus.purchaseRequsitionStatusName;
+        }
+
+        public string DocumentNumber { get; set; }
+        public bool FullyPaid { get; set; }
+        public bool Received { get; set; }
+        public string RouteLevel { get; set; }
+        public string RouteStatusCode { get; set; }
+
+        public string PoDocumentNumber { get; set; }
+        public string PoNumber { get; set; }
+        public string PoStatusCode { get; set; }
+        public string PoStatusName { get; set; }
+        public string PoTypeCode { get; set; }
+
+        public string PrStatusCode { get; set; }
+        public string PRStatusName { get; set; }
+    }
+}
--- Purchasing.WS/IFinancialRoleSystemService.cs
+++ Purchasing.WS/IFinancialRoleSystemService.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Purchasing.WS
 {
     public interface IFinancialRoleSystemService
     {
+        /// <summary>
+        /// Gets basic account information for multiple accounts
+        /// </summary>
+        /// <param name="accounts"></param>
+        /// <returns></returns>
         List<AccountInfo> GetAccountInfos(List<AccountInfo> accounts);
 
+        /// <summary>
+        /// Gets basic account information for a single account
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
         AccountInfo GetAccountInfo(AccountInfo account);
 
+        /// <summary>
+        /// Is given user the fiscal officer?
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         bool IsFiscalOfficer(AccountInfo account, string userId);
     }
 }
