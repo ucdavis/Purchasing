831f2f21b9b12aa5633f3211497185248830a588
Scott Kirkland <srkirkland@gmail.com>
2012-02-25 17:41:30 -0800

    Removed obsolete order list model

--- Purchasing.Web/Models/FilteredOrderListModel.cs
+++ Purchasing.Web/Models/FilteredOrderListModel.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using Purchasing.Core.Domain;
 using UCDArch.Core.PersistanceSupport;
-using UCDArch.Core.Utils;
 
 namespace Purchasing.Web.Models
 {
@@ -27,19 +26,14 @@ namespace Purchasing.Web.Models
 
         // for building dropdown list
         public List<Tuple<string, string>> OrderStatusCodes { get; set; }
-        public IList<Order> Orders { get; set; }
-        //public List<string> CheckedOrderStatusCodes { get; set; }
+        
         public string SelectedOrderStatus { get; set; }
-        //public bool ShowAll { get; set; }
-        //public bool ShowCompleted { get; set; }
         public bool ShowPending { get; set; }
         [Display(Name = "Created After")]
         public DateTime? StartDate { get; set; }
         [Display(Name = "Created Before")]
         public DateTime? EndDate { get; set; }
-        //public bool ShowOwned { get; set; }
         public ColumnPreferences ColumnPreferences { get; set; }
-        //public bool HideOrdersYouCreated { get; set; }
         public string ShowLast { get; set; }
 
         public void PopulateStatusCodes(IRepositoryWithTypedId<OrderStatusCode, string> statusCodeRepository, List<Tuple<string, string>> orderStatusCodes = null)
@@ -57,53 +51,6 @@ namespace Purchasing.Web.Models
             {
                 OrderStatusCodes = orderStatusCodes;
             }
-        }
-    }
-
-    public class FilteredOrderListModel
-    {
-        // for building dropdown list
-        public List<Tuple<string,string>> OrderStatusCodes { get; set; }
-        public IList<Order> Orders { get; set; }
-        //public List<string> CheckedOrderStatusCodes { get; set; }
-        public string SelectedOrderStatus { get; set; }
-        //public bool ShowAll { get; set; }
-        //public bool ShowCompleted { get; set; }
-        public bool ShowPending { get; set; }
-        [Display(Name = "Created After")]
-        public DateTime? StartDate { get; set; }
-        [Display(Name = "Created Before")]
-        public DateTime? EndDate { get; set; }
-        //public bool ShowOwned { get; set; }
-        public ColumnPreferences ColumnPreferences { get; set; }
-        //public bool HideOrdersYouCreated { get; set; }
-        public string ShowLast { get; set; }
-
-        public static FilteredOrderListModel Create(IRepository repository, IList<Order> orders, List<Tuple<string,string>> orderStatusCodes = null)
-        {
-            Check.Require(repository != null, "Repository must be supplied");
-
-            var viewModel = new FilteredOrderListModel
-            {
-                Orders = orders
-            };
-            if(orderStatusCodes == null)
-            {
-                viewModel.OrderStatusCodes = new List<Tuple<string, string>>();
-                viewModel.OrderStatusCodes.Add(new Tuple<string, string>("All", "All"));
-                viewModel.OrderStatusCodes.AddRange(repository.OfType<OrderStatusCode>().Queryable
-                    .Where(a => a.ShowInFilterList)
-                    .OrderBy(a => a.Level)
-                    .Select(a => new Tuple<string, string>(a.Id, a.Name))
-                    .ToList());
-            }
-            else
-            {
-                viewModel.OrderStatusCodes = orderStatusCodes;
-            }
-            //viewModel.CheckedOrderStatusCodes = new List<string>();
-
-            return viewModel;
         }
     }
 }
--- Purchasing.Web/Models/LandingPageViewModel.cs
+++ Purchasing.Web/Models/LandingPageViewModel.cs
@@ -1,26 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using Purchasing.Core.Domain;
+﻿using System.Collections.Generic;
 using Purchasing.Core.Queries;
-using Purchasing.Web.Controllers;
 
 namespace Purchasing.Web.Models
 {
-    [Obsolete]
-    public class LandingPageViewModel
-    {
-        public int YourOpenRequestCount { get; set; }
-        public int YourNotActedOnCount { get; set; }
-        public int OrdersPendingYourActionCount { get; set; }
-        public ColumnPreferences ColumnPreferences { get; set; }
-        //public List<RequestedHistory> RequestedHistories { get; set; }
-        public FilteredOrderListModel OldestFOLM { get; set; }
-        public FilteredOrderListModel NewestFOLM { get; set; }
-        public FilteredOrderListModel LastFiveFOLM { get; set; }
-        public List<RequesterTotals> RequesterTotals { get; set; }
-
-    }
-
     public class LandingViewModel
     {
         public IEnumerable<OrderHistoryBase> PendingOrders { get; set; }
