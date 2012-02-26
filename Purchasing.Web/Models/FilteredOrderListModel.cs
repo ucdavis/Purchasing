d571e9e1def46973cbc53fa5203dc2049160e23d
Scott Kirkland <srkirkland@gmail.com>
2012-02-25 17:05:42 -0800

    Created a new filterDto for order history. This will allow for getting data projections

--- Purchasing.Web/Models/FilteredOrderListModel.cs
+++ Purchasing.Web/Models/FilteredOrderListModel.cs
@@ -8,6 +8,50 @@ using UCDArch.Core.Utils;
 
 namespace Purchasing.Web.Models
 {
+    public class FilteredOrderListModelDto
+    {
+        public IQueryable<OrderHistoryDto> OrderHistoryDtos { get; set; }
+
+        public class OrderHistoryDto
+        {
+            public Order Order { get; set; }
+        }
+
+        // for building dropdown list
+        public List<Tuple<string, string>> OrderStatusCodes { get; set; }
+        public IList<Order> Orders { get; set; }
+        //public List<string> CheckedOrderStatusCodes { get; set; }
+        public string SelectedOrderStatus { get; set; }
+        //public bool ShowAll { get; set; }
+        //public bool ShowCompleted { get; set; }
+        public bool ShowPending { get; set; }
+        [Display(Name = "Created After")]
+        public DateTime? StartDate { get; set; }
+        [Display(Name = "Created Before")]
+        public DateTime? EndDate { get; set; }
+        //public bool ShowOwned { get; set; }
+        public ColumnPreferences ColumnPreferences { get; set; }
+        //public bool HideOrdersYouCreated { get; set; }
+        public string ShowLast { get; set; }
+
+        public void PopulateStatusCodes(IRepositoryWithTypedId<OrderStatusCode, string> statusCodeRepository, List<Tuple<string, string>> orderStatusCodes = null)
+        {
+            if (orderStatusCodes == null)
+            {
+                OrderStatusCodes = new List<Tuple<string, string>> {new Tuple<string, string>("All", "All")};
+                OrderStatusCodes.AddRange(statusCodeRepository.Queryable
+                    .Where(a => a.ShowInFilterList)
+                    .OrderBy(a => a.Level)
+                    .Select(a => new Tuple<string, string>(a.Id, a.Name))
+                    .ToList());
+            }
+            else
+            {
+                OrderStatusCodes = orderStatusCodes;
+            }
+        }
+    }
+
     public class FilteredOrderListModel
     {
         // for building dropdown list
