c581cd2e5def4fe2d25768f047c91d91d7412ce0
Scott Kirkland <srkirkland@gmail.com>
2012-02-27 13:04:39 -0800

    Css added to bundle/minify workflow. /Css/ is bundled, use /css/single for one-offs

--- Purchasing.Web/CassetteConfiguration.cs
+++ Purchasing.Web/CassetteConfiguration.cs
@@ -19,16 +19,18 @@ namespace Purchasing.Web
             // This default configuration treats each file as a separate 'bundle'.
             // In production the content will be minified, but the files are not combined.
             // So you probably want to tweak these defaults!
-            bundles.AddPerIndividualFile<StylesheetBundle>("Css");
+            //bundles.AddPerIndividualFile<StylesheetBundle>("Css");
             //bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
 
             // To combine files, try something like this instead:
             //   bundles.Add<StylesheetBundle>("Content");
             // In production mode, all of ~/Content will be combined into a single bundle.
             //TODO: combine CSS into a bundle
+            bundles.AddPerSubDirectory<StylesheetBundle>("Css", new FileSearch {Exclude = new Regex("/single/")});
+            bundles.AddPerIndividualFile<StylesheetBundle>("Css/single");
 
             bundles.AddPerSubDirectory<ScriptBundle>("Scripts/external"); //just the CDN hosted stuff
-            bundles.AddPerSubDirectory<ScriptBundle>("Scripts/public", new FileSearch {Exclude = new Regex("/single/"),});
+            bundles.AddPerSubDirectory<ScriptBundle>("Scripts/public", new FileSearch {Exclude = new Regex("/single/")});
             bundles.AddPerIndividualFile<ScriptBundle>("Scripts/public/single"); //stuff in single is shared and can be referenced individually
             // If you want a bundle per folder, try this:
             //   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
