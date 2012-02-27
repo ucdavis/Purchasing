e7449537b6ee0a0a28fc5c941fc86d2ee2026ac8
Alan Lai <anlai@ucdavis.edu>
2012-02-27 13:57:44 -0800

    added "allow controlled substances" field to workgorup, and in the forms to enable that feature.  fixed the order page so it displays the controlled substances field stuff accordingly.
    updated the db to the new views ken wrote to use with FTS.  removed the udfs.

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/DF_Workgroups_AllowControlledSubstances.defconst.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[Workgroups]
+    ADD CONSTRAINT [DF_Workgroups_AllowControlledSubstances] DEFAULT ((0)) FOR [AllowControlledSubstances];
+
---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Views/vCommentResults.view.sql
@@ -0,0 +1,7 @@
+﻿
+CREATE VIEW [dbo].[vCommentResults] WITH SCHEMABINDING
+AS
+SELECT     OC.Id, OC.OrderId, O.RequestNumber, OC.DateCreated, OC.Text, U.FirstName + ' ' + U.LastName AS CreatedBy
+FROM         dbo.OrderComments AS OC INNER JOIN
+                      dbo.Orders AS O ON OC.OrderId = O.Id INNER JOIN
+                      dbo.Users AS U ON OC.UserId = U.Id
