83b97bfe9b1d620307249b77de8da7ab0d7c664b
Alan Lai <anlai@ucdavis.edu>
2012-02-27 15:10:45 -0800

    added faq object.

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Faqs.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Faqs]
+(
+	[Id]			int				NOT NULL	IDENTITY(1,1)	PRIMARY KEY, 
+	[Category]		varchar(50)		NOT NULL,
+	[Question]		varchar(MAX)	NOT NULL,
+	[Answer]		varchar(MAX)	NOT NULL,
+	[OrgId]			varchar(10)		NULL,
+	[Like]			int				NOT NULL	DEFAULT 0
+)
