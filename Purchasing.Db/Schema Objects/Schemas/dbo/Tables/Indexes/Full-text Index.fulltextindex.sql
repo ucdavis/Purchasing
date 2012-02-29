36fe2feb4b2d4036e143ddbd0b2c3708dedf6d08
Alan Lai <anlai@ucdavis.edu>
2012-02-29 15:28:14 -0800

    imported ken's changes to the database for enabling fts for lookup tables.

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Indexes/Full-text Index.fulltextindex.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Indexes/Full-text Index.fulltextindex.sql
@@ -1,7 +1,7 @@
-﻿CREATE FULLTEXT INDEX ON [dbo].[Orders]
+﻿/*CREATE FULLTEXT INDEX ON [dbo].[Orders]
     ([DeliverTo] LANGUAGE 1033, [DeliverToEmail] LANGUAGE 1033, [Justification] LANGUAGE 1033, [RequestNumber] LANGUAGE 1033)
     KEY INDEX [PK_Orders_1]
-    ON [Orders_justification_SIDX];
+    ON [Orders_justification_SIDX];*/
 
 
 
---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/Indexes/Full-text Index_4.fulltextindex.sql
@@ -0,0 +1,5 @@
+﻿CREATE FULLTEXT INDEX ON [dbo].[vCommodities]
+    ([Name] LANGUAGE 1033, [Id] LANGUAGE 1033)
+    KEY INDEX [vCommodities_Id_UDX]
+    ON [vCommodities_IdName_SDX];
+
