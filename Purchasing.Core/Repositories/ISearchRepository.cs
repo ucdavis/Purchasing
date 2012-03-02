72b4e6f817d6ed4bb2831ba87ecaa86ab5b00035
Scott Kirkland <srkirkland@gmail.com>
2012-03-02 15:21:00 -0800

    Updated the searches to all use udfs

--- Purchasing.Core/Repositories/ISearchRepository.cs
+++ Purchasing.Core/Repositories/ISearchRepository.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using NHibernate.Transform;
-using Purchasing.Core.Queries;
-using UCDArch.Core.PersistanceSupport;
-using UCDArch.Data.NHibernate;
-using Purchasing.Core.Domain;
+﻿using System.Collections.Generic;
 using NHibernate;
+using NHibernate.Transform;
+using Purchasing.Core.Domain;
+using Purchasing.Core.Queries;
+using UCDArch.Data.NHibernate;
 
 namespace Purchasing.Core.Repositories
 {
@@ -37,64 +35,39 @@ namespace Purchasing.Core.Repositories
 
     public class SearchRepository : ISearchRepository
     {
-        public ISession Session { get { return NHibernateSessionManager.Instance.GetSession(); } }
+        public ISession Session
+        {
+            get { return NHibernateSessionManager.Instance.GetSession(); }
+        }
+
+        #region ISearchRepository Members
 
         public IList<SearchResults.OrderResult> SearchOrders(string searchTerm, string user)
         {
-            const string queryFields = "(RES.[Justification], RES.[RequestNumber], RES.[DeliverTo], RES.[DeliverToEmail])";
-            return PerformQuery<SearchResults.OrderResult>(user, searchTerm, queryFields, "Order", queryId: "Id");
+            return PerformQuery<SearchResults.OrderResult>(searchTerm, "Order", user);
         }
 
         public IList<SearchResults.LineResult> SearchLineItems(string searchTerm, string user)
         {
-            const string queryFields = "(RES.[Description], RES.[Url], RES.[Notes], RES.[CatalogNumber], RES.[CommodityId])";
-            return PerformQuery<SearchResults.LineResult>(user, searchTerm, queryFields, "Line");
+            return PerformQuery<SearchResults.LineResult>(searchTerm, "Line", user);
         }
 
         public IList<SearchResults.CustomFieldResult> SearchCustomFieldAnswers(string searchTerm, string user)
         {
-            const string queryFields = "(RES.[Answer])";
-            return PerformQuery<SearchResults.CustomFieldResult>(user, searchTerm, queryFields, "CustomField");
+            return PerformQuery<SearchResults.CustomFieldResult>(searchTerm, "CustomField", user);
         }
 
         public IList<SearchResults.CommentResult> SearchComments(string searchTerm, string user)
         {
-            const string queryFields = "(RES.[text])";
-            return PerformQuery<SearchResults.CommentResult>(user, searchTerm, queryFields, "Comment");
+            return PerformQuery<SearchResults.CommentResult>(searchTerm, "Comment", user);
         }
-
-        private IList<T> PerformQuery<T>(string user, string query, string queryFields, string queryType, string queryId = "OrderId")
-        {
-            var q = Session.CreateSQLQuery(string.Format(@"SELECT RES.* FROM [PrePurchasing].[dbo].[v{0}Results] RES
-                INNER JOIN [PrePurchasing].[dbo].[vAccess] A 
-                ON RES.[{1}] = A.[OrderId] 
-                WHERE FREETEXT({2}, :query) 
-                AND A.[AccessUserId] = :user AND A.[isadmin] = 0", queryType, queryId, queryFields))
-                .SetString("query", query)
-                .SetString("user", user)
-                .SetResultTransformer(Transformers.AliasToBean<T>());
-
-            return q.List<T>();
-        }
-
-        private IList<T> PerformUdfQuery<T>(string query, string queryType, string user = null)
-        {
-            var q = Session.CreateSQLQuery(
-                string.Format(
-                    @"DECLARE @ContainsSearchCondition varchar(255) = '{0}';
-                    SELECT * from dbo.udf_Get{1}Results(@ContainsSearchCondition)",
-                    query, queryType))
-                .SetResultTransformer(Transformers.AliasToBean<T>());
-
-            return q.List<T>();
-        } 
 
         /// <summary>
         /// Searches commodities
         /// </summary>
         public IList<Commodity> SearchCommodities(string searchTerm)
         {
-            return PerformUdfQuery<Commodity>(searchTerm, "Commodities");
+            return PerformQuery<Commodity>(searchTerm, "Commodities");
         }
 
         /// <summary>
@@ -102,7 +75,7 @@ namespace Purchasing.Core.Repositories
         /// </summary>
         public IList<Vendor> SearchVendors(string searchTerm)
         {
-            return PerformUdfQuery<Vendor>(searchTerm, "Vendor");
+            return PerformQuery<Vendor>(searchTerm, "Vendor");
         }
 
         /// <summary>
@@ -110,7 +83,29 @@ namespace Purchasing.Core.Repositories
         /// </summary>
         public IList<Account> SearchAccounts(string searchTerm)
         {
-            return PerformUdfQuery<Account>(searchTerm, "Account");
+            return PerformQuery<Account>(searchTerm, "Account");
+        }
+
+        #endregion
+
+        private IList<T> PerformQuery<T>(string query, string queryType, string user = null)
+        {
+            var queryParams = new List<string> {"@User", "@ContainsSearchCondition"};
+
+            if (string.IsNullOrWhiteSpace(user))
+            {
+                queryParams.Remove("@User");
+            }
+
+            IQuery q = Session.CreateSQLQuery(
+                string.Format(
+                    @"DECLARE @User varchar(255) = '{0}';
+                    DECLARE @ContainsSearchCondition varchar(255) = '{1}';
+                    SELECT * from dbo.udf_Get{2}Results({3})",
+                    user, query, queryType, string.Join(",", queryParams)))
+                .SetResultTransformer(Transformers.AliasToBean<T>());
+
+            return q.List<T>();
         }
     }
 }
