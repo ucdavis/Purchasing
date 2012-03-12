93bf1654f0f953b1440ca93c92b727344532bd0a
Alan Lai <anlai@ucdavis.edu>
2012-03-12 07:25:43 -0700

    imported new vbuilding table

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/vBuildings.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[vBuildings] (
+    [CampusCode]      VARCHAR (2)  NOT NULL,
+    [BuildingCode]    VARCHAR (4)  NOT NULL,
+    [CampusName]      VARCHAR (40) NULL,
+    [CampusShortName] VARCHAR (12) NULL,
+    [CampusTypeCode]  VARCHAR (1)  NULL,
+    [BuildingName]    VARCHAR (80) NULL,
+    [LastUpdateDate]  DATETIME     NULL,
+    [IsActive]        BIT          NULL
+);
+
