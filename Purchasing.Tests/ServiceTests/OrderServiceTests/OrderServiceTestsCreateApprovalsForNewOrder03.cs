79746d9f1fd5c4c9b5529127ead7869581f2a166
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-03-12 15:01:10 -0700

    WIP OrderService Tests

---
+++ Purchasing.Tests/ServiceTests/OrderServiceTests/OrderServiceTestsCreateApprovalsForNewOrder03.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Purchasing.Core;
+using Purchasing.Core.Domain;
+using Purchasing.Tests.Core;
+using Purchasing.WS;
+using Purchasing.Web.Services;
+using Rhino.Mocks;
+using UCDArch.Core.PersistanceSupport;
+using Purchasing.Tests;
+using UCDArch.Core.Utils;
+using UCDArch.Testing;
+
+namespace Purchasing.Tests.ServiceTests.OrderServiceTests
+{
+    public partial class OrderServiceTests
+    {
+    }
+}
