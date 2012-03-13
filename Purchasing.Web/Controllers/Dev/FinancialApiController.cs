5d3174bfaa822e2189b706d7270bbbe099960cd4
Alan Lai <anlai@ucdavis.edu>
2012-03-13 08:36:42 -0700

    wrote in a quick ajax call to load kfs info.

--- Purchasing.Web/Controllers/Dev/FinancialApiController.cs
+++ Purchasing.Web/Controllers/Dev/FinancialApiController.cs
@@ -31,21 +31,29 @@ namespace Purchasing.Web.Controllers
             var order1 = _repositoryFactory.OrderRepository.GetNullableById(40);
             var result1 = service.SubmitOrder(order1, "mceligot");
             ViewBag.Order1DocNum = result1.DocNumber;
+            order1.PoNumber = result1.DocNumber;
+            _repositoryFactory.OrderRepository.EnsurePersistent(order1);
 
             // another standard order, with decimal line items
             var order2 = _repositoryFactory.OrderRepository.GetNullableById(42);
             var result2 = service.SubmitOrder(order2, "mceligot");
             ViewBag.Order2DocNum = result2.DocNumber;
+            order2.PoNumber = result2.DocNumber;
+            _repositoryFactory.OrderRepository.EnsurePersistent(order2);
 
             // order level splits
             var order3 = _repositoryFactory.OrderRepository.GetNullableById(43);
             var result3 = service.SubmitOrder(order3, "mceligot");
             ViewBag.Order3DocNum = result3.DocNumber;
+            order3.PoNumber = result3.DocNumber;
+            _repositoryFactory.OrderRepository.EnsurePersistent(order3);
 
             // line item splits
             var order4 = _repositoryFactory.OrderRepository.GetNullableById(45);
             var result4 = service.SubmitOrder(order4, "mceligot");
             ViewBag.Order4DocNum = result4.DocNumber;
+            order4.PoNumber = result4.DocNumber;
+            _repositoryFactory.OrderRepository.EnsurePersistent(order4);
 
             return View();
         }
