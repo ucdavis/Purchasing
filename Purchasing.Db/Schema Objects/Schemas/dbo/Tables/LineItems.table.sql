c5ade41c50e1cd6e6229990ad63d5acd35ec9079
Alan Lai <anlai@ucdavis.edu>
2012-03-14 08:18:34 -0700

    pulled in db changes and updated lineitems, to have a "qty received"

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/LineItems.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/LineItems.table.sql
@@ -1,15 +1,18 @@
 ﻿CREATE TABLE [dbo].[LineItems] (
-    [Id]            INT             IDENTITY (1, 1) NOT NULL,
-    [Quantity]      DECIMAL (18, 3) NOT NULL,
-    [CatalogNumber] VARCHAR (25)    NULL,
-    [Description]   VARCHAR (MAX)   NOT NULL,
-    [Unit]          VARCHAR (25)    NULL,
-    [UnitPrice]     MONEY           NOT NULL,
-    [Url]           VARCHAR (200)   NULL,
-    [Notes]         VARCHAR (MAX)   NULL,
-    [OrderId]       INT             NOT NULL,
-    [CommodityId]   VARCHAR (9)     NULL
+    [Id]               INT             IDENTITY (1, 1) NOT NULL,
+    [Quantity]         DECIMAL (18, 3) NOT NULL,
+    [QuantityReceived] DECIMAL (18, 3) NULL,
+    [CatalogNumber]    VARCHAR (25)    NULL,
+    [Description]      VARCHAR (MAX)   NOT NULL,
+    [Unit]             VARCHAR (25)    NULL,
+    [UnitPrice]        MONEY           NOT NULL,
+    [Url]              VARCHAR (200)   NULL,
+    [Notes]            VARCHAR (MAX)   NULL,
+    [OrderId]          INT             NOT NULL,
+    [CommodityId]      VARCHAR (9)     NULL
 );
+
+
 
 
 
