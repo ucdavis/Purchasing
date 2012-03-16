00c97f0227d2317b93984da1f755c3ba389eb0a5
unknown <sulai@.ou.ad3.ucdavis.edu>
2012-03-16 13:31:01 -0700

    updated a few db objects to be more generic and not with hard coded paths.

--- Purchasing.Db/Schema Objects/Database Level Objects/Storage/Files/PrePurchasing_KfsData_log1.sqlfile.sql
+++ Purchasing.Db/Schema Objects/Database Level Objects/Storage/Files/PrePurchasing_KfsData_log1.sqlfile.sql
@@ -1,3 +1,3 @@
 ﻿ALTER DATABASE [$(DatabaseName)]
-    ADD LOG FILE (NAME = [PrePurchasing_KfsData_log1], FILENAME = 'E:\DB\PrePurchasing_KfsData_log1.ldf', SIZE = 5195968 KB, MAXSIZE = 2097152 MB, FILEGROWTH = 10 %);
+    ADD LOG FILE (NAME = [PrePurchasing_KfsData_log1], FILENAME = '$(DefaultDataPath)PrePurchasing_KfsData_log1.ldf', SIZE = 5195968 KB, MAXSIZE = 2097152 MB, FILEGROWTH = 10 %);
 
