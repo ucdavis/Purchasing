9143c95a9a7536af612778f90a315207d16934d6
Alan Lai <anlai@ucdavis.edu>
2012-04-04 08:04:47 -0700

    approval view, which shows which one is workgroup permissions

---
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Views/vApprovals.view.sql
@@ -0,0 +1,28 @@
+﻿/*
+
+	Description:
+
+	Logic for determining when an approval goes out to the workgroup permissions.
+
+	Conditions for workgroup permissions:
+		* Approval is not conditional approval
+		* Either: Primary user is not assigned or primary user is away
+
+*/
+
+CREATE VIEW [dbo].[vApprovals]
+	AS 
+
+select approvals.id, completed, orderstatuscodeid, orderid, splitid
+	, os.level
+	, pusers.id primaryuserid, pusers.isaway primaryaway
+	, susers.id secondaryuserid, susers.isaway secondaryaway
+	, cast (case
+		when orderstatuscodeid <> 'CA' and pusers.id is null then 1
+		when orderstatuscodeid <> 'CA' and pusers.id is not null and pusers.isaway = 1 then 1
+		else 0
+	  end as bit) IsWorkgroup	
+from approvals
+inner join orderstatuscodes os on os.id = approvals.orderstatuscodeid
+left outer join users pusers on approvals.userid = pusers.id
+left outer join users susers on approvals.secondaryuserid = susers.id
