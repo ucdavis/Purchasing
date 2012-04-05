5c8de17c7a04abf17001b02c88909f7a6416f6cd
Scott Kirkland <srkirkland@gmail.com>
2012-04-05 10:37:01 -0700

    removing all old FAQ files and code

--- Purchasing.Web/Controllers/HelpController.cs
+++ Purchasing.Web/Controllers/HelpController.cs
@@ -19,14 +19,13 @@ namespace Purchasing.Web.Controllers
     /// Getting replaced with user voice, once that integration happens, we will delete this controller and all views.
     /// anlai 4/1/2012
     /// </remarks>
+    [HandleTransactionsManually]
     public class HelpController : ApplicationController
     {
-	    private readonly IRepository<Faq> _faqRepository;
         private readonly IUservoiceService _uservoiceService;
 
-        public HelpController(IRepository<Faq> faqRepository, IUservoiceService uservoiceService)
+        public HelpController(IUservoiceService uservoiceService)
         {
-            _faqRepository = faqRepository;
             _uservoiceService = uservoiceService;
         }
 
@@ -34,25 +33,24 @@ namespace Purchasing.Web.Controllers
         /// Gets the number of active issues in the purchasing uservoice forum
         /// </summary>
         /// <returns></returns>
-        [HandleTransactionsManually]
         [OutputCache(Duration = 60)] //Cache this call for a while
         public ActionResult GetActiveIssuesCount()
         {
             var issuesCount = _uservoiceService.GetActiveIssuesCount();
-            
+
             return Json(new {HasIssues = issuesCount > 0, IssuesCount = issuesCount, TimeStamp = DateTime.Now.Ticks},
                         JsonRequestBehavior.AllowGet);
         }
 
-        [HandleTransactionsManually]
+        [OutputCache(Duration = 60)] //Cache this call for a while
         public ActionResult OpenIssues()
         {
             var issues = _uservoiceService.GetOpenIssues();
-            
+
             return new JsonNetResult(issues);
         }
 
-        [HandleTransactionsManually]
+        [OutputCache(Duration = 60)] //Cache this call for a while
         public ActionResult IssuesForUser(string user)
         {
             var issues = _uservoiceService.GetActiveIssuesForUser(user ?? CurrentUser.Identity.Name);
@@ -73,6 +71,7 @@ namespace Purchasing.Web.Controllers
         //    return Content("Status set to " + status);
         //}
 
+        /*
         [IpFilter]
         public ActionResult AutomaticJob()
         {
@@ -83,147 +82,6 @@ namespace Purchasing.Web.Controllers
             // email followup to original created user after 5-7 days old?
 
             // force close and potentially notify, if uservoice doesn't?
-
-            return View();
-        }
-
-
-        // ************************************************************
-        // Stuff below, is being replaced with above.
-        // ************************************************************
-
-        //
-        // GET: /Faq/
-        public ActionResult Index()
-        {
-            var faqList = _faqRepository.Queryable;
-
-            return View(faqList);
-        }
-
-        //
-        // GET: /Faq/Details/5
-        public ActionResult Details(int id)
-        {
-            var faq = _faqRepository.GetNullableById(id);
-
-            if (faq == null) return RedirectToAction("Index");
-
-            return View(faq);
-        }
-
-        //
-        // GET: /Faq/Create
-        public ActionResult Create()
-        {
-			var viewModel = FaqViewModel.Create(Repository);
-            
-            return View(viewModel);
-        } 
-
-        //
-        // POST: /Faq/Create
-        [HttpPost]
-        public ActionResult Create(Faq faq)
-        {
-            var faqToCreate = new Faq();
-
-            TransferValues(faq, faqToCreate);
-
-            if (ModelState.IsValid)
-            {
-                _faqRepository.EnsurePersistent(faqToCreate);
-
-                Message = "Faq Created Successfully";
-
-                return RedirectToAction("Index");
-            }
-            else
-            {
-				var viewModel = FaqViewModel.Create(Repository);
-                viewModel.Faq = faq;
-
-                return View(viewModel);
-            }
-        }
-
-        //
-        // GET: /Faq/Edit/5
-        public ActionResult Edit(int id)
-        {
-            var faq = _faqRepository.GetNullableById(id);
-
-            if (faq == null) return RedirectToAction("Index");
-
-			var viewModel = FaqViewModel.Create(Repository);
-			viewModel.Faq = faq;
-
-			return View(viewModel);
-        }
-        
-        //
-        // POST: /Faq/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, Faq faq)
-        {
-            var faqToEdit = _faqRepository.GetNullableById(id);
-
-            if (faqToEdit == null) return RedirectToAction("Index");
-
-            TransferValues(faq, faqToEdit);
-
-            if (ModelState.IsValid)
-            {
-                _faqRepository.EnsurePersistent(faqToEdit);
-
-                Message = "Faq Edited Successfully";
-
-                return RedirectToAction("Index");
-            }
-            else
-            {
-				var viewModel = FaqViewModel.Create(Repository);
-                viewModel.Faq = faq;
-
-                return View(viewModel);
-            }
-        }
-        
-        //
-        // GET: /Faq/Delete/5 
-        public ActionResult Delete(int id)
-        {
-			var faq = _faqRepository.GetNullableById(id);
-
-            if (faq == null) return RedirectToAction("Index");
-
-            return View(faq);
-        }
-
-        //
-        // POST: /Faq/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, Faq faq)
-        {
-			var faqToDelete = _faqRepository.GetNullableById(id);
-
-            if (faqToDelete == null) return RedirectToAction("Index");
-
-            _faqRepository.Remove(faqToDelete);
-
-            Message = "Faq Removed Successfully";
-
-            return RedirectToAction("Index");
-        }
-        
-        /// <summary>
-        /// Transfer editable values from source to destination
-        /// </summary>
-        private static void TransferValues(Faq source, Faq destination)
-        {
-			//Recommendation: Use AutoMapper
-            Mapper.Map(source, destination);
-        }
-
+        }*/
     }
 }
