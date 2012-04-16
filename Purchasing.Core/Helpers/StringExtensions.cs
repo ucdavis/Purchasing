65b07bba626e37dc0027b9ec3e8e48daff13bd59
Scott Kirkland <srkirkland@gmail.com>
2012-04-16 10:21:02 -0700

    updated workgroup address to show display name for easier distinction

---
+++ Purchasing.Core/Helpers/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Purchasing.Core.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string Summarize(this string str, int maxLength = 25)
+        {
+            return str.Length < maxLength ? str : string.Format("{0}...", str.Substring(0, maxLength - 3));
+        }
+    }
+}
