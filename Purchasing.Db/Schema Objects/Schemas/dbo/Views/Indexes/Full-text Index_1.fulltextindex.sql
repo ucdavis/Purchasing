7987be43b1982d6a169be92761aaa5c5a3912f0a
Scott Kirkland <srkirkland@gmail.com>
2012-04-23 13:18:56 -0700

    url field added to workgroupvendor class/map/db

--- Purchasing.Db/Schema Objects/Schemas/dbo/Views/Indexes/Full-text Index_1.fulltextindex.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Views/Indexes/Full-text Index_1.fulltextindex.sql
@@ -1,5 +1,5 @@
-﻿CREATE FULLTEXT INDEX ON [dbo].[vLineResults]
+﻿/*CREATE FULLTEXT INDEX ON [dbo].[vLineResults]
     ([CatalogNumber] LANGUAGE 1033, [Description] LANGUAGE 1033, [Url] LANGUAGE 1033, [Notes] LANGUAGE 1033, [CommodityId] LANGUAGE 1033)
     KEY INDEX [vLineResults_Id_UDX]
-    ON [LineItems_DescriptionUrlNotesCatalognumberCommoditycode_SIDX];
+    ON [LineItems_DescriptionUrlNotesCatalognumberCommoditycode_SIDX];*/
 
