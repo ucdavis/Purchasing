09492ee8ce94d03bbaca59540bd6596d3b6cb5c3
anlai <anlai@ucdavis.edu>
2012-04-25 15:17:36 -0700

    see what previous checkin by me said....it's actually that

--- Purchasing.Db/Schema Objects/Schemas/dbo/Tables/OrderRequestSaves.table.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Tables/OrderRequestSaves.table.sql
@@ -1,16 +1,17 @@
 ﻿CREATE TABLE [dbo].[OrderRequestSaves] (
-    [Id]          UNIQUEIDENTIFIER NOT NULL,
-    [Name]        VARCHAR (50)     NOT NULL,
-    [Comments]    VARCHAR (MAX)    NULL,
-    [UserId]      VARCHAR (10)     NOT NULL,
-    [PreparedBy]  VARCHAR (10)     NULL,
-    [WorkgroupId] INT              NOT NULL,
-    [FormData]    VARCHAR (MAX)    NOT NULL,
-    [AccountData] VARCHAR (MAX)    NOT NULL,
-    [DateCreated] DATETIME         NOT NULL,
-    [LastUpdate]  DATETIME         NOT NULL,
-    [Version]     VARCHAR (15)     NULL
+    [Id]           UNIQUEIDENTIFIER NOT NULL,
+    [Name]         VARCHAR (150)    NOT NULL,
+    [UserId]       VARCHAR (10)     NOT NULL,
+    [PreparedById] VARCHAR (10)     NULL,
+    [WorkgroupId]  INT              NOT NULL,
+    [FormData]     VARCHAR (MAX)    NOT NULL,
+    [AccountData]  VARCHAR (MAX)    NOT NULL,
+    [DateCreated]  DATETIME         NOT NULL,
+    [LastUpdate]   DATETIME         NOT NULL,
+    [Version]      VARCHAR (15)     NULL
 );
+
+
 
 
 
