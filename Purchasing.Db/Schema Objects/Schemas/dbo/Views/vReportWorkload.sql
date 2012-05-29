c77c5047f57023099a9b60b5fad1738c02982459
anlai <anlai@ucdavis.edu>
2012-05-29 10:25:13 -0700

    updated view for workload report, that deals with non-admin workgroups.

--- Purchasing.Db/Schema Objects/Schemas/dbo/Views/vReportWorkload.sql
+++ Purchasing.Db/Schema Objects/Schemas/dbo/Views/vReportWorkload.sql
@@ -5,16 +5,17 @@ select row_number() over (order by ReportingWorkgroupId) id, ReportingWorkgroupI
 	, WorkgroupName, WorkgroupOrg
 	, sum(Approved) Approved, sum(Edited) Edited
 from (
+
 	select distinct w.id ReportingWorkgroupId, w.name ReportWorkgroup, vaw.workgroupid, vaw.primaryorganizationid WorkgroupOrg, vaw.name WorkgroupName
 		, o.id, ot.userid, ot.orderstatuscodeid
 		, case  when ot.[description] like '%approve%' then 1
 				when ot.[description] like '%created%' then 1
 				else 0
-		  end Approved
+			end Approved
 		, case when ot.[description] like '%edit%' then 1
-			   when ot.[description] like '%rerouted%' then 1
-			   else 0
-		  end Edited
+				when ot.[description] like '%rerouted%' then 1
+				else 0
+			end Edited
 	from workgroups w
 		inner join workgroupsxorganizations wo on w.id = wo.WorkgroupId
 		inner join vadminworkgroups vaw on wo.organizationid = vaw.rollupparentid
@@ -22,6 +23,25 @@ from (
 		inner join ordertracking ot on ot.orderid = o.id
 		inner join workgrouppermissions wp on w.id = wp.workgroupid and wp.userid = ot.userid
 	where administrative = 1
+
+	union
+
+	select w.id ReportingWorkgroupId, w.name ReportingWorkgroup, w.id WorkgroupId, w.PrimaryOrganizationId WorkgroupOrg, w.Name WorkgroupName
+		, o.id, ot.userid, ot.orderstatuscodeid
+		, case  when ot.[description] like '%approve%' then 1
+				when ot.[description] like '%created%' then 1
+				else 0
+			end Approved
+		, case when ot.[description] like '%edit%' then 1
+				when ot.[description] like '%rerouted%' then 1
+				else 0
+			end Edited
+	from workgroups w
+		inner join orders o on o.workgroupid = w.id
+		inner join ordertracking ot on ot.orderid = o.id
+		inner join WorkgroupPermissions wp on w.id = wp.WorkgroupId and wp.userid = ot.userid
+	where administrative = 0
+
 ) TrackingHistory
 	inner join users on users.id = trackinghistory.userid
 group by ReportingWorkgroupId, ReportWorkgroup, WorkgroupName, WorkgroupOrg, UserId, firstname, lastname
