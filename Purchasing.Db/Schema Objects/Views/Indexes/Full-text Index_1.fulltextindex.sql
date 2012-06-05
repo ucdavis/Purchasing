9051ac4994f725d83522482c2e63c8e5ff3c07fd
Alan Lai <anlai@ucdavis.edu>
2012-06-05 10:14:00 -0700

    i have no idea why there are so many files, but it matches the orrect schema.  I only modified the vaccess and associated views.

---
+++ Purchasing.Db/Schema Objects/Views/Indexes/Full-text Index_1.fulltextindex.sql
@@ -0,0 +1,5 @@
+﻿/*CREATE FULLTEXT INDEX ON [dbo].[vLineResults]
+    ([CatalogNumber] LANGUAGE 1033, [Description] LANGUAGE 1033, [Url] LANGUAGE 1033, [Notes] LANGUAGE 1033, [CommodityId] LANGUAGE 1033)
+    KEY INDEX [vLineResults_Id_UDX]
+    ON [LineItems_DescriptionUrlNotesCatalognumberCommoditycode_SIDX];*/
+
---
+++ Purchasing.Db/Schema Objects/Views/vPendingOrders.view.sql
@@ -0,0 +1,160 @@
+﻿/*
+	Summary:
+
+	Query takes the recent orders that a person needs to access.
+
+	Date :	2/16/2012
+	
+*/
+
+CREATE VIEW [dbo].[vPendingOrders]
+	AS 
+
+select row_number() over (order by pending.orderid) id, *
+from 
+(
+	select distinct orders.id orderid, requestnumber, orders.datecreated
+		, dateneeded, creator.firstname + ' ' + creator.lastname creator
+		, tracking.datecreated lastactiondate
+		, codes.name statusname
+		, lineitemsummary.summary
+		, access.accessuserid
+	from orders
+		inner join users creator on creator.id = orders.createdby
+		inner join ordertracking tracking on tracking.orderid = orders.id
+		inner join orderstatuscodes codes on orders.orderstatuscodeid = codes.id
+		inner join ( 
+			 select orderid, STUFF(
+				(
+					select ', ' + convert(varchar(10), a.quantity) + ' [' + a.Unit + '] ' + a.[description]
+					from lineitems a
+					where a.orderid = lineitems.orderid
+					order by a.[description]
+					for xml PATH('')
+				), 1, 1, ''
+			) as summary
+			from lineitems
+			group by orderid
+			) lineitemsummary on lineitemsummary.orderid = orders.id
+		inner join vaccess access on access.OrderId = dbo.orders.id and editaccess = 1 and isadmin = 0
+	where
+		tracking.datecreated in ( select max(itracking.datecreated)
+								  from ordertracking itracking
+								  where tracking.orderid = itracking.orderid )
+		and codes.IsComplete = 0
+) Pending							  							  
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vPendingOrders';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 9
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vPendingOrders';
+
