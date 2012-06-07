1075671c1a8d45bada43e196b9925633241ca7c3
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-06-07 15:01:01 -0700

    ApplicationController Tests

---
+++ Purchasing.Tests/ControllerTests/ApplicationControllerTests.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.Windsor;
+using Purchasing.Web.Controllers;
+//using Purchasing.Controllers.Filters;
+using Purchasing.Core.Domain;
+//using Purchasing.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using Rhino.Mocks;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Testing;
+using UCDArch.Web.Attributes;
+
+
+namespace Purchasing.Tests.ControllerTests
+{
+    /// <summary>
+    /// Just doing reflection tests because that is all that is important
+    /// </summary>
+    [TestClass]
+    public class ApplicationControllerTests 
+    {
+        private readonly Type _controllerClass = typeof(ApplicationController);
+
+
+        #region Reflection Tests
+
+        #region Controller Class Tests
+        /// <summary>
+        /// Tests the controller inherits from SuperController controller.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerInheritsFromSuperControllerController()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            Assert.IsNotNull(controllerClass.BaseType);
+            var result = controllerClass.BaseType.Name;
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual("SuperController", result);
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has Five attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasFiveAttributes()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(5, result.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has transaction attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasTransactionAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseTransactionsByDefaultAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseTransactionsByDefaultAttribute not found.");
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has anti forgery token attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasAntiForgeryTokenAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseAntiForgeryTokenOnPostByDefault>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseAntiForgeryTokenOnPostByDefault not found.");
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestControllerHasVersionAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<Web.Attributes.VersionAttribute>();
+            #endregion Act
+
+            #region Assert
+            var element = result.ElementAt(0);
+            Assert.IsTrue(result.Count() > 0, "Web.Attributes.VersionAttribute not found.");
+            Assert.AreEqual(1, element.MajorVersion);
+            Assert.AreEqual("Version", element.VersionKey);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestControllerHasAuthorizeAttribute()
+        {
+            #region Arrange
+            var controllerClass = _controllerClass;
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<AuthorizeAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "AuthorizeAttribute not found.");
+            #endregion Assert
+        }
+
+        #endregion Controller Class Tests
+
+        #endregion Reflection Tests
+    }
+}
