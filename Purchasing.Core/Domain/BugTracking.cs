04b302c89354c085cd00f920ffe381729ca20a5a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-06-18 09:46:54 -0700

    added bug tracking stuff for subAccounts

---
+++ Purchasing.Core/Domain/BugTracking.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
+
+namespace Purchasing.Core.Domain
+{
+    public class BugTracking : DomainObject
+    {
+        public BugTracking()
+        {
+            DateTimeStamp = DateTime.Now;
+        }
+
+        public virtual int OrderId { get; set; }
+        public virtual string UserId { get; set; }
+        public virtual DateTime DateTimeStamp { get; set; }
+        public virtual string TrackingMessage { get; set; }
+        public virtual int? SplitId { get; set; }
+        public virtual int? LineItemId { get; set; }
+
+    }
+
+    public class BugTrackingMap : ClassMap<BugTracking>
+    {
+        public BugTrackingMap()
+        {
+            Table("BugTracking");
+
+            Id(x => x.Id);
+
+            Map(x => x.OrderId);
+            Map(x => x.UserId);
+            Map(x => x.DateTimeStamp);
+            Map(x => x.TrackingMessage);
+            Map(x => x.SplitId);
+            Map(x => x.LineItemId);
+        }
+    }
+}
