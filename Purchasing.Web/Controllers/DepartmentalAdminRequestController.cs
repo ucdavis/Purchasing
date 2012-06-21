74cae073011c85c43e9536222983c609e9c3c789
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-06-21 09:22:04 -0700

    Dept Admin request form improvements

--- Purchasing.Web/Controllers/DepartmentalAdminRequestController.cs
+++ Purchasing.Web/Controllers/DepartmentalAdminRequestController.cs
@@ -16,13 +16,15 @@ namespace Purchasing.Web.Controllers
     /// </summary>
     public class DepartmentalAdminRequestController : ApplicationController
     {
-	    private readonly IRepository<DepartmentalAdminRequest> _departmentalAdminRequestRepository;
+	    private readonly IRepositoryWithTypedId<DepartmentalAdminRequest, string> _departmentalAdminRequestRepository;
+        private readonly IRepositoryWithTypedId<Organization, string> _organizationRepository;
         private readonly IDirectorySearchService _directorySearchService;
-        
 
-        public DepartmentalAdminRequestController(IRepository<DepartmentalAdminRequest> departmentalAdminRequestRepository, IDirectorySearchService directorySearchService )
+
+        public DepartmentalAdminRequestController(IRepositoryWithTypedId<DepartmentalAdminRequest, string> departmentalAdminRequestRepository,IRepositoryWithTypedId<Organization, string> organizationRepository , IDirectorySearchService directorySearchService)
         {
             _departmentalAdminRequestRepository = departmentalAdminRequestRepository;
+            _organizationRepository = organizationRepository;
             _directorySearchService = directorySearchService;
         }
 
@@ -30,56 +32,74 @@ namespace Purchasing.Web.Controllers
         // GET: /DepartmentalAdminRequest/
         public ActionResult Index()
         {
-            var departmentalAdminRequestList = _departmentalAdminRequestRepository.Queryable;
+            var departmentalAdminRequestList = _departmentalAdminRequestRepository.Queryable.Where(a => !a.Complete).OrderBy(b => b.DateCreated);
 
             return View(departmentalAdminRequestList.ToList());
         }
 
         public ActionResult Create()
         {
-            
-            DirectoryUser ldap = _directorySearchService.FindUser(CurrentUser.Identity.Name);
-           Check.Require(ldap != null, "Person requesting Departmental Access ID not found. ID = " + CurrentUser.Identity.Name);
-           DepartmentalAdminRequest model = null;
+            var request = _departmentalAdminRequestRepository.GetNullableById(CurrentUser.Identity.Name);
+            var ldap = _directorySearchService.FindUser(CurrentUser.Identity.Name);
+            Check.Require(ldap != null, "Person requesting Departmental Access ID not found. ID = " + CurrentUser.Identity.Name);
 
-           model = new DepartmentalAdminRequest(ldap.LoginId.ToLower());
-           model.FirstName = ldap.FirstName;
-           model.LastName = ldap.LastName;
-           model.Email = ldap.EmailAddress;
-           model.PhoneNumber = ldap.PhoneNumber;
+            var model = DepartmentalAdminRequestViewModel.Create();
 
-           return View(model);
+            model.DepartmentalAdminRequest = request ?? new DepartmentalAdminRequest(ldap.LoginId.ToLower());
+            model.DepartmentalAdminRequest.FirstName = ldap.FirstName;
+            model.DepartmentalAdminRequest.LastName = ldap.LastName;
+            model.DepartmentalAdminRequest.Email = ldap.EmailAddress;
+            model.DepartmentalAdminRequest.PhoneNumber = ldap.PhoneNumber;
+
+            foreach (var orgId in model.DepartmentalAdminRequest.Organizations.Split(','))
+            {
+                var org = _organizationRepository.GetNullableById(orgId);
+                if (org != null)
+                {
+                    model.Organizations.Add(org);
+                }
+            }
+
+            return View(model);
         }
 
         [HttpPost]
-        public ActionResult Create(DepartmentalAdminRequest request, List<string> orgs )
+        public ActionResult Create(DepartmentalAdminRequestViewModel request, List<string> orgs)
         {
             Check.Require(request != null);
-            if (orgs == null || orgs.Count() == 0)
+            if (orgs == null || !orgs.Any())
             {
                 ErrorMessage = "Must select at least one organization";
                 return this.RedirectToAction(x => x.Create());
             }
            
-            DirectoryUser ldap = _directorySearchService.FindUser(CurrentUser.Identity.Name);
+            var ldap = _directorySearchService.FindUser(CurrentUser.Identity.Name);
            
             Check.Require(ldap != null, "Person requesting Departmental Access ID not found. ID = " + CurrentUser.Identity.Name);
-            DepartmentalAdminRequest model = null;
 
-            request.FirstName = ldap.FirstName;
-            request.LastName = ldap.LastName;
-            request.Email = ldap.EmailAddress;
-
-            request.DateCreated = DateTime.Now;
-            request.Organizations = string.Join(", ", orgs);
+            var requestToSave = _departmentalAdminRequestRepository.GetNullableById(request.DepartmentalAdminRequest.Id) ?? new DepartmentalAdminRequest(request.DepartmentalAdminRequest.Id);
+            requestToSave.RequestCount++;
+            requestToSave.FirstName = ldap.FirstName;
+            requestToSave.LastName = ldap.LastName;
+            requestToSave.Email = ldap.EmailAddress;
+            requestToSave.DateCreated = DateTime.Now;
+            requestToSave.Organizations = string.Join(",", orgs);
+            requestToSave.PhoneNumber = request.DepartmentalAdminRequest.PhoneNumber;
+            requestToSave.SharedOrCluster = request.DepartmentalAdminRequest.SharedOrCluster;
+            requestToSave.DepartmentSize = request.DepartmentalAdminRequest.DepartmentSize;
+            
             ModelState.Clear();
-            request.TransferValidationMessagesTo(ModelState);
+            requestToSave.TransferValidationMessagesTo(ModelState);            
+
             if(ModelState.IsValid)
             {
-                _departmentalAdminRequestRepository.EnsurePersistent(request);
+                _departmentalAdminRequestRepository.EnsurePersistent(requestToSave);
                 Message = "Request created.";
                 return this.RedirectToAction<HomeController>(x => x.Index());
             }
+
+            ErrorMessage = "There were Errors, please correct and try again.";
+            request.DepartmentalAdminRequest = requestToSave;
             return View(request);
 
         }
@@ -93,12 +113,13 @@ namespace Purchasing.Web.Controllers
     public class DepartmentalAdminRequestViewModel
 	{
 		public DepartmentalAdminRequest DepartmentalAdminRequest { get; set; }
+        public IList<Organization> Organizations { get; set; } 
  
-		public static DepartmentalAdminRequestViewModel Create(IRepository repository)
+		public static DepartmentalAdminRequestViewModel Create()
 		{
-			Check.Require(repository != null, "Repository must be supplied");
 			
 			var viewModel = new DepartmentalAdminRequestViewModel {DepartmentalAdminRequest = new DepartmentalAdminRequest()};
+            viewModel.Organizations = new List<Organization>();
  
 			return viewModel;
 		}
