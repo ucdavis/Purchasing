2ab0437e32d8b52207ea1b10dcdfc9dbcd345f20
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-06-26 11:24:07 -0700

    Code cleanup. Removed unused code.

--- Purchasing.Web/Controllers/HomeController.cs
+++ Purchasing.Web/Controllers/HomeController.cs
@@ -1,36 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Web.Mvc;
+using MvcContrib;
+using Purchasing.Core;
 using Purchasing.Core.Domain;
 using Purchasing.Core.Queries;
-using Purchasing.Web.Helpers;
 using Purchasing.Web.Models;
-using Purchasing.Web.Services;
 using UCDArch.Core.PersistanceSupport;
 using UCDArch.Web.ActionResults;
 using UCDArch.Web.Attributes;
-using UCDArch.Web.Controller;
-using MvcContrib;
-using Purchasing.Core;
-using System.Net;
 
 namespace Purchasing.Web.Controllers
 {
     public class HomeController : ApplicationController
     {
-        private readonly IOrderService _orderAccessService;
         private readonly IRepositoryWithTypedId<User, string> _userRepository;
         private readonly IQueryRepositoryFactory _queryRepositoryFactory;
-        private readonly IOrderService _orderService;
 
-        public HomeController(IOrderService orderAccessService, IRepositoryWithTypedId<User, string> userRepository, IQueryRepositoryFactory queryRepositoryFactory, IOrderService orderService)
+        public HomeController(IRepositoryWithTypedId<User, string> userRepository, IQueryRepositoryFactory queryRepositoryFactory)
         {
-            _orderAccessService = orderAccessService;
             _userRepository = userRepository;
             _queryRepositoryFactory = queryRepositoryFactory;
-            _orderService = orderService;
         }
 
         /// <summary>
@@ -138,17 +129,6 @@ namespace Purchasing.Web.Controllers
             }
 
             return rtValue;
-        }
-
-        private List<int> GetListOfOrderIds(int id)
-        {
-            var dt = DateTime.Now.AddDays(-id).Date;
-
-            var orderIds = (Repository.OfType<OrderTracking>().Queryable
-                .Where(a => a.Order.CreatedBy.Id == CurrentUser.Identity.Name && !a.Order.StatusCode.IsComplete)
-                .GroupBy(s => s.Order.Id).Select(g => new { id = g.Key, LastDateActedOn = g.Max(s => s.DateCreated) }).ToList().
-                Where(xy => xy.LastDateActedOn <= dt)).Select(cx => cx.id).Distinct().ToList();
-            return orderIds;
         }
 
         public static ColumnPreferences GetLandingPageColumnPreferences()
