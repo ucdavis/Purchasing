d6996889d446260784732b39aa7d0bafa67af654
Alan Lai <anlai@ucdavis.edu>
2012-06-27 10:06:50 -0700

    added some more install scripts for the lookups db

---
+++ PurchasingLookups.Db/Install Scripts/Replication.sql
@@ -0,0 +1,75 @@
+﻿-- Enabling the replication database
+use master
+exec sp_replicationdboption @dbname = N'PrePurchasingLookups', @optname = N'publish', @value = N'true'
+GO
+
+-- Adding the snapshot publication
+use [PrePurchasingLookups]
+exec sp_addpublication @publication = N'PrePurchasing Lookups', @description = N'Snapshot publication of database ''PrePurchasingLookups'' from Publisher ''CLAMPS''.', @sync_method = N'native', @retention = 0, @allow_push = N'true', @allow_pull = N'true', @allow_anonymous = N'true', @enabled_for_internet = N'false', @snapshot_in_defaultfolder = N'true', @compress_snapshot = N'false', @ftp_port = 21, @ftp_login = N'anonymous', @allow_subscription_copy = N'false', @add_to_active_directory = N'false', @repl_freq = N'snapshot', @status = N'active', @independent_agent = N'true', @immediate_sync = N'true', @allow_sync_tran = N'false', @autogen_sync_procs = N'false', @allow_queued_tran = N'false', @allow_dts = N'false', @replicate_ddl = 1
+GO
+
+
+exec sp_addpublication_snapshot @publication = N'PrePurchasing Lookups', @frequency_type = 4, @frequency_interval = 1, @frequency_relative_interval = 1, @frequency_recurrence_factor = 0, @frequency_subday = 1, @frequency_subday_interval = 1, @active_start_time_of_day = 50000, @active_end_time_of_day = 235959, @active_start_date = 0, @active_end_date = 0, @job_login = N'aesdean\prodreplication', @job_password = null, @publisher_security_mode = 1
+exec sp_grant_publication_access @publication = N'PrePurchasing Lookups', @login = N'sa'
+GO
+exec sp_grant_publication_access @publication = N'PrePurchasing Lookups', @login = N'NT AUTHORITY\SYSTEM'
+GO
+
+/*
+	fill in the login for the job that will be running the job
+exec sp_grant_publication_access @publication = N'PrePurchasing Lookups', @login = N''
+GO
+*/
+
+-- Adding the snapshot articles
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'ParamNameAndValue', @source_owner = N'dbo', @source_object = N'ParamNameAndValue', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'ParamNameAndValue', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'udf_GetParameterValue', @source_owner = N'dbo', @source_object = N'udf_GetParameterValue', @type = N'func schema only', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x0000000008000001, @destination_table = N'udf_GetParameterValue', @destination_owner = N'dbo', @status = 16
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'UnitOfMeasures', @source_owner = N'dbo', @source_object = N'UnitOfMeasures', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'UnitOfMeasures', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'usp_Post_DownloadAccountsTable_Processing', @source_owner = N'dbo', @source_object = N'usp_Post_DownloadAccountsTable_Processing', @type = N'proc schema only', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x0000000008000001, @destination_table = N'usp_Post_DownloadAccountsTable_Processing', @destination_owner = N'dbo', @status = 16
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'usp_Post_DownloadBuildingsTable_Processing', @source_owner = N'dbo', @source_object = N'usp_Post_DownloadBuildingsTable_Processing', @type = N'proc schema only', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x0000000008000001, @destination_table = N'usp_Post_DownloadBuildingsTable_Processing', @destination_owner = N'dbo', @status = 16
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'usp_Post_DownloadCommoditiesTable_Processing', @source_owner = N'dbo', @source_object = N'usp_Post_DownloadCommoditiesTable_Processing', @type = N'proc schema only', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x0000000008000001, @destination_table = N'usp_Post_DownloadCommoditiesTable_Processing', @destination_owner = N'dbo', @status = 16
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'usp_Post_DownloadVendorsTable_Processing', @source_owner = N'dbo', @source_object = N'usp_Post_DownloadVendorsTable_Processing', @type = N'proc schema only', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x0000000008000001, @destination_table = N'usp_Post_DownloadVendorsTable_Processing', @destination_owner = N'dbo', @status = 16
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'usp_RunPostProcessingScripts', @source_owner = N'dbo', @source_object = N'usp_RunPostProcessingScripts', @type = N'proc schema only', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x0000000008000001, @destination_table = N'usp_RunPostProcessingScripts', @destination_owner = N'dbo', @status = 16
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'vAccounts', @source_owner = N'dbo', @source_object = N'vAccounts', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'vAccounts', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'vBuildings', @source_owner = N'dbo', @source_object = N'vBuildings', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'vBuildings', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'vCommodities', @source_owner = N'dbo', @source_object = N'vCommodities', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'vCommodities', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'vCommodityGroups', @source_owner = N'dbo', @source_object = N'vCommodityGroups', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'vCommodityGroups', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'vOrganizationDescendants', @source_owner = N'dbo', @source_object = N'vOrganizationDescendants', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'manual', @destination_table = N'vOrganizationDescendants', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'vOrganizations', @source_owner = N'dbo', @source_object = N'vOrganizations', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'vOrganizations', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'vSubAccounts', @source_owner = N'dbo', @source_object = N'vSubAccounts', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'vSubAccounts', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'vVendorAddresses', @source_owner = N'dbo', @source_object = N'vVendorAddresses', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'vVendorAddresses', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
+use [PrePurchasingLookups]
+exec sp_addarticle @publication = N'PrePurchasing Lookups', @article = N'vVendors', @source_owner = N'dbo', @source_object = N'vVendors', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'drop', @schema_option = 0x000000000903509D, @identityrangemanagementoption = N'none', @destination_table = N'vVendors', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'SQL', @del_cmd = N'SQL', @upd_cmd = N'SQL'
+GO
---
+++ PurchasingLookups.Db/Install Scripts/Subscriber.sql
@@ -0,0 +1,34 @@
+﻿-- Adding the transactional pull subscription
+
+declare @pub varchar(max), @login varchar(max), @password varchar(max), @snapshotfolder varchar(max)
+
+-- publishing server's name
+set @pub = N''	
+-- login that is running the service
+set @login = N''			
+-- password for above login
+set @password = N''
+-- snapshot folder, share from the publisher to get the data from
+set @snapshotfolder = N''
+
+/****** Begin: Script to be run at Subscriber ******/
+use [PrePurchasingLookups]
+exec sp_addpullsubscription @publisher = @pub, @publication = N'PrePurchasing Lookups', @publisher_db = N'PrePurchasingLookups', 
+	@independent_agent = N'True', @subscription_type = N'pull', @description = N'', @update_mode = N'read only', @immediate_sync = 1
+
+exec sp_addpullsubscription_agent @publisher = N'CLAMPS', @publisher_db = N'PrePurchasingLookups', @publication = N'PrePurchasing Lookups', 
+	@distributor = @pub, @distributor_security_mode = 1, @distributor_login = N'', @distributor_password = N'', @enabled_for_syncmgr = N'False', 
+	@frequency_type = 4, @frequency_interval = 1, @frequency_relative_interval = 1, @frequency_recurrence_factor = 0, @frequency_subday = 1, 
+	@frequency_subday_interval = 0, @active_start_time_of_day = 53000, @active_end_time_of_day = 235959, @active_start_date = 0, @active_end_date = 0, 
+	@alt_snapshot_folder = @snapshotfolder, @working_directory = N'', @use_ftp = N'False', 
+	@job_login = @login, @job_password = @password, @publication_type = 0
+GO
+/****** End: Script to be run at Subscriber ******/
+
+/****** Begin: Script to be run at Publisher ******/
+/*use [PrePurchasingLookups]
+-- Parameter @sync_type is scripted as 'automatic', please adjust when appropriate.
+exec sp_addsubscription @publication = N'PrePurchasing Lookups', @subscriber = N'JOEYMOUSEPADS', @destination_db = N'PrePurchasingLookups', @sync_type = N'Automatic', @subscription_type = N'pull', @update_mode = N'read only'
+*/
+/****** End: Script to be run at Publisher ******/
+
