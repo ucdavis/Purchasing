047b7b26bb20e83cd33d540336889837f1636a91
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-06-28 10:17:13 -0700

    Code Cleanup. Removed unused and obsolete code from HomeController.

--- Purchasing.Web/Controllers/HomeController.cs
+++ Purchasing.Web/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using MvcContrib;
 using Purchasing.Core;
@@ -8,7 +6,6 @@ using Purchasing.Core.Domain;
 using Purchasing.Core.Queries;
 using Purchasing.Web.Models;
 using UCDArch.Core.PersistanceSupport;
-using UCDArch.Web.ActionResults;
 using UCDArch.Web.Attributes;
 
 namespace Purchasing.Web.Controllers
@@ -59,97 +56,5 @@ namespace Purchasing.Web.Controllers
 
             return View(viewModel);
         }
-
-        /// <summary>
-        /// Ajax call
-        /// </summary>
-        /// <param name="filter"></param>
-        /// <returns></returns>
-        [Authorize]
-        public JsonNetResult RequestAmountSummaryCall(string filter)
-        {
-            return new JsonNetResult(GetRequesterTotals(filter));
-        }
-        
-        [HandleTransactionsManually]
-        public ActionResult About()
-        {
-            return View();
-        }
-
-        #region Private Methods
-        private List<RequesterTotals> GetRequesterTotals(string filter)
-        {
-            var ordersPreFilter = Repository.OfType<Approval>().Queryable.Where(
-                a =>
-                ((a.User != null && a.User.Id == CurrentUser.Identity.Name) ||
-                 (a.SecondaryUser != null && a.SecondaryUser.Id == CurrentUser.Identity.Name)) &&
-                a.StatusCode.Id == OrderStatusCode.Codes.Approver).Select(b => b.Order);
-
-            List<Order> orders;
-            var localDate = DateTime.Now.Date;
-            switch(filter)
-            {
-                case "A":
-                    orders = ordersPreFilter.ToList();
-                    break;
-                case "W":
-                    localDate = localDate.AddDays(-7);
-                    orders = ordersPreFilter.Where(a => a.DateCreated >= localDate).ToList();
-                    break;
-                case "M":
-                    localDate = localDate.AddMonths(-1);
-                    orders = ordersPreFilter.Where(a => a.DateCreated >= localDate).ToList();
-                    break;
-                case "Y":
-                    localDate = localDate.AddYears(-1);
-                    orders = ordersPreFilter.Where(a => a.DateCreated >= localDate).ToList();
-                    break;
-                default:
-                    localDate = localDate.AddMonths(-1);
-                    orders = ordersPreFilter.Where(a => a.DateCreated >= localDate).ToList();
-                    break;
-            }
-
-            var completed = orders.Where(c => c.OrderTrackings.Any(d => d.StatusCode.IsComplete))
-                .Select(e => new { e.CreatedBy, Pending = 0, Completed = e.TotalFromDb }).ToList()
-                .GroupBy(f => f.CreatedBy).Select(g => new { id = g.Key, Pending = 0m, Completed = g.Sum(s => s.Completed) }).ToList();
-
-
-            var open = orders.Where(c => !c.OrderTrackings.Any(d => d.StatusCode.IsComplete))
-                .Select(e => new { e.CreatedBy, Pending = e.TotalFromDb, Completed = 0 }).ToList()
-                .GroupBy(f => f.CreatedBy).Select(g => new { id = g.Key, Pending = g.Sum(s => s.Pending), Completed = 0m }).ToList();
-
-            var rtValue = completed.Union(open).GroupBy(a => a.id).Select(b => new RequesterTotals() { FullNameAndId = b.Key.FullNameAndId, Pending = string.Format("{0:C}", b.Sum(s => s.Pending)), Completed = string.Format("{0:C}", b.Sum(s => s.Completed)) }).ToList();
-            var odd = false;
-            foreach(var requesterTotal in rtValue)
-            {
-                requesterTotal.EvenOdd = odd ? "odd" : "even";
-                odd = !odd;
-            }
-
-            return rtValue;
-        }
-
-        public static ColumnPreferences GetLandingPageColumnPreferences()
-        {
-            var rtValue = new ColumnPreferences();
-            //These are the default columns
-            rtValue.ShowWorkgroup = false;
-            rtValue.ShowVendor = false;
-            rtValue.ShowCreatedBy = true;
-            rtValue.ShowCreatedDate = true;
-            rtValue.ShowStatus = false;
-            rtValue.ShowNeededDate = false;
-            rtValue.ShowDaysNotActedOn = true;
-            rtValue.ShowAccountManager = false;
-
-            rtValue.ShowRequestNumber = true;
-
-            return rtValue;
-
-        }
-
-        #endregion Private Methods
     }
 }
