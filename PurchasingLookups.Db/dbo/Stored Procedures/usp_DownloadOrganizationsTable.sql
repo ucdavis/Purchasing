4181ab50da71d9053252a486fce9170204e9b5f7
Alan Lai <anlai@ucdavis.edu>
2012-07-18 15:30:21 -0700

    updated lookups db

--- PurchasingLookups.Db/dbo/Stored Procedures/usp_DownloadOrganizationsTable.sql
+++ PurchasingLookups.Db/dbo/Stored Procedures/usp_DownloadOrganizationsTable.sql
@@ -4,6 +4,8 @@
 -- Description:	Download organizational data and ultimately load into the vOrganizations table.
 -- Modifications:
 --	2012-06-23 by kjt: Converted from partitioned/swap table loading to direct table loading.
+--	2012-07-10 by kjt: Changed to slower merge load as per Alan Lai in order to preserve records
+--	missing from DaFIS.
 -- =============================================
 CREATE PROCEDURE [dbo].[usp_DownloadOrganizationsTable]
 	-- Add the parameters for the stored procedure here
@@ -20,15 +22,12 @@ BEGIN
 	DECLARE @TSQL varchar(MAX) = ''
 
     -- Insert statements for procedure here
-	SELECT @TSQL = '
-	TRUNCATE TABLE [PrePurchasingLookups].[dbo].[' + @LoadTableName + ']
-	
---	ALTER TABLE [dbo].[' + @ReferentialTableName + ']  WITH CHECK ADD  CONSTRAINT [FK_' + @ReferentialTableName + '_' + @LoadTableName + '] FOREIGN KEY([OrganizationId])
---REFERENCES [dbo].[' + @LoadTableName + '] ([Id])
---	ALTER TABLE [dbo].[' + @ReferentialTableName + '] CHECK CONSTRAINT [FK_' + @ReferentialTableName + '_' + @LoadTableName + '] 
-	
-	INSERT INTO ' + @LoadTableName + ' SELECT DISTINCT
-		[Id]
+	SELECT @TSQL = '	
+merge ' + @LoadTableName + ' as ' + @LoadTableName + ' 
+using
+(
+	SELECT DISTINCT
+		 [Id]
 		,ISNULL ([Name], [Id]) AS [Name]
 		,[TypeCode]
 		,[TypeName]
@@ -47,10 +46,25 @@ BEGIN
 		FROM FINANCE.ORGANIZATION_HIERARCHY O
 		WHERE O.FISCAL_YEAR = 9999 AND O.FISCAL_PERIOD = ''''--'''' 
 	'')
-	
-	--IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N''[dbo].[FK_' + @ReferentialTableName + '_' + @LoadTableName + ']'') AND parent_object_id = OBJECT_ID(N''[dbo].[' + @ReferentialTableName + ']''))
-	--	ALTER TABLE [dbo].[' + @ReferentialTableName + '] DROP CONSTRAINT [FK_' + @ReferentialTableName + '_' + @LoadTableName + ']
-	'
+) ' + @LinkedServerName + '_' + @LoadTableName + ' ON ' + @LoadTableName + '.Id = ' + @LinkedServerName + '_' + @LoadTableName + '.Id
+WHEN MATCHED THEN UPDATE SET
+	    ' + @LoadTableName + '.[Name] = ' + @LinkedServerName + '_' + @LoadTableName + '.[Name]
+	   ,' + @LoadTableName + '.[TypeCode] = ' + @LinkedServerName + '_' + @LoadTableName + '.[TypeCode]
+	   ,' + @LoadTableName + '.[TypeName] = ' + @LinkedServerName + '_' + @LoadTableName + '.[TypeName]
+	   ,' + @LoadTableName + '.[ParentId] = ' + @LinkedServerName + '_' + @LoadTableName + '.[ParentId]
+	   ,' + @LoadTableName + '.[IsActive] = ' + @LinkedServerName + '_' + @LoadTableName + '.[IsActive]
+WHEN NOT MATCHED BY TARGET THEN INSERT VALUES 
+ (
+	   [Id]
+      ,[Name]
+      ,[TypeCode]
+      ,[TypeName]
+      ,[ParentId]
+      ,[IsActive]
+ )
+WHEN NOT MATCHED BY SOURCE THEN UPDATE SET
+	   ' + @LoadTableName + '.[IsActive] = 0
+ ;'
 	
 	-------------------------------------------------------------------------
 	if @IsDebug = 1
--- PurchasingLookups.Db/dbo/Stored Procedures/usp_DownloadSubAccountsTable.sql
+++ PurchasingLookups.Db/dbo/Stored Procedures/usp_DownloadSubAccountsTable.sql
@@ -1,13 +1,15 @@
 ﻿-- =============================================
 -- Author:		Ken Taylor
 -- Create date: February 7, 2012
--- Description:	Download sub-accounts data and ultimately load into the vSubAccountsPartitionTable
+-- Description:	Download sub-accounts data and ultimately load into the vSubAccounts table
+--	2012-06-23 by kjt: Converted from partitioned/swap table loading to direct table loading.
+--	2012-07-10 by kjt: Changed to slower merge load as per Alan Lai in order to preserve records
+--	missing from DaFIS.
 -- =============================================
 CREATE PROCEDURE [dbo].[usp_DownloadSubAccountsTable]
 	-- Add the parameters for the stored procedure here
-	@LoadTableName varchar(255) = 'vSubAccountsPartitionTable_Load', --Name of load table being loaded 
+	@LoadTableName varchar(255) = 'vSubAccounts', --Name of table being loaded 
 	@LinkedServerName varchar(20) = 'FIS_DS', --Name of the linked DaFIS server.
-	
 	@IsDebug bit = 0 --Set to 1 just print the SQL and not actually execute it. 
 AS
 BEGIN
@@ -20,15 +22,15 @@ BEGIN
     -- Insert statements for procedure here
 	SELECT @TSQL = '
 	
-	TRUNCATE TABLE [PrePurchasingLookups].[dbo].[' + @LoadTableName + ']
-	
-	INSERT INTO ' + @LoadTableName + ' (AccountNumber, SubAccountNumber, Name, IsActive)
+merge ' + @LoadTableName + ' as ' + @LoadTableName + ' 
+using
+(
 	SELECT DISTINCT
-		[AccountNumber]
+		 [AccountNumber]
 		,[SubAccountNumber]
 		,[Name]
 		,(CASE [IsActive] WHEN ''Y'' THEN 1 ELSE 0 END) AS [IsActive]
-      
+		
 	FROM 
 	OPENQUERY(' + @LinkedServerName + ', ''
 		SELECT DISTINCT
@@ -38,8 +40,29 @@ BEGIN
 			S.sub_acct_active_ind  AS IsActive
 		FROM FINANCE.SUB_ACCOUNT S
 		WHERE S.FISCAL_YEAR = 9999 AND S.FISCAL_PERIOD = ''''--'''' 
-	'')'
-	
+	'')
+) ' + @LinkedServerName + '_' + @LoadTableName + ' ON ' + @LoadTableName + '.AccountNumber = ' + @LinkedServerName + '_' + @LoadTableName + '.AccountNumber AND
+						  ' + @LoadTableName + '.SubAccountNumber = ' + @LinkedServerName + '_' + @LoadTableName + '.SubAccountNumber
+WHEN MATCHED THEN UPDATE SET
+	    ' + @LoadTableName + '.[Name] = ' + @LinkedServerName + '_' + @LoadTableName + '.[Name]	
+	   ,' + @LoadTableName + '.[IsActive] = ' + @LinkedServerName + '_' + @LoadTableName + '.[IsActive]
+WHEN NOT MATCHED BY TARGET THEN INSERT
+	( 
+	   [AccountNumber]
+      ,[SubAccountNumber]
+      ,[Name]
+      ,[IsActive]
+	) 
+	VALUES 
+	(
+	   [AccountNumber]
+      ,[SubAccountNumber]
+      ,[Name]
+      ,[IsActive]
+	)
+WHEN NOT MATCHED BY SOURCE THEN UPDATE SET
+	   ' + @LoadTableName + '.[IsActive] = 0
+ ;'
 	-------------------------------------------------------------------------
 	if @IsDebug = 1
 		BEGIN
