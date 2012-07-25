4f12a201737f525b0442b75acea60cf67e203469
Alan Lai <anlai@ucdavis.edu>
2012-07-25 09:42:01 -0700

    added cols to workgroup perms for vaccess rewrite

--- PurchasingTP.Db/dbo/Tables/WorkgroupPermissions.sql
+++ PurchasingTP.Db/dbo/Tables/WorkgroupPermissions.sql
@@ -3,6 +3,8 @@
     [WorkgroupId] INT          NOT NULL,
     [UserId]      VARCHAR (10) NOT NULL,
     [RoleId]      CHAR (2)     NOT NULL,
+    [IsAdmin] BIT NOT NULL DEFAULT 0, 
+    [ParentWorkgroupId] INT NULL, 
     CONSTRAINT [PK_WorkgroupUsers_1] PRIMARY KEY NONCLUSTERED ([Id] ASC) WITH (ALLOW_PAGE_LOCKS = OFF, ALLOW_ROW_LOCKS = OFF),
     CONSTRAINT [FK_WorkgroupPermissions_Roles] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[Roles] ([Id]),
     CONSTRAINT [FK_WorkgroupPermissions_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
