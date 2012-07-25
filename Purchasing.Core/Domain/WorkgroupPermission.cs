c3f9623a0c07af10c14dded40bafef584f7695d5
Alan Lai <anlai@ucdavis.edu>
2012-07-25 07:26:33 -0700

    added new cols to support new method for vaccess

--- Purchasing.Core/Domain/WorkgroupPermission.cs
+++ Purchasing.Core/Domain/WorkgroupPermission.cs
@@ -6,12 +6,23 @@ namespace Purchasing.Core.Domain
 {
     public class WorkgroupPermission : DomainObject
     {
+        public WorkgroupPermission()
+        {
+            IsAdmin = false;
+        }
+
         [Required]
         public virtual Workgroup Workgroup { get; set; }
         [Required]
         public virtual User User { get; set; }
         [Required]
         public virtual Role Role { get; set; }
+
+        public virtual bool IsAdmin { get; set; }
+        /// <summary>
+        /// When permission is an admin role, what workgroup is this permission coming from?
+        /// </summary>
+        public virtual Workgroup ParentWorkgroup { get; set; }
     }
 
     public class WorkgroupPermissionMap : ClassMap<WorkgroupPermission>
@@ -23,6 +34,9 @@ namespace Purchasing.Core.Domain
             References(x => x.Workgroup);
             References(x => x.User);
             References(x => x.Role);
+
+            Map(x => x.IsAdmin);
+            References(x => x.ParentWorkgroup).Column("ParentWorkgroupId");
         }
     }
 }
