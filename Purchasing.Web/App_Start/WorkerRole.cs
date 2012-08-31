bca9c0474a27ef1136304306cb56870622c3488d
Scott Kirkland <srkirkland@gmail.com>
2012-08-31 11:37:57 -0700

    create index of vOrderHistory on startup

--- Purchasing.Web/App_Start/WorkerRole.cs
+++ Purchasing.Web/App_Start/WorkerRole.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Threading;
+using System.Web;
+using Dapper;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using Microsoft.Practices.ServiceLocation;
 using Microsoft.WindowsAzure.ServiceRuntime;
+using Purchasing.Core.Queries;
+using Purchasing.Web.Services;
 
 namespace Purchasing.Web.App_Start
 {
+    /*
     public class WorkerRole : RoleEntryPoint
     {
         public override void Run()
@@ -12,9 +25,8 @@ namespace Purchasing.Web.App_Start
             while (true)
             {
                 Thread.Sleep(TimeSpan.FromMinutes(15));
-
-
             }
         }
     }
+     */
 }
