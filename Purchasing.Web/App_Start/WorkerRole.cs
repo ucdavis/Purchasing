386b5da7c7914f4aed2690d08c42a0817dec69f7
Scott Kirkland <srkirkland@gmail.com>
2012-08-31 11:15:55 -0700

    added lucene to project and created worker role

---
+++ Purchasing.Web/App_Start/WorkerRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace Purchasing.Web.App_Start
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        public override void Run()
+        {
+            while (true)
+            {
+                Thread.Sleep(TimeSpan.FromMinutes(15));
+
+
+            }
+        }
+    }
+}
