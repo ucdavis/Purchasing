8237489e3276d9ecf519a413a3db1f37138adf4d
Alan Lai <anlai@ucdavis.edu>
2012-09-25 08:24:17 -0700

    added fields to email preference

--- Purchasing.Core/Domain/EmailPreferences.cs
+++ Purchasing.Core/Domain/EmailPreferences.cs
@@ -40,6 +40,9 @@ namespace Purchasing.Core.Domain
             PurchaserCampusServicesItemReceived = true;
 
             NotificationType = NotificationTypes.PerEvent;
+
+            AddAttachment = true;
+            AddNote = true;
         }
         public EmailPreferences(string id) : this() {Id = id;}
 
@@ -144,7 +147,10 @@ namespace Purchasing.Core.Domain
 
         #endregion Purchaser Settings
 
-        
+        #region Role Inspecific
+        public virtual bool AddAttachment { get; set; }
+        public virtual bool AddNote { get; set; }
+        #endregion
 
         public virtual NotificationTypes NotificationType { get; set; }
 
@@ -192,6 +198,9 @@ namespace Purchasing.Core.Domain
             Map(x => x.PurchaserCampusServicesItemReceived);
 
             Map(x => x.NotificationType).CustomType<NHibernate.Type.EnumStringType<EmailPreferences.NotificationTypes>>().Not.Nullable();
+
+            Map(x => x.AddAttachment);
+            Map(x => x.AddNote);
         }
     }
 }
