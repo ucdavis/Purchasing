19175df6d4b65025773e5b7a11ce61489e7a87aa
Scott Kirkland <srkirkland@gmail.com>
2012-10-10 13:41:07 -0700

    refactored common search API
    also added justification and new fields to order search

--- Purchasing.Web/Services/IndexSearchService.cs
+++ Purchasing.Web/Services/IndexSearchService.cs
@@ -7,9 +7,14 @@ using Lucene.Net.Search;
 using Purchasing.Core.Domain;
 using Purchasing.Core.Queries;
 using Version = Lucene.Net.Util.Version;
+using UCDArch.Core.Utils;
 
 namespace Purchasing.Web.Services
 {
+    /// <summary>
+    /// Searches using Lucene indexes
+    /// TODO: MUST filter orders by user
+    /// </summary>
     public class IndexSearchService : ISearchService
     {
         private readonly IIndexService _indexService;
@@ -26,28 +31,42 @@ namespace Purchasing.Web.Services
             var distinctOrderIds = new[] {747, 1149, 528, 151, 354, 123, 222, 365, 33, 98};
 
             var searcher = _indexService.GetIndexSearcherFor(Indexes.OrderHistory);
-            var analyzer = new StandardAnalyzer(Version.LUCENE_29);
-            Query accessQuery = new QueryParser(Version.LUCENE_29, "orderid", analyzer).Parse(string.Join(" ", distinctOrderIds));
-            var termsQuery = new MultiFieldQueryParser(Version.LUCENE_29, OrderHistory.SearchableFields, analyzer).Parse(searchTerm);
+            IEnumerable<ScoreDoc> results = SearchIndex(searcher, distinctOrderIds, searchTerm, OrderHistory.SearchableFields);
 
-            var results = searcher.Search(termsQuery, new CachingWrapperFilter(new QueryWrapperFilter(accessQuery)), 20).ScoreDocs; //get the top 20 results for this search
-            
             var orderResults = results
                 .Select(scoreDoc => searcher.Doc(scoreDoc.doc))
                 .Select(doc => new SearchResults.OrderResult
                                    {
                                        Id = int.Parse(doc.Get("orderid")),
+                                       Justification = doc.Get("justification"),
                                        CreatedBy = doc.Get("createdby"),
                                        DeliverTo = doc.Get("shipto"),
+                                       DeliverToEmail = doc.Get("shiptoemail"),
                                        RequestNumber = doc.Get("requestnumber"),
                                        DateCreated = DateTime.Parse(doc.Get("datecreated"))
                                    }).ToList();
 
-            analyzer.Close();
             searcher.Close();
             searcher.Dispose();
 
             return orderResults;
+        }
+
+        /// <summary>
+        /// Searches an index by the searchTerm and across searchableFields, filtering the results to only within the given orderIds
+        /// </summary>
+        /// <returns>ScoreDoc hits</returns>
+        private IEnumerable<ScoreDoc> SearchIndex(IndexSearcher searcher, IEnumerable<int> filteredOrderIds, string searchTerm, string[] searchableFields)
+        {
+            var analyzer = new StandardAnalyzer(Version.LUCENE_29);
+            Query accessQuery = new QueryParser(Version.LUCENE_29, "orderid", analyzer).Parse(string.Join(" ", filteredOrderIds));
+            var termsQuery =
+                new MultiFieldQueryParser(Version.LUCENE_29, searchableFields, analyzer).Parse(searchTerm);
+            var results = searcher.Search(termsQuery, new CachingWrapperFilter(new QueryWrapperFilter(accessQuery)), 20).ScoreDocs;
+            
+            analyzer.Close();
+            
+            return results;
         }
 
         public IList<SearchResults.LineResult> SearchLineItems(string searchTerm, string user)
