4dc81d8b5226902bab883ef563a176cb632884ce
Scott Kirkland <srkirkland@gmail.com>
2012-10-11 09:58:23 -0700

    create complex trigger for running lookups

--- Purchasing.Web/App_Start/CreateIndexes.cs
+++ Purchasing.Web/App_Start/CreateIndexes.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using Microsoft.Practices.ServiceLocation;
-using Purchasing.Web.Services;
+using Purchasing.Web.App_Start.Jobs;
 using Quartz;
 using Quartz.Impl;
 using System.Web;
@@ -14,47 +13,46 @@ namespace Purchasing.Web.App_Start
         {
             var indexRoot = HttpContext.Current.Server.MapPath("~/App_Data/Indexes");
 
-            // create job
-            var jobDetail = JobBuilder.Create<CreateOrderIndexJob>().UsingJobData("indexRoot", indexRoot).Build();
+            CreateOrderIndexesJob(indexRoot);
+            CreateLookupIndexsJob(indexRoot);
+        }
 
+        private static void CreateLookupIndexsJob(string indexRoot)
+        {
+            // create job
+            var jobDetails = JobBuilder.Create<CreateLookupIndexsJob>().UsingJobData("indexRoot", indexRoot).Build();
+            
             // create trigger
-            var everyFiveMinutes = TriggerBuilder.Create().ForJob(jobDetail).WithSchedule(SimpleScheduleBuilder.RepeatMinutelyForever(5)).StartNow().Build();
+            //run monday->friday every day starting at 5AM and only running once
+            //StartAt is 2 seconds from now, in order to give the more important jobs time to run quickly
+            var dailyTrigger = TriggerBuilder.Create().WithSchedule(
+                DailyTimeIntervalScheduleBuilder.Create()
+                    .OnMondayThroughFriday()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(10, 0))
+                    .WithRepeatCount(0))
+                .StartAt(DateTimeOffset.Now.AddSeconds(2))
+                .Build();
 
             // get reference to scheduler (remote or local) and schedule job
             var sched = StdSchedulerFactory.GetDefaultScheduler();
-            sched.ScheduleJob(jobDetail, everyFiveMinutes);
+            sched.ScheduleJob(jobDetails, dailyTrigger);
             sched.Start();
         }
-    }
-   
-    /// <summary>
-    /// Create the order indexes, which include HistoricalOrder, LineItems, Comments, and CustomAnswers
-    /// </summary>
-    public class CreateOrderIndexJob : IJob
-    {
-        private readonly IIndexService _indexService;
 
-        public CreateOrderIndexJob()
+        private static void CreateOrderIndexesJob(string indexRoot)
         {
-            _indexService = ServiceLocator.Current.GetInstance<IIndexService>();
-        }
+            // create job
+            var jobDetails = JobBuilder.Create<CreateOrderIndexsJob>().UsingJobData("indexRoot", indexRoot).Build();
 
-        public void Execute(IJobExecutionContext context)
-        {
-            var indexRoot = context.MergedJobDataMap["indexRoot"] as string;
-            _indexService.SetIndexRoot(indexRoot);
+            // create trigger
+            var everyFiveMinutes =
+                TriggerBuilder.Create().ForJob(jobDetails).WithSchedule(
+                    SimpleScheduleBuilder.RepeatMinutelyForever(5)).StartNow().Build();
 
-            try
-            {
-                _indexService.CreateHistoricalOrderIndex();
-                _indexService.CreateLineItemsIndex();
-                _indexService.CreateCommentsIndex();
-                _indexService.CreateCustomAnswersIndex();
-            }
-            catch (Exception ex)
-            {
-                Elmah.ErrorSignal.FromCurrentContext().Raise(ex);
-            }
+            // get reference to scheduler (remote or local) and schedule job
+            var sched = StdSchedulerFactory.GetDefaultScheduler();
+            sched.ScheduleJob(jobDetails, everyFiveMinutes);
+            sched.Start();
         }
     }
 }
