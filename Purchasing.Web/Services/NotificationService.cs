261924b0e794f4160e1ef83de9ba87c9b4f727d3
Alan Lai <anlai@ucdavis.edu>
2012-10-16 14:44:31 -0700

    wrote basic code for sending emails

--- Purchasing.Web/Services/NotificationService.cs
+++ Purchasing.Web/Services/NotificationService.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using IronRuby.Builtins;
 using Purchasing.Core;
 using Purchasing.Core.Domain;
+using SendGridMail;
+using SendGridMail.Transport;
 using UCDArch.Core.PersistanceSupport;
 using UCDArch.Core.Utils;
 
@@ -23,6 +28,8 @@ namespace Purchasing.Web.Services
 
         void OrderAddAttachment(Order order, User actor);
         void OrderAddNote(Order order, User actor);
+
+        void SendEmails();
     }
 
     public class NotificationService : INotificationService
@@ -321,6 +328,60 @@ namespace Purchasing.Web.Services
                     var emailQueue = new EmailQueue(order, preference.NotificationType, string.Format(AddNoteMessage, GenerateLink(_serverLink.Address, order.OrderRequestNumber()), actor.FullName),  ot);
                     order.AddEmailQueue(emailQueue);
                 }
+            }
+        }
+
+        public void SendEmails()
+        {
+            // always trigger per event emails
+            ProcessEmails(EmailPreferences.NotificationTypes.PerEvent);
+
+            // only send at 5
+            if (DateTime.Now.Hour == 5 && DateTime.Now.Minute == 0)
+            {
+                ProcessEmails(EmailPreferences.NotificationTypes.Daily);
+
+                // send weekly summaries
+                if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+                {
+                    ProcessEmails(EmailPreferences.NotificationTypes.Weekly);
+                }
+            }
+        }
+
+        private const string _sendGridUserName = "";
+        private const string _sendGridPassword = "";
+        private const string _sendGridFrom = "opp-noreply@ucdavis.edu";
+
+        private void ProcessEmails(EmailPreferences.NotificationTypes notificationType)
+        {
+            var pending = _emailRepository.Queryable.Where(a => a.Pending && a.NotificationType == notificationType).ToList();
+            var users = pending.Select(a => a.User).Distinct();
+
+            foreach (var user in users)
+            {
+                var message = new StringBuilder("<ul>");
+                var email = user.Email;
+
+                foreach (var eq in pending.Where(a => a.User == user))
+                {
+                    if (!string.IsNullOrEmpty(eq.Email)) email = eq.Email;
+
+                    message.Append("<li>");
+                    message.Append(eq.Text);
+                    message.Append("</li>");
+                }
+
+                message.Append("</ul>");
+
+                var sgMessage = SendGrid.GenerateInstance();
+                sgMessage.From = new MailAddress(_sendGridFrom);
+                sgMessage.Subject = "PrePurchasing Notifications";
+                sgMessage.AddTo(email);
+                sgMessage.Html = message.ToString();
+
+                var transport = SMTP.GenerateInstance(new NetworkCredential(_sendGridUserName, _sendGridPassword));
+                transport.Deliver(sgMessage);
             }
         }
 
