a54341c78d9379b9c89a5b6f05e5c5d9dea4a3d5
Alan Lai <anlai@ucdavis.edu>
2012-10-16 20:33:04 -0700

    added the email job

---
+++ Purchasing.Web/App_Start/Jobs/EmailJob.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Purchasing.Web.Services;
+using Quartz;
+
+namespace Purchasing.Web.App_Start.Jobs
+{
+    public class EmailJob : IJob
+    {
+        private readonly INotificationService _notificationService;
+
+        public EmailJob()
+        {
+            _notificationService = ServiceLocator.Current.GetInstance<INotificationService>(); ;
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            _notificationService.SendEmails();
+        }
+    }
+}
