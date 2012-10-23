ec594a29a666b95562fa086c50b33d061eea7d5c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-10-23 09:28:42 -0700

    Removed db access to states table for order dialog. Specify option values for States.

--- Purchasing.Core/IRepositoryFactory.cs
+++ Purchasing.Core/IRepositoryFactory.cs
@@ -38,7 +38,6 @@ namespace Purchasing.Core
         IRepository<ServiceMessage> ServiceMessageRepository { get; set; }
         IRepository<HistoryReceivedLineItem> HistoryReceivedLineItemRepository { get; set; }
         IRepositoryWithTypedId<Organization, string> OrganizationRepository { get; set; }
-        IRepositoryWithTypedId<State, string> StateRepository { get; set; } 
     }
 
     public class RepositoryFactory : IRepositoryFactory
@@ -74,7 +73,6 @@ namespace Purchasing.Core
         public IRepository<Building> BuildingRepository { get; set; }
         public IRepository<ServiceMessage> ServiceMessageRepository { get; set; }
         public IRepository<HistoryReceivedLineItem> HistoryReceivedLineItemRepository { get; set; }
-        public IRepositoryWithTypedId<Organization, string> OrganizationRepository { get; set; }
-        public IRepositoryWithTypedId<State, string> StateRepository { get; set; }       
+        public IRepositoryWithTypedId<Organization, string> OrganizationRepository { get; set; }     
     }
 }
