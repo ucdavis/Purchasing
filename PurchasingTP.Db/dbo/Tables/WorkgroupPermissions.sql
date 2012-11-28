1c744f7fa56316346e1982c186feba146ee987d6
Jason Sylvestre <jsylvestre@ucdavis.edu>
2012-11-28 09:53:45 -0800

    updated table.

--- PurchasingTP.Db/dbo/Tables/WorkgroupPermissions.sql
+++ PurchasingTP.Db/dbo/Tables/WorkgroupPermissions.sql
@@ -6,6 +6,7 @@
     [IsAdmin] BIT NOT NULL DEFAULT ((0)), 
     [ParentWorkgroupId] INT NULL, 
     [IsFullFeatured] BIT NOT NULL DEFAULT ((0)), 
+    [IsDefaultForAccount] BIT NOT NULL DEFAULT ((0)), 
     CONSTRAINT [PK_WorkgroupUsers_1] PRIMARY KEY NONCLUSTERED ([Id] ASC) WITH (ALLOW_PAGE_LOCKS = OFF, ALLOW_ROW_LOCKS = OFF),
     CONSTRAINT [FK_WorkgroupPermissions_Roles] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[Roles] ([Id]),
     CONSTRAINT [FK_WorkgroupPermissions_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
