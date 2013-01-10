1c276037d12e71b1a2738b9fd0161aba75232d55
Jason Sylvestre <jsylvestre@ucdavis.edu>
2013-01-10 15:31:13 -0800

    Changed the vOrderHistory view so the unspecified vendor has space between the dashes and the word. This makes the order by work like the datatable sort.

--- PurchasingTP.AzureDb/dbo/Views/vOrderHistory.sql
+++ PurchasingTP.AzureDb/dbo/Views/vOrderHistory.sql
@@ -3,7 +3,7 @@ AS
 select row_number() over (order by o.id) id, o.id orderid,  o.RequestNumber
 	, w.id workgroupid, w.name workgroupname
 	, case 
-		when o.WorkgroupVendorId is null then '--Unspecified--'
+		when o.WorkgroupVendorId is null then '-- Unspecified --'
 		when o.WorkgroupVendorId is not null and wv.line1 = 'N/A' and url is null then wv.name + '(No Adddress)' 
 		when o.WorkgroupVendorId is not null and wv.line1 = 'N/A' and url is not null then wv.name + '('+wv.url+')' 
 		else wv.name + ' (' + wv.Line1 + ', ' + wv.City + ', ' + wv.State + ', ' +wv.Zip +  ', ' + isnull(wv.CountryCode, '') + ')'
