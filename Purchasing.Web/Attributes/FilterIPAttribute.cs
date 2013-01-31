a3faa57b176b5be8c7f9f2678ca768604b4eee26
Jason Sylvestre <jsylvestre@ucdavis.edu>
2013-01-31 13:55:15 -0800

    Added IP Filter attribute, removed other IP filter attribute which wasn't used anymore

---
+++ Purchasing.Web/Attributes/FilterIPAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Purchasing.Web.Attributes
+{
+    public class FilterIPAttribute : AuthorizeAttribute
+    {
+        //List of the IP Addresses allow. Delimited by semicolon
+        public string AllowedIPs { get; set; }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext == null)
+                throw new ArgumentNullException("httpContext");
+
+            if (string.IsNullOrWhiteSpace(AllowedIPs))
+            {
+                throw new ArgumentException("No Allowed IP Addresses Configured-- you must allowed someone!");
+            }
+
+            return AllowedIPs.Split(';').Contains(httpContext.Request.UserHostAddress);
+        }
+    }
+}
