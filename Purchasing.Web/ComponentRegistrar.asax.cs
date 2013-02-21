0a648c8eb38cb79eb12906aea3c44ab0c6fecc3e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2013-02-21 14:54:17 -0800

    specifically don't use the sendgrid email sender

--- Purchasing.Web/ComponentRegistrar.asax.cs
+++ Purchasing.Web/ComponentRegistrar.asax.cs
@@ -61,14 +61,14 @@ namespace Purchasing.Web
             container.Register(Component.For<IUservoiceService>().ImplementedBy<UservoiceService>().Named("uservoiceService"));
             container.Register(Component.For<IBugTrackingService>().ImplementedBy<BugTrackingService>().Named("bugTrackingService"));
 
-#if DEBUG   
+  
             container.Register(Component.For<INotificationSender>().ImplementedBy<DevNotificationSender>().Named("notificationSender"));
-#else
-            container.Register(
-                Component.For<INotificationSender>().ImplementedBy<EmailNotificationSender>().Named("notificationSender")
-                    .OnCreate(service => service.SetAuthentication(ConfigurationManager.AppSettings["SendGridUserName"],
-                                                                   ConfigurationManager.AppSettings["SendGridPassword"])));
-#endif
+
+            //container.Register(
+            //    Component.For<INotificationSender>().ImplementedBy<EmailNotificationSender>().Named("notificationSender")
+            //        .OnCreate(service => service.SetAuthentication(ConfigurationManager.AppSettings["SendGridUserName"],
+            //                                                       ConfigurationManager.AppSettings["SendGridPassword"])));
+
         }
 
         private static void AddGenericRepositoriesTo(IWindsorContainer container)
