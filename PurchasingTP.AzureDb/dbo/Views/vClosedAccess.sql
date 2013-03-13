4dec393457293663169199a3a3cf812736c46883
Alan Lai <anlai@ucdavis.edu>
2013-03-13 11:30:19 -0700

    added id to closed access

--- PurchasingTP.AzureDb/dbo/Views/vClosedAccess.sql
+++ PurchasingTP.AzureDb/dbo/Views/vClosedAccess.sql
@@ -8,27 +8,30 @@ Orders that are in the following status codes: ('CN', 'CP', 'OC', 'OD')
 CREATE VIEW [dbo].[vClosedAccess]
 	AS 
 
-select distinct vClosedAccess.orderid, vClosedAccess.accessuserid, [admin] isadmin, OrderStatusCodeId accesslevel
-from
-(
-	select orderid, userid accessuserid, 0 [admin], ordertracking.OrderStatusCodeId
-	from ordertracking
-		inner join orders o on ordertracking.orderid = o.id
-	where o.orderstatuscodeid in ('CN', 'CP', 'OC', 'OD')
+select ROW_NUMBER() over (order by orderid) id, *
+from (
+	select distinct vClosedAccess.orderid, vClosedAccess.accessuserid, [admin] isadmin, OrderStatusCodeId accesslevel
+	from
+	(
+		select orderid, userid accessuserid, 0 [admin], ordertracking.OrderStatusCodeId
+		from ordertracking
+			inner join orders o on ordertracking.orderid = o.id
+		where o.orderstatuscodeid in ('CN', 'CP', 'OC', 'OD')
 
-	union
+		union
 
-	-- reviewer role
-	select o.id orderid, wp.userid accessuserid
-		, cast (case when wp.isadmin = 1 and wp.isfullfeatured = 0 then 1 else 0 end as bit) [admin]
-		, wp.RoleId orderstatuscodeid
-	from workgrouppermissions wp
-		inner join orders o on o.WorkgroupId = wp.WorkgroupId
-		inner join workgroups w on wp.workgroupid = w.id and w.IsActive = 1
-	where wp.roleid = 'RV'
-	  and o.orderstatuscodeid in ('CN', 'CP', 'OC', 'OD')
+		-- reviewer role
+		select o.id orderid, wp.userid accessuserid
+			, cast (case when wp.isadmin = 1 and wp.isfullfeatured = 0 then 1 else 0 end as bit) [admin]
+			, wp.RoleId orderstatuscodeid
+		from workgrouppermissions wp
+			inner join orders o on o.WorkgroupId = wp.WorkgroupId
+			inner join workgroups w on wp.workgroupid = w.id and w.IsActive = 1
+		where wp.roleid = 'RV'
+		  and o.orderstatuscodeid in ('CN', 'CP', 'OC', 'OD')
 
-) vClosedAccess
+	) vClosedAccess
+) cAccess
 
 
 	
