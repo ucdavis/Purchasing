f60947ca7753d25ea2e569cb111b5df66851d887
Scott Kirkland <srkirkland@gmail.com>
2014-02-12 14:00:56 -0800

    order contains Tag field for free text assoc

--- Purchasing.Core/Domain/Order.cs
+++ Purchasing.Core/Domain/Order.cs
@@ -76,8 +76,13 @@ namespace Purchasing.Core.Domain
         public virtual Organization Organization { get; set; }
         [StringLength(50)]
         public virtual string ReferenceNumber { get; set; }
+        
         [StringLength(50)]
         public virtual string PoNumber { get; set; }
+        
+        [StringLength(256)]
+        public virtual string Tag { get; set; }
+
         public virtual Approval LastCompletedApproval { get; set; }
         public virtual decimal ShippingAmount { get; set; }
         public virtual decimal FreightAmount { get; set; }
@@ -499,6 +504,7 @@ namespace Purchasing.Core.Domain
             Map(x => x.EstimatedTax);
             Map(x => x.ReferenceNumber);
             Map(x => x.PoNumber); //Added a specific PO Number because users were changing the reference number, messing up KFS access and some other reasons https://ucdavis.uservoice.com/admin/forums/126891-purchasing/suggestions/3224208-purchase-order-number
+            Map(x => x.Tag).Length(256);
             Map(x => x.ShippingAmount);
             Map(x => x.FreightAmount);
             Map(x => x.DeliverTo);
