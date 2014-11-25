d397d89c14dc32a66d13679f25ff7e99c7616619
Scott Kirkland <srkirkland@gmail.com>
2014-11-25 14:31:08 -0800

    added new mvc web project

---
+++ Purchasing.Mvc/App_Start/UCDArchBootstrapper.cs
@@ -0,0 +1,42 @@
+using System.Web.Mvc;
+using Castle.Windsor;
+using Microsoft.Practices.ServiceLocation;
+using Purchasing.Mvc.App_Start;
+using Purchasing.Mvc.Controllers;
+using UCDArch.Data.NHibernate;
+using UCDArch.Web.IoC;
+using UCDArch.Web.ModelBinder;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(UCDArchBootstrapper), "PreStart")]
+namespace Purchasing.Mvc.App_Start
+{
+    public class Customer { }
+    public class UCDArchBootstrapper
+    {
+        /// <summary>
+        /// PreStart for the UCDArch Application configures the model binding, db, and IoC container
+        /// </summary>
+        public static void PreStart()
+        {
+            ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
+
+            NHibernateSessionConfiguration.Mappings.UseFluentMappings(typeof(Customer).Assembly);
+            
+            IWindsorContainer container = InitializeServiceLocator();
+        }
+
+        private static IWindsorContainer InitializeServiceLocator()
+        {
+            IWindsorContainer container = new WindsorContainer();
+
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
+
+            container.RegisterControllers(typeof(HomeController).Assembly);
+            ComponentRegistrar.AddComponentsTo(container);
+
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+
+            return container;
+        }
+    }
+}
---
+++ Purchasing.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Purchasing.Mvc.MvcApplication" Language="C#" %>
