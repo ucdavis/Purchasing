bfdec4da8b3d6ee36d6846877935be85cec9b0bd
jscubbage@ucdavis.edu <jscubbage@ucdavis.edu>
2014-12-02 14:55:27 -0800

    Update Bundles.Reference to Scripts.Render

--- Purchasing.Mvc/App_Start/BundleConfig.cs
+++ Purchasing.Mvc/App_Start/BundleConfig.cs
@@ -43,6 +43,10 @@ namespace Purchasing.Mvc
                 .Include("~/Scripts/public/single/RearrangeDataTable.js")
                 .Include("~/Scripts/public/single/FixedHeader.js")
                 );
+
+            bundles.Add(new ScriptBundle("~/bundles/highlight")
+                .Include("~/Scripts/public/single/jquery.highlight.js")
+                );
             
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -91,6 +95,7 @@ namespace Purchasing.Mvc
             bundles.Add(new ScriptBundle("~/bundles/customField").Include("~/Scripts/public/single/jquery.tablednd_0_5.js"));
             bundles.Add(new ScriptBundle("~/bundles/landing").Include("~/Scripts/public/single/Landing.js"));
             bundles.Add(new ScriptBundle("~/bundles/jquery.tmpl").Include("~/Scripts/public/single/jquery.tmpl.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery.qtip").Include("~/Scripts/public/common/jquery.qtip.js"));
 
         }
     }
