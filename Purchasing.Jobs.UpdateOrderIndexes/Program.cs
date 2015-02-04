5d8c4a7cec3e8aec70e86abe0d822a195e9a5060
Scott Kirkland <srkirkland@gmail.com>
2015-02-04 11:20:57 -0800

    initial shot

--- Purchasing.Jobs.UpdateOrderIndexes/Program.cs
+++ Purchasing.Jobs.UpdateOrderIndexes/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Purchasing.Jobs.UpdateOrderIndexes
+﻿using System;
+
+namespace Purchasing.Jobs.UpdateOrderIndexes
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting v1 run at {0}", DateTime.Now);
         }
     }
 }
