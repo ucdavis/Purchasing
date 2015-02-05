24b17e2fdc20506a0910b79e73579eaa2c48588c
Scott Kirkland <srkirkland@gmail.com>
2015-02-05 11:24:04 -0800

    new webjob for recreating order indexes

---
+++ Purchasing.Jobs.CreateOrderIndexes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Ninject;
+using Purchasing.Core.Services;
+using Purchasing.Jobs.Common;
+
+namespace Purchasing.Jobs.CreateOrderIndexes
+{
+    class Program : WebJobBase
+    {
+        static void Main(string[] args)
+        {
+            var kernel = ConfigureServices();
+
+            var indexService = kernel.Get<IIndexService>();
+
+            try
+            {
+                indexService.CreateHistoricalOrderIndex();
+                indexService.CreateLineItemsIndex();
+                indexService.CreateCommentsIndex();
+                indexService.CreateCustomAnswersIndex();
+
+                Console.WriteLine("Order indexes created successfully at {0}", DateTime.Now);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FAILED: Index creation failed because {0}", ex.Message);
+            }
+        }
+    }
+}
