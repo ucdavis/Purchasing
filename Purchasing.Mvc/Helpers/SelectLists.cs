3ff2eab2ee929150a47528c5ef27f0430766f0b8
jscubbage@ucdavis.edu <jscubbage@ucdavis.edu>
2015-03-17 09:05:06 -0700

    Creating dropdownlist for PostStatus

---
+++ Purchasing.Mvc/Helpers/SelectLists.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Purchasing.Mvc.Helpers
+{
+    public static class SelectLists
+    {
+        public static List<SelectListItem> PostStatusList = new List<SelectListItem>()
+        {
+            new SelectListItem() {Text = "", Value = ""},
+            new SelectListItem() {Text = "Really complete", Value = "Really complete"},
+            new SelectListItem() {Text = "Receiving", Value = "Receiving"},
+            new SelectListItem() {Text = "Accounts Payable", Value = "Accounts Payable"},
+        };
+
+    }
+}
