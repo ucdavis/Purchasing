799463c191def6ef7ef8815b71945293d3d86858
jscubbage@ucdavis.edu <jscubbage@ucdavis.edu>
2015-03-19 11:35:37 -0700

    Temp best guess at what they should be

--- Purchasing.Mvc/Helpers/SelectLists.cs
+++ Purchasing.Mvc/Helpers/SelectLists.cs
@@ -11,9 +11,10 @@ namespace Purchasing.Mvc.Helpers
         public static List<SelectListItem> PostStatusList = new List<SelectListItem>()
         {
             new SelectListItem() {Text = "", Value = ""},
-            new SelectListItem() {Text = "Really complete", Value = "Really complete"},
+            new SelectListItem() {Text = "Awaiting User", Value = "Awaiting User"},
             new SelectListItem() {Text = "Receiving", Value = "Receiving"},
-            new SelectListItem() {Text = "Accounts Payable", Value = "Accounts Payable"},
+            new SelectListItem() {Text = "Paying", Value = "Paying"},
+            new SelectListItem() {Text = "Complete", Value = "Complete"},
         };
 
     }
