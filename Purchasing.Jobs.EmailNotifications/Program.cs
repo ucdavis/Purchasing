de1fab438cbece3b52a21c69527dc9ada0c57e8d
Scott Kirkland <srkirkland@gmail.com>
2015-07-16 14:08:10 -0700

    add in required packages

--- Purchasing.Jobs.EmailNotifications/Program.cs
+++ Purchasing.Jobs.EmailNotifications/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ninject;
+using Purchasing.Core.Services;
+using Purchasing.Jobs.Common;
+using Purchasing.Jobs.Common.Logging;
+using Microsoft.Azure.WebJobs;
 
 namespace Purchasing.Jobs.EmailNotifications
 {
-    public class Program
+    public class Program : WebJobBase
     {
+        private static IDbService _dbService;
+
         static void Main(string[] args)
         {
+            LogHelper.ConfigureLogging();
+
+            Console.WriteLine("Build Number: {0}", typeof(Program).Assembly.GetName().Version);
+
+            var kernel = ConfigureServices();
+            _dbService = kernel.Get<IDbService>();
+            var jobHost = new JobHost();
+            jobHost.Call(typeof(Program).GetMethod("EmailNotifications"));
+        }
+
+        [NoAutomaticTrigger]
+        public static void EmailNotifications()
+        {
+
         }
     }
 }
