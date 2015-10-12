5a6957b9004685a46744d3b9bcd77c25b7cb7945
Scott Kirkland <srkirkland@gmail.com>
2015-10-12 14:05:51 -0700

    use new attribute to handle and log errors

--- Purchasing.Mvc/App_Start/FilterConfig.cs
+++ Purchasing.Mvc/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using Purchasing.Mvc.Attributes;
 
 namespace Purchasing.Mvc
 {
@@ -7,7 +8,7 @@ namespace Purchasing.Mvc
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleAndLogErrorAttribute());
         }
     }
 }
---
+++ Purchasing.Mvc/Attributes/HandleAndLogErrorAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Serilog;
+
+namespace Purchasing.Mvc.Attributes
+{
+    public class HandleAndLogErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            Log.Error(filterContext.Exception, "Unhandled Exception Caught");
+
+            base.OnException(filterContext);
+        }
+    }
+}
