f3d502e6952b811ab6ddbb0dfe0b5e0f59260080
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-05-03 14:16:05 -0700

    If user isn't active, don't add to the list of email queues.

--- Purchasing.Mvc/Services/NotificationService.cs
+++ Purchasing.Mvc/Services/NotificationService.cs
@@ -727,6 +727,10 @@ namespace Purchasing.Mvc.Services
         /// <param name="emailQueue"></param>
         private void AddToQueue(List<EmailQueueV2> emailQueues, EmailQueueV2 emailQueue)
         {
+            if (!emailQueue.User.IsActive)
+            {
+                return;                
+            }
             if (!emailQueues.Any(a => a.User == emailQueue.User)) emailQueues.Add(emailQueue);
         }
 
