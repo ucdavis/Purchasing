53500c015e8a90394df3a0d904aa9a0abc562a4f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-10-18 13:46:15 -0700

    vSub accounts index

--- PurchasingTP.AzureDb/dbo/Tables/vSubAccounts.sql
+++ PurchasingTP.AzureDb/dbo/Tables/vSubAccounts.sql
@@ -10,3 +10,11 @@
 
 
 
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [nci_wi_vSubAccounts_D5E67DA673835CC5DC09051A0338E175]
+    ON [dbo].[vSubAccounts]([IsActive] ASC)
+    INCLUDE([AccountNumber], [Name], [SubAccountNumber]);
+
