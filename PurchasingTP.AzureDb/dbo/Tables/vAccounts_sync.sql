f1e725c8543aa28fcdedaff7a02939078953c099
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-01-24 09:13:33 -0800

    DB change from KFS
    Was causing the import to fail.

--- PurchasingTP.AzureDb/dbo/Tables/vAccounts_sync.sql
+++ PurchasingTP.AzureDb/dbo/Tables/vAccounts_sync.sql
@@ -4,7 +4,7 @@
     [IsActive]                BIT            DEFAULT ((0)) NOT NULL,
     [AccountManager]          VARCHAR (30)   NULL,
     [AccountManagerId]        VARCHAR (10)   NULL,
-    [PrincipalInvestigator]   VARCHAR (30)   NULL,
+    [PrincipalInvestigator]   VARCHAR (50)   NULL,
     [PrincipalInvestigatorId] VARCHAR (10)   NULL,
     [OrganizationId]          VARCHAR (10)   NOT NULL,
     [UpdateHash]              VARBINARY (16) NULL,
