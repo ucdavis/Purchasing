766eef0fda18ca4f7581df7d9395d233cee4c684
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-04-17 13:39:49 -0700

    Dev CAS
    It is going to happen

--- Purchasing.Mvc/Helpers/CasHelper.cs
+++ Purchasing.Mvc/Helpers/CasHelper.cs
@@ -8,7 +8,12 @@ namespace Purchasing.Mvc.Helpers
 {
     public static class CasHelper
     {
+#if DEBUG
+        private const string StrCasUrl = "https://ssodev.ucdavis.edu/cas/";
+#else
         private const string StrCasUrl = "https://cas.ucdavis.edu/cas/";
+#endif
+
         private const string StrTicket = "ticket";
         private const string StrReturnUrl = "ReturnURL";
 
