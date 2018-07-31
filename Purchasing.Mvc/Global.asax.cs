701bd8ea7d28020cd4dccdb738dcf1a403faf2b3
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-07-31 12:48:23 -0700

    Default TLS
    Maybe closes #381

--- Purchasing.Mvc/Global.asax.cs
+++ Purchasing.Mvc/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -12,6 +13,9 @@ namespace Purchasing.Mvc
     {
         protected void Application_Start()
         {
+            // specify to use TLS 1.2 as default connection
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
