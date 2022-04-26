db08224e4087aad140aae2ab05d12ef718c2307a
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2022-04-26 13:37:29 -0700

    Fix emulation

--- Purchasing.Mvc/Controllers/AccountController.cs
+++ Purchasing.Mvc/Controllers/AccountController.cs
@@ -65,10 +65,10 @@ namespace Purchasing.Mvc.Controllers
                 var identity = new ClaimsIdentity(new[]
                 {
                     new Claim(ClaimTypes.NameIdentifier, user.Id),
-                    // new Claim(ClaimTypes.Name, user.Id),
-                    // new Claim(ClaimTypes.GivenName, user.FirstName),
-                    // new Claim(ClaimTypes.Surname, user.LastName),
-                    // new Claim(ClaimTypes.Email, user.Email)
+                    new Claim(ClaimTypes.Name, user.Id),
+                    new Claim(ClaimTypes.GivenName, user.FirstName),
+                    new Claim(ClaimTypes.Surname, user.LastName),
+                    new Claim(ClaimTypes.Email, user.Email)
                 }, CookieAuthenticationDefaults.AuthenticationScheme);
 
                 // kill old login
