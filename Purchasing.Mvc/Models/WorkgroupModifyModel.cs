61d2fc0a4eba1d3a0118e8fcdaa152c7807b27fb
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2022-07-11 16:44:57 -0700

    Fix arguments to SelectListItem constructor

--- Purchasing.Mvc/Models/WorkgroupModifyModel.cs
+++ Purchasing.Mvc/Models/WorkgroupModifyModel.cs
@@ -22,15 +22,15 @@ namespace Purchasing.Mvc.Models
         {
             var modifyModel = new WorkgroupModifyModel { Workgroup = workgroup ?? new Workgroup() };
 
-            modifyModel.Organizations = workgroup != null ? workgroup.Organizations.Select(x => new SelectListItem(x.Name + " (" + x.Id + ")", x.Id, true) {Selected = true}).ToList() : new List<SelectListItem>();
+            modifyModel.Organizations = workgroup != null ? workgroup.Organizations.Select(x => new SelectListItem(x.Name + " (" + x.Id + ")", x.Id) {Selected = true}).ToList() : new List<SelectListItem>();
 
             var userOrgs = user.Organizations.Where(x => !modifyModel.Organizations.Select(y => y.Value).Contains(x.Id));
-            modifyModel.Organizations.AddRange(userOrgs.Select(x => new SelectListItem(x.Name + " (" + x.Id + ")", x.Id, true)));
+            modifyModel.Organizations.AddRange(userOrgs.Select(x => new SelectListItem(x.Name + " (" + x.Id + ")", x.Id)));
 
             var ids = modifyModel.Organizations.Select(y => y.Value).ToList();
 
             var decendantOrgs = queryRepositoryFactory.OrganizationDescendantRepository.Queryable.Where(a => ids.Contains(a.RollupParentId)).ToList();
-            modifyModel.Organizations.AddRange(decendantOrgs.Select(x => new SelectListItem(x.Name + " (" + x.OrgId + ")", x.OrgId, true)));
+            modifyModel.Organizations.AddRange(decendantOrgs.Select(x => new SelectListItem(x.Name + " (" + x.OrgId + ")", x.OrgId)));
 
             modifyModel.Organizations = modifyModel.Organizations.Distinct().OrderBy(a => a.Text).ToList();
 
