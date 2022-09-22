a97a3fef9432704ddacb1f18a0661318b85ec09d
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-09-22 10:30:16 -0700

    DB Change. New column

--- Purchasing.Core/Domain/Order.cs
+++ Purchasing.Core/Domain/Order.cs
@@ -33,6 +33,7 @@ namespace Purchasing.Core.Domain
             HasControlledSubstance = false;
 
             EstimatedTax = 7.25m; //Default 7.25% UCD estimated tax
+            ConsumerTrackingId = Guid.NewGuid(); //Used for Aggie Enterprise, but doesn't hurt to have it here
         }
 
         public virtual string RequestNumber { get; protected set; }
@@ -100,6 +101,8 @@ namespace Purchasing.Core.Domain
         public virtual bool HasControlledSubstance { get; set; }
 
         public virtual string CompletionReason { get; set; }
+
+        public virtual Guid ConsumerTrackingId { get; set; }
 
         public virtual IList<Attachment> Attachments { get; set; }
         public virtual IList<LineItem> LineItems { get; set; }
@@ -527,6 +530,7 @@ namespace Purchasing.Core.Domain
             Map(x => x.HasControlledSubstance).Column("HasAuthorizationNum");
             Map(x => x.TotalFromDb).Column("Total");
             Map(x => x.KfsDocType);
+            Map(x => x.ConsumerTrackingId);
 
             References(x => x.OrderType);
             References(x => x.ShippingType);
