406ddd905ee8ace94e707411ab8754047cde972a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-09-22 08:33:17 -0700

    Basic Service

---
+++ Purchasing.Core/Models/Configuration/AggieEnterpriseOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purchasing.Core.Models.Configuration
+{
+    public class AggieEnterpriseOptions
+    {
+        public string GraphQlUrl { get; set; }
+        public string Token { get; set; }
+
+    }
+}
--- Purchasing.Core/Services/AggieEnterpriseService.cs
+++ Purchasing.Core/Services/AggieEnterpriseService.cs
@@ -1,17 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AggieEnterpriseApi;
+using AggieEnterpriseApi.Extensions;
+using AggieEnterpriseApi.Types;
+using AggieEnterpriseApi.Validation;
+using Microsoft.Extensions.Options;
+using Purchasing.Core.Models.Configuration;
 using System.Threading.Tasks;
 
 namespace Purchasing.Core.Services
 {
     public interface IAggieEnterpriseService
     {
-        
+        Task<bool> IsAccountValid(string financialSegmentString, bool validateCVRs = true);
     }
     public class AggieEnterpriseService : IAggieEnterpriseService
     {
-        
+        private readonly IAggieEnterpriseClient _aggieClient;
+
+        public AggieEnterpriseService(IOptions<AggieEnterpriseOptions> options)
+        {
+            _aggieClient = GraphQlClient.Get(options.Value.GraphQlUrl, options.Value.Token);
+        }
+
+        public async Task<bool> IsAccountValid(string financialSegmentString, bool validateCVRs = true)
+        {
+            var segmentStringType = FinancialChartValidation.GetFinancialChartStringType(financialSegmentString);
+
+            if (segmentStringType == FinancialChartStringType.Gl)
+            {
+                var result = await _aggieClient.GlValidateChartstring.ExecuteAsync(financialSegmentString, validateCVRs);
+
+                var data = result.ReadData();
+
+                var isValid = data.GlValidateChartstring.ValidationResponse.Valid;
+
+
+                return isValid;
+            }
+
+            if (segmentStringType == FinancialChartStringType.Ppm)
+            {
+                var result = await _aggieClient.PpmStringSegmentsValidate.ExecuteAsync(financialSegmentString);
+
+                var data = result.ReadData();
+
+                var isValid = data.PpmStringSegmentsValidate.ValidationResponse.Valid;
+
+                return isValid;
+            }
+
+
+
+            return false;
+        }
+
     }
 }
