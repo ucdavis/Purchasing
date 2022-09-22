39e9e5dca8ae2f0b8724896ba958fac533171658
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-09-22 08:13:12 -0700

    Aggie Enterprise

---
+++ Purchasing.Core/Services/AggieEnterpriseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purchasing.Core.Services
+{
+    public interface IAggieEnterpriseService
+    {
+        
+    }
+    public class AggieEnterpriseService : IAggieEnterpriseService
+    {
+        
+    }
+}
