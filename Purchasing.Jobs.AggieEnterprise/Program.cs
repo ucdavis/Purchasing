f8ffb5fac1391820be1245538e1a5984223c8424
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-10-04 10:13:21 -0700

    wip job

--- Purchasing.Jobs.AggieEnterprise/Program.cs
+++ Purchasing.Jobs.AggieEnterprise/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Ninject;
+using Purchasing.Core.Helpers;
+using Purchasing.Core.Services;
+using Purchasing.Jobs.Common;
+using Purchasing.Jobs.Common.Logging;
+using Serilog;
+
+class Program : WebJobBase
+{
+    static void Main(string[] args)
+    {
+        var kernel = ConfigureServices();
+
+
+        try
+        {
+            Log.Information("Updating Aggie Enterprise Purchasing Categories (Commodities)");
+
+
+
+            Log.Information("Aggie Enterprise Purchasing Categories updated successfully at {0}", DateTime.UtcNow.ToPacificTime());
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "FAILED: Aggie Enterprise Purchasing Categories failed because {0}", ex.Message);
+        }
+    }
+}
