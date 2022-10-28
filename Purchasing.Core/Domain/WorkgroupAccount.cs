f813b9f6d3d8f4f50fe4cfecd2154640d1b36d15
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-10-28 07:40:39 -0700

    WIP DB Changes

--- Purchasing.Core/Domain/WorkgroupAccount.cs
+++ Purchasing.Core/Domain/WorkgroupAccount.cs
@@ -16,13 +16,34 @@ namespace Purchasing.Core.Domain
         [Display(Name = "Account Manager")]
         public virtual User AccountManager { get; set; }
         public virtual User Purchaser { get; set; }
-    }
+
+        [StringLength(64)] // 64 to hold account id and name (3-CRU9033: Account Name)
+        public virtual string Name { get;set;}
+        [DisplayName("CCOA")]
+        [StringLength(128)]
+        public virtual string FinancialSegmentString { get; set; }
+
+        //NotMapped
+        public virtual string GetName
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(Name))
+                {
+                    return Name;
+                }
+                return Account?.Name ?? "Not Set";
+            }
+        }
 
     public class WorkgroupAccountMap : ClassMap<WorkgroupAccount>
     {
         public WorkgroupAccountMap()
         {
             Id(x => x.Id);
+
+            Map(x => x.Name);
+            Map(x => x.FinancialSegmentString);
 
             References(x => x.Workgroup);
             References(x => x.Account);
