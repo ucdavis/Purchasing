6dd96fbf4a18762385b7d046b8bbe5c20e74fb7c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-11-18 12:31:37 -0800

    no longer available

--- Purchasing.Core/Services/AggieEnterpriseService.cs
+++ Purchasing.Core/Services/AggieEnterpriseService.cs
@@ -172,7 +172,6 @@ namespace Purchasing.Core.Services
                 SupplierSiteCode = supplier.SupplierSiteCode ,
                 RequesterEmailAddress = await GetAggieEnterpriseUserEmail(purchaserKerb, purchaserEmail),
                 Description = $"{order.RequestNumber} {order.Justification?.Trim()}{bp}".SafeTruncate(240),
-                //Justification has been deprecated, we will just pass in the description above
             };
                 
             var unitCodes = order.LineItems.Select(a => a.Unit).Distinct().ToArray();
