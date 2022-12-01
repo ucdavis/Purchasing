353155f5e6dcb5a2a353421a00db34bea3e1b8e4
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-12-01 07:54:58 -0800

    cleanup

--- Purchasing.Core/Domain/Split.cs
+++ Purchasing.Core/Domain/Split.cs
@@ -44,7 +44,6 @@ namespace Purchasing.Core.Domain
             get {
                 if (!string.IsNullOrWhiteSpace(FinancialSegmentString))
                 {
-                    //return $"{FinancialSegmentString} ({Name})";
                     return $"{Name} ({FinancialSegmentString})";
                 }
                 return DbAccount == null ? Account : DbAccount.NameAndId;
