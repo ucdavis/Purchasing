42b716f9cca4221e7b56fbcff889029d35f28328
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-05-01 13:33:09 -0700

    New Table for Favorites

---
+++ PurchasingTP.AzureDb/dbo/Tables/Favorites.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Favorites] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]      VARCHAR (10)  NOT NULL,
+    [OrderId]     INT           NOT NULL,
+    [CreatedDate] DATETIME      NOT NULL,
+    [Category]    VARCHAR (50)  NULL,
+    [Notes]       VARCHAR (MAX) NULL,
+    [IsActive]    BIT           CONSTRAINT [DF_Favorites_IsActive] DEFAULT ((1)) NOT NULL
+);
+
