f875e6e8d564f35d3825d74b151bf6fc61cbf2a9
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-05-01 13:57:21 -0700

    WIP

---
+++ Purchasing.Core/Domain/Favorite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using FluentNHibernate.Mapping;
+using FluentNHibernate.MappingModel;
+using Purchasing.Core.Helpers;
+using UCDArch.Core.DomainModel;
+using UCDArch.Core.Utils;
+
+namespace Purchasing.Core.Domain
+{
+    public class Favorite : DomainObject
+    {
+        public Favorite() 
+        {
+            DateAdded = DateTime.UtcNow;
+            IsActive = true;
+        }
+
+        //public virtual int Id { get; set; }
+        public virtual string UserId { get; set; }
+        public virtual int OrderId { get; set; }
+        public virtual DateTime DateAdded { get; set; }
+        [StringLength(50)]
+        public virtual string Category { get; set; }
+        public virtual string Notes { get; set; }
+        public virtual bool IsActive { get; set; } = true;
+    }
+}
--- PurchasingTP.AzureDb/dbo/Tables/Favorites.sql
+++ PurchasingTP.AzureDb/dbo/Tables/Favorites.sql
@@ -2,7 +2,7 @@
     [Id]          INT           IDENTITY (1, 1) NOT NULL,
     [UserId]      VARCHAR (10)  NOT NULL,
     [OrderId]     INT           NOT NULL,
-    [CreatedDate] DATETIME      NOT NULL,
+    [DateAdded] DATETIME      NOT NULL,
     [Category]    VARCHAR (50)  NULL,
     [Notes]       VARCHAR (MAX) NULL,
     [IsActive]    BIT           CONSTRAINT [DF_Favorites_IsActive] DEFAULT ((1)) NOT NULL
