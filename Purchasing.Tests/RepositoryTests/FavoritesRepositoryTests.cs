a8ee1dba964bf395176f22a6b2f14ff86a4c9f50
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-05-06 14:13:57 -0700

    test

---
+++ Purchasing.Tests/RepositoryTests/FavoritesRepositoryTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Purchasing.Core.Domain;
+using Purchasing.Tests.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purchasing.Tests.RepositoryTests
+{
+    [TestClass]
+    public class FavoritesRepositoryTests : AbstractRepositoryTests<Favorite, int, FavoriteMap>
+    {
+        protected override void FoundEntityComparison(Favorite entity, int counter)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IQueryable<Favorite> GetQuery(int numberAtEnd)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Favorite GetValid(int? counter)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void UpdateUtility(Favorite entity, ARTAction action)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Reflection of Database.
+
+        /// <summary>
+        /// Tests all fields in the database have been tested.
+        /// If this fails and no other tests, it means that a field has been added which has not been tested above.
+        /// </summary>
+        [TestMethod]
+        public void TestAllFieldsInTheDatabaseHaveBeenTested()
+        {
+            #region Arrange
+            var expectedFields = new List<NameAndType>();
+
+            expectedFields.Add(new NameAndType("Category", "System.String", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.StringLengthAttribute((Int32)50)]"
+            }));
+            expectedFields.Add(new NameAndType("DateAdded", "System.DateTime", new List<string>()));
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>
+            {
+                "[Newtonsoft.Json.JsonPropertyAttribute()]",
+                "[System.Xml.Serialization.XmlIgnoreAttribute()]"
+            }));
+            expectedFields.Add(new NameAndType("IsActive", "System.Boolean", new List<string>()));
+            expectedFields.Add(new NameAndType("Notes", "System.String", new List<string>()));
+            expectedFields.Add(new NameAndType("Order", "Purchasing.Core.Domain.Order", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]"
+            }));
+            expectedFields.Add(new NameAndType("User", "Purchasing.Core.Domain.User", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]"
+            }));
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(Favorite));
+
+        }
+
+        #endregion Reflection of Database.	
+
+
+    }
+}
